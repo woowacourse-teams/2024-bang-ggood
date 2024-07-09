e44b60eadb8682c9602da5faad449ebed248b144
Hyelim Choi <HyelimChoi01@gmail.com>
2024-07-09 13:53:43 +0900

    chore: 웹팩 세팅

---
+++ frontend/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => {
+  return <div>index</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById('root'));

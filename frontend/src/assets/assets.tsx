8baf69db9fdcfd103dbd2a4b1e9ad212d107537d
Hyelim Choi <HyelimChoi01@gmail.com>
2024-07-15 17:40:25 +0900

    chore: assets (face-icon, arrow, dot) 추가

---
+++ frontend/src/assets/assets.tsx
@@ -0,0 +1,20 @@
+import ArrowBack from '@/assets/arrow-back.svg';
+import QuestionDot from '@/assets/question-dot.svg';
+/* face icon */
+import FaceIconBadGray from '@/assets/face-icon/face-icon-bad-gray.svg';
+import FaceIconBad from '@/assets/face-icon/face-icon-bad.svg';
+import FaceIconGoodGray from '@/assets/face-icon/face-icon-good-gray.svg';
+import FaceIconGood from '@/assets/face-icon/face-icon-good.svg';
+import FaceIconSosoGray from '@/assets/face-icon/face-icon-soso-gray.svg';
+import FaceIconSoso from '@/assets/face-icon/face-icon-soso.svg';
+
+export {
+  ArrowBack,
+  FaceIconBad,
+  FaceIconBadGray,
+  FaceIconGood,
+  FaceIconGoodGray,
+  FaceIconSoso,
+  FaceIconSosoGray,
+  QuestionDot,
+};

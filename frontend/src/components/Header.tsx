569587faf8c6ee2bfe1de78f1ece5ad0e63289ff
Hyelim Choi <HyelimChoi01@gmail.com>
2024-07-15 22:12:02 +0900

    feat: 헤더 navigate 추가 및 로고 헤더 추가

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 import ArrowBack from '@/assets/arrow-back.svg';
 
@@ -14,16 +15,29 @@ const Header = () =>
 
   {
     return (
-      <StyledHeader>
+      <S.Wrapper>
         <ArrowBack />
-      </StyledHeader>
+        <Link to="/saved">
+          <S.TextButton>저장</S.TextButton>
+        </Link>
+      </S.Wrapper>
     );
   };
 export default Header;
 
-const StyledHeader = styled.header`
-  width: 100%;
-  height: 64px;
-  box-sizing: border-box;
-  padding: 24px;
-`;
+const S = {
+  Wrapper: styled.header`
+    width: 100%;
+    height: 64px;
+    box-sizing: border-box;
+    padding: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `,
+  TextButton: styled.button`
+    color: ${({ theme }) => theme.palette.black};
+    font-size: ${({ theme }) => theme.text.size.medium};
+    font-weight: ${({ theme }) => theme.text.weight.bold};
+  `,
+};

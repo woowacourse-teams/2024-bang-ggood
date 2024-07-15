865c27a06b9019c128e4dedcda2fc1d274699ee2
Hyelim Choi <HyelimChoi01@gmail.com>
2024-07-15 19:31:13 +0900

    design: 반응형 작업을 위한 뷰포트 추가 및 reset css 추가
    Co-Authored-By: Seyeong Jeon <86130706+skiende74@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import { css, Global, ThemeProvider } from '@emotion/react';
 import { RouterProvider } from 'react-router-dom';
 
 import router from '@/Router';
+import { normalize } from '@/styles/reset';
 import theme from '@/styles/theme';
 
 const baseStyle = css`
+  ${normalize}
   body {
-    width: 600px;
+    max-width: 600px;
     height: 100vh;
-    margin: auto;
+    margin: 0px auto;
+    box-sizing: border-box;
     border-right: 1px solid ${theme.palette.grey200};
     border-left: 1px solid ${theme.palette.grey200};
     box-shadow: 0 0 20px ${theme.palette.grey100};

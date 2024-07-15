61d14b96e8474d9001ece66b3d64854a7036e79c
ooherin <555ohr@naver.com>
2024-07-15 20:37:28 +0900

    feat(tab): 체크리스트 탭 css 완성

--- frontend/src/components/Tab.tsx
+++ frontend/src/components/Tab.tsx
@@ -31,12 +31,7 @@ const mockMenuList = [
   },
 ];
 const Tab = () => {
-  // console.log(mockMenuList);
-  // console.log(menuList);
-  //현재 선택된 라우터
-  // const [currentIdx, setCurrentIdx] = useState(0);
-
-  const [selectedId, setSelectedId] = useState(0);
+  const [selectedId, setSelectedId] = useState(1);
 
   const onClickMenu = (id: number) => {
     setSelectedId(id);
@@ -53,7 +48,9 @@ const Tab = () => {
               key={menu.menu_id}
               onClick={() => onClickMenu(menu.menu_id)}
               selected={menu.menu_id === selectedId}
-            />
+            >
+              <div>{menu.name}</div>
+            </S.OneMenu>
           );
         })}
       </S.FlexContainer>
@@ -63,18 +60,13 @@ const Tab = () => {
 
 export default Tab;
 
-//flexCenter
 export const S = {
   Container: styled.div`
     display: flex;
     position: relative;
     width: 100%;
-    height: 100px;
-    margin-top: 100px;
+    height: 50px;
 
-    background-color: yellow;
-
-    color: black;
     flex-direction: column;
   `,
   FlexContainer: styled.div`
@@ -82,11 +74,12 @@ export const S = {
   `,
   OneMenu: styled.div<{ selected?: boolean }>`
     width: 33%;
-    height: 34px;
     padding-top: 10px;
+    padding-bottom: 15px;
+    ${flexCenter}
 
-    font-weight: ${({ selected }) => (selected ? '600' : '400')};
-    border-top: ${({ selected, theme }) =>
-      selected ? `3px solid ${theme.palette.yellow500}` : `2px solid ${theme.palette.grey500}`};
+    font-weight: ${({ selected, theme }) => (selected ? theme.text.weight.bold : theme.text.weight.medium)};
+    border-bottom: ${({ selected, theme }) =>
+      selected ? `3px solid ${theme.palette.yellow400}` : `3px solid ${theme.palette.yellow100}`};
   `,
 };

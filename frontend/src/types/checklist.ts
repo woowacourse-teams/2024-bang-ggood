08c8742e03daf778bc3e0507bc909ba9f69eb282
Hyelim Choi <HyelimChoi01@gmail.com>
2024-07-16 11:22:43 +0900

    feat: 체크리스트 타입 선언

--- frontend/src/types/checklist.ts
+++ frontend/src/types/checklist.ts
@@ -1,11 +1,15 @@
 export interface ChecklistCategory {
   categoryId: number;
   category: string;
-  questions: ChecklistQuestion[];
+  questions: ChecklistAnswer[];
 }
 
 export interface ChecklistQuestion {
   questionId: number;
   questionTitle: string;
   questionSubtitle: string | null;
+}
+
+export interface ChecklistAnswer extends ChecklistQuestion {
+  answer?: number | null;
 }

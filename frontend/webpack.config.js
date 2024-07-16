46c4b92da07c2aba953978be3fc25c0f5c44196a
Hyelim Choi <HyelimChoi01@gmail.com>
2024-07-16 10:32:18 +0900

    feat: env 환경 설정 추가

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,13 +1,24 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
+// env
+const dotenv = require('dotenv');
+const env = dotenv.config().parsed;
 const isProduction = process.env.NODE_ENV == 'production';
 
 const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+const envKeys = env
+  ? Object.keys(env).reduce((prev, next) => {
+      prev[`process.env.${next}`] = JSON.stringify(env[next]);
+      return prev;
+    }, {})
+  : {};
 
 const config = {
   entry: './src/index.tsx',
@@ -22,6 +33,7 @@ const config = {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
+    new webpack.DefinePlugin(envKeys),
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],

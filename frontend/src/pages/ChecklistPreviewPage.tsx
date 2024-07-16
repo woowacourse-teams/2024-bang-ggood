1c784f62009ce22f56951108fc77f4750a629095
Hyelim Choi <HyelimChoi01@gmail.com>
2024-07-16 11:23:59 +0900

    feat: 체크리스트 확인 페이지 작업

---
+++ frontend/src/pages/ChecklistPreviewPage.tsx
@@ -0,0 +1,89 @@
+import { useCallback, useState } from 'react';
+
+import mockCategories from '@/_mock/checklist.json';
+import ChecklistCategory from '@/components/checklist/ChecklistCategory';
+import Header from '@/components/Header';
+
+interface AccordianOpen {
+  categoryId: number;
+  isOpen: boolean;
+}
+
+interface Answer {
+  questionId: number;
+  answer: number;
+}
+
+export interface addAnswerProps {
+  questionId: number;
+  newAnswer: number;
+}
+
+const ChecklistPreviewPage = () => {
+  const categories: ChecklistCategory[] = mockCategories;
+
+  // const [checklistData, setChecklistData] = useState(categories);
+  const [answers, setAnswers] = useState<Answer[]>([]);
+
+  const [accordianOpen, setAccordianOpen] = useState<AccordianOpen[]>(
+    categories.map(category => ({
+      categoryId: category.categoryId,
+      isOpen: true,
+    })),
+  );
+
+  const onToggleCategoryOpen = (id: number) => {
+    const newAccordianOpen = accordianOpen.map(category => {
+      return category.categoryId === id ? { ...category, isOpen: !category.isOpen } : category;
+    });
+
+    setAccordianOpen(newAccordianOpen);
+  };
+
+  const isAccordianOpen = (id: number) => {
+    const target = accordianOpen.filter(category => category.categoryId === id);
+    return target[0].isOpen;
+  };
+
+  const addAnswer = useCallback(
+    ({ questionId, newAnswer }: addAnswerProps) => {
+      const target = [...answers].find(answer => answer.questionId === questionId);
+      if (target) {
+        const newAnswers = [...answers].map(answer =>
+          answer.questionId === questionId ? { questionId, answer: newAnswer } : answer,
+        );
+        setAnswers(newAnswers);
+        return;
+      }
+      setAnswers(prev => [...prev, { questionId, answer: newAnswer }]);
+    },
+    [answers],
+  );
+
+  const deleteAnswer = (questionId: number) => {
+    setAnswers(prevAnswers => {
+      return prevAnswers.filter(answer => answer.questionId !== questionId);
+    });
+  };
+
+  return (
+    <>
+      <Header />
+      {categories.map(category => (
+        <ChecklistCategory
+          key={category.categoryId}
+          category={category}
+          toggleOpen={() => {
+            onToggleCategoryOpen(category.categoryId);
+          }}
+          isAccordianOpen={isAccordianOpen(category.categoryId)}
+          addAnswer={addAnswer}
+          deleteAnswer={deleteAnswer}
+          isAnswer={true}
+        />
+      ))}
+    </>
+  );
+};
+
+export default ChecklistPreviewPage;

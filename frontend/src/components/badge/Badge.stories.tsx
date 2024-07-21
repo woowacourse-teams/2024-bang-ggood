a6fcefe1f58b97375557c912e34d569ffb6ec859
ooherin <555ohr@naver.com>
2024-07-21 20:57:47 +0900

    fix: 병합 충돌 해결

--- frontend/src/components/badge/Badge.stories.tsx
+++ frontend/src/components/badge/Badge.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Badge from '@/components/Badge/Badge';
+import Badge from '@/components/badge/Badge';
 
 const meta = {
   title: 'components/Badge',

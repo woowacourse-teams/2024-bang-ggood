dfa3a11474fdc03a94b65821508ff0bbabad4e13
jinwoo22 <jenny010925@naver.com>
2024-07-21 18:01:26 +0900

    chore: BaseEntity 세팅
    Co-authored-by: tsulocalize <tsulocalizing@gmail.com>

--- backend/bang-ggood/src/main/java/com/bang_ggood/Application.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/Application.java
@@ -2,7 +2,9 @@ package com.bang_ggood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
---
+++ backend/bang-ggood/src/main/java/com/bang_ggood/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.bang_ggood;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+}

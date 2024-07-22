4b566b7728b90dd9aff3b340f9a2454a859eb1d5
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-07-22 14:59:24 +0900

    feat: Room 엔티티 생성

---
+++ backend/bang-ggood/src/main/java/com/bang_ggood/room/domain/Room.java
@@ -0,0 +1,75 @@
+package com.bang_ggood.room.domain;
+
+import com.bang_ggood.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Room extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer floor;
+
+    private String station;
+
+    private Integer walkingTime;
+
+    protected Room() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getFloor() {
+        return floor;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public Integer getWalkingTime() {
+        return walkingTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Room room = (Room) o;
+        return Objects.equals(id, room.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", floor=" + floor +
+                ", station='" + station + '\'' +
+                ", walkingTime=" + walkingTime +
+                '}';
+    }
+}

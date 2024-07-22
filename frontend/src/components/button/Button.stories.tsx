67e1da084b7fe09eb35a49631638c5eac2327032
ooherin <555ohr@naver.com>
2024-07-22 14:29:56 +0900

    fix: 대문자 파일이름 문제 해결

--- frontend/src/components/button/Button.stories.tsx
+++ frontend/src/components/button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Button from '@/components/button/Button';
+import Button from '@/components/Button/Button';
 
 const meta = {
   title: 'components/Button',

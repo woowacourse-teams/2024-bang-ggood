80f44f062806d8c1c9812ec90ec0f8803deb49b3
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-07-23 16:49:52 +0900

    feat(Room): 테이블 이름 지정
    Co-Authored-By: tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>

--- backend/bang-ggood/src/main/java/com/bang_ggood/room/domain/Room.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/room/domain/Room.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Objects;
 
+@Table(name = "room")
 @Entity
 public class Room extends BaseEntity {
 

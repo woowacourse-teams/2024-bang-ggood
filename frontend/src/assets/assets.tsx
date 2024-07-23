d0fa7f32fe371eb93662f829ffd1de577918221e
Hyelim Choi <HyelimChoi01@gmail.com>
2024-07-23 14:29:23 +0900

    chore: 방끗 로고, 오른쪽 화살표 아이콘 추가

--- frontend/src/assets/assets.tsx
+++ frontend/src/assets/assets.tsx
@@ -1,8 +1,10 @@
+/* Arrow */
 import ArrowBack from '@/assets/arrow-back.svg';
-import QuestionDot from '@/assets/question-dot.svg';
-/* face icon */
 import ArrowDownSmall from '@/assets/arrow-down-small.svg';
+import ArrowRightGreen from '@/assets/arrow-right-green.svg';
 import ArrowUpSmall from '@/assets/arrow-up-small.svg';
+
+/* face icon */
 import FaceIconBadGray from '@/assets/face-icon/face-icon-bad-gray.svg';
 import FaceIconBad from '@/assets/face-icon/face-icon-bad.svg';
 import FaceIconGoodGray from '@/assets/face-icon/face-icon-good-gray.svg';
@@ -20,10 +22,17 @@ import LocationLogoActive from '@/assets/logos/location-logo-active.svg';
 import LocationLogo from '@/assets/logos/location-logo.svg';
 import MyPageLogoActive from '@/assets/logos/my-page-logo-active.svg';
 import MyPageLogo from '@/assets/logos/my-page-logo.svg';
+
+/* etc */
+import BangGgoodIcon from '@/assets/bang-ggood-icon.svg';
+import QuestionDot from '@/assets/question-dot.svg';
+
 export {
   ArrowBack,
   ArrowDownSmall,
+  ArrowRightGreen,
   ArrowUpSmall,
+  BangGgoodIcon,
   ChecklistLogo,
   ChecklistLogoActive,
   FaceIconBad,
@@ -32,12 +41,12 @@ export {
   FaceIconGoodGray,
   FaceIconSoso,
   FaceIconSosoGray,
-  Plus,
   HomeLogo,
   HomeLogoActive,
   LocationLogo,
   LocationLogoActive,
   MyPageLogo,
   MyPageLogoActive,
+  Plus,
   QuestionDot,
 };

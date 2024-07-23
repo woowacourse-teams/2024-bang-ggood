1979f7cb87b720056821b9e3f26c16e477e6e951
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-07-23 16:49:33 +0900

    feat: 체크리스트 저장 시 예외 처리 구현
    Co-Authored-By: tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>

--- backend/bang-ggood/src/main/java/com/bang_ggood/exception/ExceptionCode.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/exception/ExceptionCode.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpStatus;
 
 public enum ExceptionCode {
 
-    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 인자입니다.");
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 인자입니다."),
+    INVALID_OPTION_ID(HttpStatus.BAD_REQUEST, "잘못된 옵션 ID입니다."),
+    INVALID_QUESTION_ID(HttpStatus.BAD_REQUEST, "잘못된 질문 ID입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;

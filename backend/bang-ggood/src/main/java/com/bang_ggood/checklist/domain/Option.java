90970c30eaaa6f7b0b032195a7f428643cd54c13
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-07-23 13:47:57 +0900

    style: 옵션 중요한 순으로 정렬
    Co-Authored-By: tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>

--- backend/bang-ggood/src/main/java/com/bang_ggood/checklist/domain/Option.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/checklist/domain/Option.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 
 public enum Option {
     AIR_CONDITIONER(1, "에어컨"),
-    SINK(2, "싱크대"),
-    REFRIGERATOR(3, "냉장고"),
+    REFRIGERATOR(2, "냉장고"),
+    MICROWAVE_OVEN(3, "전자레인지"),
     WASHING_MACHINE(4, "세탁기"),
-    MICROWAVE_OVEN(5, "전자레인지"),
-    TV(6, "TV"),
-    DRYER(7, "건조기"),
-    INTERNET(8, "인터넷"),
-    GAS_STOVE(9, "가스레인지/인덕션"),
-    ELEVATOR(10, "엘리베이터"),
-    BED(11, "침대"),
-    DESK(12, "책상"),
-    CLOSET(13, "옷장"),
-    SHOE_RACK(14, "신발장");
+    SINK(5, "싱크대"),
+    GAS_STOVE(6, "가스레인지/인덕션"),
+    INTERNET(7, "인터넷"),
+    BED(8, "침대"),
+    DESK(9, "책상"),
+    CLOSET(10, "옷장"),
+    SHOE_RACK(11, "신발장"),
+    ELEVATOR(12, "엘리베이터"),
+    DRYER(13, "건조기"),
+    TV(14, "TV");
 
     private Integer id;
     private String name;

2b22c5700935957d9702fd6f1cfc2723e2456907
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-07-24 15:48:42 +0900

    test: hasMessage ExceptionCode 사용
    Co-Authored-By: tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>

--- backend/bang-ggood/src/test/java/com/bang_ggood/checklist/service/ChecklistServiceTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/checklist/service/ChecklistServiceTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.bang_ggood.IntegrationTestSupport;
 import com.bang_ggood.checklist.ChecklistFixture;
 import com.bang_ggood.exception.BangggoodException;
+import com.bang_ggood.exception.ExceptionCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +34,7 @@ class ChecklistServiceTest extends IntegrationTestSupport {
         assertThatThrownBy(
                         () -> checklistService.createChecklist(ChecklistFixture.CHECKLIST_CREATE_REQUEST_INVALID_QUESTION_ID))
                 .isInstanceOf(BangggoodException.class)
-                .hasMessage("잘못된 질문 ID입니다.");
+                .hasMessage(ExceptionCode.INVALID_QUESTION.getMessage());
     }
 
     @DisplayName("체크리스트 방 정보 작성 실패: 질문 id가 중복일 경우")
@@ -43,7 +44,7 @@ class ChecklistServiceTest extends IntegrationTestSupport {
         assertThatThrownBy(
                 () -> checklistService.createChecklist(ChecklistFixture.CHECKLIST_CREATE_REQUEST_DUPLICATED_QUESTION_ID))
                 .isInstanceOf(BangggoodException.class)
-                .hasMessage("중복된 질문이 존재합니다.");
+                .hasMessage(ExceptionCode.QUESTION_DUPLICATED.getMessage());
     }
 
     @DisplayName("체크리스트 방 정보 작성 실패: 옵션 id가 유효하지 않을 경우")
@@ -53,7 +54,7 @@ class ChecklistServiceTest extends IntegrationTestSupport {
         assertThatThrownBy(
                 () -> checklistService.createChecklist(ChecklistFixture.CHECKLIST_CREATE_REQUEST_INVALID_OPTION_ID))
                 .isInstanceOf(BangggoodException.class)
-                .hasMessage("잘못된 옵션 ID입니다.");
+                .hasMessage(ExceptionCode.INVALID_OPTION.getMessage());
     }
 
     @DisplayName("체크리스트 방 정보 작성 실패: 옵션 id가 중복일 경우")
@@ -63,6 +64,6 @@ class ChecklistServiceTest extends IntegrationTestSupport {
         assertThatThrownBy(
                 () -> checklistService.createChecklist(ChecklistFixture.CHECKLIST_CREATE_REQUEST_DUPLICATED_OPTION_ID))
                 .isInstanceOf(BangggoodException.class)
-                .hasMessage("중복된 옵션이 존재합니다.");
+                .hasMessage(ExceptionCode.OPTION_DUPLICATED.getMessage());
     }
 }

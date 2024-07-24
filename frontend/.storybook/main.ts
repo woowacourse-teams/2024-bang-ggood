a5a1682ea03b9eb210a3005441609d9303304079
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-24 19:17:35 +0900

    chore: storybook에서 svgr에 icon: true 추가

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -58,11 +58,12 @@ const config: StorybookConfig = {
         return rule;
       }),
       {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [{ loader: '@svgr/webpack', options: { icon: true } }],
       },
     ];
-    
+
     return config;
   },
 };

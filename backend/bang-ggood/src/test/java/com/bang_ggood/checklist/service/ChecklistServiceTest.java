6d9c9fcf74de1e5864464eedb47f16d2076cea69
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-07-24 14:21:37 +0900

    test: 중복 어노테이션 제거
    Co-Authored-By: tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>

--- backend/bang-ggood/src/test/java/com/bang_ggood/checklist/service/ChecklistServiceTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/checklist/service/ChecklistServiceTest.java
@@ -8,9 +8,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ChecklistServiceTest extends IntegrationTestSupport {
 
     @Autowired

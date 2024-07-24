7dde2b0fd4cae5c136bf2760f079a81a6bb75a74
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-24 19:20:51 +0900

    feat: Header 컴포넌트에서 rest prop 전달

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -55,11 +55,12 @@ const Header = Object.assign(HeaderWrapper, {
       }}
     />
   ),
-  Backward: () => (
+  Backward: (props: React.SVGProps<SVGSVGElement>) => (
     <ArrowBack
       style={{
         cursor: 'pointer',
       }}
+      {...props}
     />
   ),
   TextButton: S.TextButton,

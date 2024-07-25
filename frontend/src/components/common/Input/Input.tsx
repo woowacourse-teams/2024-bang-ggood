d01d0197b2b38d624f18c5700d0e5d29a230b089
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-25 11:45:05 +0900

    fix: Input태그 width 적용안되던 버그 해결

--- frontend/src/components/common/Input/Input.tsx
+++ frontend/src/components/common/Input/Input.tsx
@@ -24,6 +24,7 @@ const Input = ({ width, onChange, ...rest }: Props) => {
 const S = {
   Input: styled.input<StyledProps>`
     display: flex;
+    width: ${({ width }) => width};
     height: 32px;
     padding: 6px 11px;
     border: 1px solid ${({ $color, theme }) => ($color ? $color : theme.palette.grey100)};

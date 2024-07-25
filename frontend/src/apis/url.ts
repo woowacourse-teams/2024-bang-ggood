d790bbd2fa20a216606b8f7acd93d5326aa114f1
Hyelim Choi <HyelimChoi01@gmail.com>
2024-07-25 15:02:26 +0900

    feat: 방 비교 데이터 api 작업

--- frontend/src/apis/url.ts
+++ frontend/src/apis/url.ts
@@ -8,6 +8,9 @@ export const ENDPOINT = {
   CHECKLISTS: '/checklists',
   CHECKLIST_QUESTION: '/checklist/question',
   CHECKLIST_ID: (id: number) => `/checklist/${id}`,
+  // TODO: id3 없을 때 처리
+  CHECKLIST_COMPARE: ({ id1, id2, id3 }: { id1: number; id2: number; id3: number }) =>
+    `/checklists/comparison?id=${id1}&id=${id2}&id=${id3}`,
   /* category */
   CATEGORY: '/categories',
   CATEGORY_ADD: '/categories/priority',

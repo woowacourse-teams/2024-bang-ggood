bd3d8d660539b8d5aa2c0d1b27d7e48c7b80302c
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-07-25 10:32:51 +0900

    refactor(Category): 질문 저장하는 자료형 Set으로 변경
    Co-Authored-By: tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>

--- backend/bang-ggood/src/main/java/com/bang_ggood/category/domain/Category.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/category/domain/Category.java
@@ -1,23 +1,24 @@
 package com.bang_ggood.category.domain;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public enum Category {
 
-    CLEAN(1, "청결", List.of(1, 2, 3, 4, 5)),
-    ROOM_CONDITION(2, "방 컨디션", List.of(6, 7, 8, 9, 10, 11)),
-    AMENITY(3, "편의시설", List.of(12, 13, 14)),
-    OPTION(4, "옵션", List.of(15, 16)),
-    ENVIRONMENT(5, "주거환경", List.of(17, 18, 19, 20, 21, 22)),
-    SECURITY(6, "보안", List.of(22, 23, 24, 25, 26, 27, 28, 29, 30)),
-    ECONOMIC(7, "경제적", List.of(31, 32));
+    CLEAN(1, "청결", new LinkedHashSet<>(Set.of(1, 2, 3, 4, 5))),
+    ROOM_CONDITION(2, "방 컨디션", new LinkedHashSet<>(Set.of(6, 7, 8, 9, 10, 11))),
+    AMENITY(3, "편의시설", new LinkedHashSet<>(Set.of(12, 13, 14))),
+    OPTION(4, "옵션", new LinkedHashSet<>(Set.of(15, 16))),
+    ENVIRONMENT(5, "주거환경", new LinkedHashSet<>(Set.of(17, 18, 19, 20, 21, 22))),
+    SECURITY(6, "보안", new LinkedHashSet<>(Set.of(22, 23, 24, 25, 26, 27, 28, 29, 30))),
+    ECONOMIC(7, "경제적", new LinkedHashSet<>(Set.of(31, 32)));
 
     private final int id;
     private final String description;
-    private final List<Integer> questionIds;
+    private final Set<Integer> questionIds;
 
-    Category(int id, String description, List<Integer> questionIds) {
+    Category(int id, String description, Set<Integer> questionIds) {
         this.id = id;
         this.description = description;
         this.questionIds = questionIds;
@@ -36,7 +37,7 @@ public enum Category {
         return description;
     }
 
-    public List<Integer> getQuestionIds() {
+    public Set<Integer> getQuestionIds() {
         return questionIds;
     }
 }

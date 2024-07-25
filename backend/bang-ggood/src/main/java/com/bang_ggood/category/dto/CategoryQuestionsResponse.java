7cbfcbbc2fe2b1c2ad0ed48c7601ee6cfccc6754
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-07-25 15:44:45 +0900

    feat: 사용자 체크리스트 조회 DTO 생성
    Co-Authored-By: tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>

--- backend/bang-ggood/src/main/java/com/bang_ggood/category/dto/CategoryQuestionsResponse.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/category/dto/CategoryQuestionsResponse.java
@@ -3,5 +3,5 @@ package com.bang_ggood.category.dto;
 import com.bang_ggood.checklist.dto.QuestionResponse;
 import java.util.List;
 
-public record CategoryQuestionsResponse(Integer id, String categoryName, List<QuestionResponse> questions) {
+public record CategoryQuestionsResponse(Integer categoryId, String categoryName, List<QuestionResponse> questions) {
 }

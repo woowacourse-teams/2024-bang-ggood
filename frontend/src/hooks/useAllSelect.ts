b6b7f8d58fa46c670db030464ad3ad5c5bbde054
ooherin <555ohr@naver.com>
2024-07-25 10:30:12 +0900

    fix: 불필요한 파일 삭제

--- frontend/src/hooks/useAllSelect.ts
+++
@@ -1,9 +0,0 @@
-// import { useEffect } from 'react';
-
-// const useAllSelect = )selectedOptions => {
-//   const [isAllSelected, setIsAllSelected] = useState(selectedOptions.length === totalOptionCount);
-
-//   useEffect(() => {}, [selectedOptions]);
-// };
-
-// export default useAllSelect;

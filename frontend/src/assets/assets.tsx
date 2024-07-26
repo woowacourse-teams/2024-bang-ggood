b13f8040e0de5311c9ded7269cbc6e624d7195f8
Hyelim Choi <HyelimChoi01@gmail.com>
2024-07-26 14:33:15 +0900

    chore: assets icons 모두 아이콘 폴더로 이동

--- frontend/src/assets/assets.tsx
+++ frontend/src/assets/assets.tsx
@@ -1,13 +1,16 @@
 /* Arrow */
-import ArrowBack from '@/assets/arrow-back.svg';
-import InputRequiredDot from '@/assets/input-required-dot.svg';
+import ArrowBack from '@/assets/icons/arrow/arrow-back.svg';
+import ArrowDownSmall from '@/assets/icons/arrow/arrow-down-small.svg';
+import ArrowRightGreen from '@/assets/icons/arrow/arrow-right-green.svg';
+import ArrowRightYellow from '@/assets/icons/arrow/arrow-right-yellow.svg';
+import ArrowUpSmall from '@/assets/icons/arrow/arrow-up-small.svg';
 
-/* face icon */
-import ArrowDownSmall from '@/assets/arrow-down-small.svg';
-import ArrowRightGreen from '@/assets/arrow-right-green.svg';
-import ArrowUpSmall from '@/assets/arrow-up-small.svg';
-import XIcon from '@/assets/delete.svg';
-import ArrowRightYellow from '@/assets/icons/arrow-right-yellow.svg';
+/*common-icons */
+import CheckIcon from '@/assets/icons/check.svg';
+import CloseIcon from '@/assets/icons/close.svg';
+import XIcon from '@/assets/icons/delete.svg';
+import LocationLineIcon from '@/assets/icons/location-lined.svg';
+import Plus from '@/assets/icons/plus.svg';
 
 /* face icon */
 import FaceIconBadGray from '@/assets/face-icon/face-icon-bad-gray.svg';
@@ -16,25 +19,23 @@ import FaceIconGoodGray from '@/assets/face-icon/face-icon-good-gray.svg';
 import FaceIconGood from '@/assets/face-icon/face-icon-good.svg';
 import FaceIconSosoGray from '@/assets/face-icon/face-icon-soso-gray.svg';
 import FaceIconSoso from '@/assets/face-icon/face-icon-soso.svg';
-import Plus from '@/assets/plus.svg';
+
 /* logos */
-import ChecklistLogoActive from '@/assets/logos/checklist-logo-active.svg';
-import ChecklistLogo from '@/assets/logos/checklist-logo.svg';
-import HomeLogoActive from '@/assets/logos/home-logo-active.svg';
-import HomeLogo from '@/assets/logos/home-logo.svg';
-import LocationLogoActive from '@/assets/logos/location-logo-active.svg';
-import LocationLogo from '@/assets/logos/location-logo.svg';
-import MyPageLogoActive from '@/assets/logos/my-page-logo-active.svg';
-import MyPageLogo from '@/assets/logos/my-page-logo.svg';
-/*common-icons */
-import CheckIcon from '@/assets/check.svg';
+import ChecklistLogoActive from '@/assets/icons/footer-icon/checklist-logo-active.svg';
+import ChecklistLogo from '@/assets/icons/footer-icon/checklist-logo.svg';
+import HomeLogoActive from '@/assets/icons/footer-icon/home-logo-active.svg';
+import HomeLogo from '@/assets/icons/footer-icon/home-logo.svg';
+import LocationLogoActive from '@/assets/icons/footer-icon/location-logo-active.svg';
+import LocationLogo from '@/assets/icons/footer-icon/location-logo.svg';
+import MyPageLogoActive from '@/assets/icons/footer-icon/my-page-logo-active.svg';
+import MyPageLogo from '@/assets/icons/footer-icon/my-page-logo.svg';
 
 /* etc */
 import BangBangIcon from '@/assets/icons/bang-bang-icon.svg';
-import CloseIcon from '@/assets/icons/close.svg';
+import InputRequiredDot from '@/assets/icons/input-required-dot.svg';
 import LampIcon from '@/assets/icons/lamp-icon.svg';
-import LocationLineIcon from '@/assets/icons/location-lined.svg';
-import QuestionDot from '@/assets/question-dot.svg';
+import QuestionDot from '@/assets/icons/question-dot.svg';
+
 export {
   ArrowBack,
   ArrowDownSmall,

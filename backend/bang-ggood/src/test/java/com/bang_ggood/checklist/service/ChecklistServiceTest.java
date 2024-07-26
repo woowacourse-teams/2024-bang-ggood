26db04f76666fbcba339f74355fede6bc04fa137
tkdgur0906 <tkdgur0906@naver.com>
2024-07-27 03:55:42 +0900

    test: 작성된 체크리스트 조회 기능 테스트

--- backend/bang-ggood/src/test/java/com/bang_ggood/checklist/service/ChecklistServiceTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/checklist/service/ChecklistServiceTest.java
@@ -1,18 +1,20 @@
 package com.bang_ggood.checklist.service;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import com.bang_ggood.IntegrationTestSupport;
 import com.bang_ggood.category.domain.Category;
 import com.bang_ggood.checklist.ChecklistFixture;
 import com.bang_ggood.checklist.dto.ChecklistQuestionsResponse;
+import com.bang_ggood.checklist.dto.WrittenChecklistResponse;
 import com.bang_ggood.exception.BangggoodException;
 import com.bang_ggood.exception.ExceptionCode;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ChecklistServiceTest extends IntegrationTestSupport {
 
@@ -26,7 +28,7 @@ class ChecklistServiceTest extends IntegrationTestSupport {
         long checklistId = checklistService.createChecklist(ChecklistFixture.CHECKLIST_CREATE_REQUEST);
 
         //then
-        assertThat(checklistId).isEqualTo(1);
+        assertThat(checklistId).isEqualTo(2);
     }
 
     @DisplayName("체크리스트 방 정보 작성 실패: 질문 id가 유효하지 않을 경우")
@@ -34,7 +36,7 @@ class ChecklistServiceTest extends IntegrationTestSupport {
     void createChecklist_invalidQuestionId_exception() {
         //given & when & then
         assertThatThrownBy(
-                        () -> checklistService.createChecklist(ChecklistFixture.CHECKLIST_CREATE_REQUEST_INVALID_QUESTION_ID))
+                () -> checklistService.createChecklist(ChecklistFixture.CHECKLIST_CREATE_REQUEST_INVALID_QUESTION_ID))
                 .isInstanceOf(BangggoodException.class)
                 .hasMessage(ExceptionCode.INVALID_QUESTION.getMessage());
     }
@@ -77,5 +79,27 @@ class ChecklistServiceTest extends IntegrationTestSupport {
 
         // then
         assertThat(checklistQuestionsResponse.categories().size()).isEqualTo(Category.values().length);
+    }
+
+    @DisplayName("작성된 체크리스트 조회 성공")
+    @Test
+    void readChecklistById() {
+        // given & when
+        WrittenChecklistResponse writtenChecklistResponse = checklistService.readChecklistById(1L);
+
+        // then
+        Assertions.assertAll(
+                () -> assertThat(writtenChecklistResponse.room().name()).isEqualTo("살기 좋은 방"),
+                () -> assertThat(writtenChecklistResponse.room().address()).isEqualTo("인천광역시 부평구")
+        );
+    }
+
+    @DisplayName("작성된 체크리스트 조회 실패 : 체크리스트가 존재하지 않는 id인 경우")
+    @Test
+    void readChecklistById_invalidChecklistId_exception() {
+        // given & when & then
+        assertThatThrownBy(() -> checklistService.readChecklistById(0))
+                .isInstanceOf(BangggoodException.class)
+                .hasMessage(ExceptionCode.CHECKLIST_NOT_FOUND.getMessage());
     }
 }

bb6e0a69a7fb7a26a88ae98ddbfa2bfeeef27f17
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-26 14:03:28 +0900

    feat: Footer 사용시 앞에 / 쓸때와 안쓸때 둘다 사용가능하게 변경. (전역 상수 참조가능하게 변경)

--- frontend/src/constants/routePath.ts
+++ frontend/src/constants/routePath.ts
@@ -1,7 +1,11 @@
 export const ROUTE_PATH = {
+  root: '/',
   checklistNew: '/checklist-new',
   checklistList: '/checklist-list',
   checklistOne: (id: number) => `/checklists/${id}`,
   categoryChoose: '/category-choose',
   roomCompare: '/room-compare',
+  /* TODO : 추후 추가될 페이지들의 라우트패스 */
+  location: '/location',
+  myPage: '/my-page',
 };

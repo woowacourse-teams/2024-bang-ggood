099b8c93b35826e47c5b65c26a52523f5349f551
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-07-27 16:32:59 +0900

    feat: response dto에 정적 팩토리 메서드 구현

--- backend/bang-ggood/src/main/java/com/bang_ggood/category/dto/CategoryQuestionsResponse.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/category/dto/CategoryQuestionsResponse.java
@@ -1,7 +1,12 @@
 package com.bang_ggood.category.dto;
 
+import com.bang_ggood.category.domain.Category;
 import com.bang_ggood.checklist.dto.QuestionResponse;
 import java.util.List;
 
 public record CategoryQuestionsResponse(Integer categoryId, String categoryName, List<QuestionResponse> questions) {
+
+    public static CategoryQuestionsResponse of(Category category, List<QuestionResponse> questions) {
+        return new CategoryQuestionsResponse(category.getId(), category.getDescription(), questions);
+    }
 }
--- backend/bang-ggood/src/main/java/com/bang_ggood/checklist/dto/QuestionResponse.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/checklist/dto/QuestionResponse.java
@@ -1,4 +1,11 @@
 package com.bang_ggood.checklist.dto;
 
+import com.bang_ggood.category.domain.Badge;
+import com.bang_ggood.checklist.domain.Question;
+
 public record QuestionResponse(Integer questionId, String title, String subtitle) {
+
+    public static QuestionResponse of(Question question) {
+        return new QuestionResponse(question.getId(), question.getTitle(), question.getSubtitle());
+    }
 }

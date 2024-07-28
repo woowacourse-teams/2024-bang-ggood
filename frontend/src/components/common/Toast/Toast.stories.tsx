7965f1f4e69129994fb9ec40b21a011527a45fc2
skiende74 <skiende74@gmail.com>
2024-07-28 20:15:02 +0900

    feat: storybook에 반영

--- frontend/src/components/common/Toast/Toast.stories.tsx
+++ frontend/src/components/common/Toast/Toast.stories.tsx
@@ -5,7 +5,7 @@ import Toast from '@/components/common/Toast/Toast';
 import { ToastProvider, useToastContext } from '@/components/common/Toast/ToastContext';
 
 /**
- * 'Toast는 사용자에게 일정시간 동안 알림을 주는 컴포넌트입니다.'
+ * Toast는 사용자에게 일정시간 동안 알림을 주는 컴포넌트입니다.
  */
 const meta = {
   title: 'components/Toast',
@@ -24,13 +24,8 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   render: () => {
-    const { showToast } = useToastContext();
-
-    return (
-      <>
-        <Button label={'토스트 생성'} onClick={() => showToast('토스트가 생성되었습니다.')} />
-      </>
-    );
+    const { showToast, setDuration } = useToastContext();
+    setDuration(0.5);
+    return <Button label={'토스트 생성'} onClick={() => showToast('토스트가 생성되었습니다.')} />;
   },
-  args: { duration: 3 },
 };

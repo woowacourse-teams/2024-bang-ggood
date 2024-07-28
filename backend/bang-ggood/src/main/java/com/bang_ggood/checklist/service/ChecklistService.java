f3f4c230aa7ad74fe5ae7a5317deb116870deb71
tkdgur0906 <tkdgur0906@naver.com>
2024-07-29 00:55:06 +0900

    refactor: 주어진 질문에서 특정 카테고리에 맞는 질문을 반환하는 기능 리팩토링

--- backend/bang-ggood/src/main/java/com/bang_ggood/checklist/service/ChecklistService.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/checklist/service/ChecklistService.java
@@ -1,45 +1,23 @@
 package com.bang_ggood.checklist.service;
 
 import com.bang_ggood.category.domain.Category;
-import com.bang_ggood.category.dto.CategoryQuestionsResponse;
 import com.bang_ggood.category.dto.WrittenCategoryQuestionsResponse;
 import com.bang_ggood.checklist.domain.Checklist;
 import com.bang_ggood.checklist.domain.ChecklistOption;
 import com.bang_ggood.checklist.domain.ChecklistQuestion;
-import com.bang_ggood.checklist.domain.Option;
 import com.bang_ggood.checklist.domain.Question;
-import com.bang_ggood.checklist.domain.Questionlist;
-import com.bang_ggood.checklist.dto.BadgeResponse;
-import com.bang_ggood.checklist.dto.CategoryScoreReadResponse;
-import com.bang_ggood.checklist.dto.ChecklistComparisonReadResponse;
-import com.bang_ggood.checklist.dto.ChecklistCreateRequest;
-import com.bang_ggood.checklist.dto.ChecklistInfo;
-import com.bang_ggood.checklist.dto.ChecklistQuestionsResponse;
-import com.bang_ggood.checklist.dto.ChecklistsComparisonReadResponse;
-import com.bang_ggood.checklist.dto.QuestionCreateRequest;
-import com.bang_ggood.checklist.dto.QuestionResponse;
-import com.bang_ggood.checklist.dto.UserChecklistPreviewResponse;
-import com.bang_ggood.checklist.dto.UserChecklistsPreviewResponse;
 import com.bang_ggood.checklist.dto.WrittenChecklistResponse;
 import com.bang_ggood.checklist.dto.WrittenQuestionResponse;
 import com.bang_ggood.checklist.repository.ChecklistOptionRepository;
 import com.bang_ggood.checklist.repository.ChecklistQuestionRepository;
 import com.bang_ggood.checklist.repository.ChecklistRepository;
-import com.bang_ggood.exception.BangggoodException;
-import com.bang_ggood.exception.ExceptionCode;
-import com.bang_ggood.room.domain.Room;
 import com.bang_ggood.room.dto.WrittenRoomResponse;
 import com.bang_ggood.room.repository.RoomRepository;
-import com.bang_ggood.user.domain.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -60,127 +38,127 @@ public class ChecklistService {
     }
 
 
-    @Transactional
-    public long createChecklist(ChecklistCreateRequest checklistCreateRequest) {
-        Room room = roomRepository.save(checklistCreateRequest.toRoomEntity());
-
-        ChecklistInfo checklistInfo = checklistCreateRequest.toChecklistInfo();
-        Checklist checklist = new Checklist(new User(1L, "방방이"), room, checklistInfo.deposit(), checklistInfo.rent(),
-                checklistInfo.contractTerm(), checklistInfo.realEstate());
-        checklistRepository.save(checklist);
-
-        createChecklistOptions(checklistCreateRequest, checklist);
-        createChecklistQuestions(checklistCreateRequest, checklist);
-        return checklist.getId();
-    }
-
-    private void createChecklistOptions(ChecklistCreateRequest checklistCreateRequest, Checklist checklist) {
-        List<Integer> optionIds = checklistCreateRequest.options();
-        validateOptions(optionIds);
-        List<ChecklistOption> checklistOptions = optionIds.stream()
-                .map(option -> new ChecklistOption(checklist, option))
-                .toList();
-        checklistOptionRepository.saveAll(checklistOptions);
-    }
-
-    private void validateOptions(List<Integer> optionIds) {
-        validateOptionDuplicate(optionIds);
-        validateOptionInvalid(optionIds);
-    }
-
-    private void validateOptionDuplicate(List<Integer> optionIds) {
-        Set<Integer> set = new HashSet<>();
-        optionIds.forEach(id -> {
-            if (!set.add(id)) {
-                throw new BangggoodException(ExceptionCode.OPTION_DUPLICATED);
-            }
-        });
-    }
-
-    private void validateOptionInvalid(List<Integer> optionIds) {
-        for (Integer optionId : optionIds) {
-            if (!Option.contains(optionId)) {
-                throw new BangggoodException(ExceptionCode.INVALID_OPTION);
-            }
-        }
-    }
-
-    private void createChecklistQuestions(ChecklistCreateRequest checklistCreateRequest, Checklist checklist) {
-        List<QuestionCreateRequest> questions = checklistCreateRequest.questions();
-        validateQuestion(questions);
-        for (QuestionCreateRequest questionCreateRequest : questions) {
-            Integer questionId = questionCreateRequest.questionId();
-            ChecklistQuestion checklistQuestion = new ChecklistQuestion(checklist, questionId,
-                    questionCreateRequest.answer());
-            checklistQuestionRepository.save(checklistQuestion);
-        }
-    }
-
-    private void validateQuestion(List<QuestionCreateRequest> questions) {
-        validateQuestionDuplicate(questions);
-        validateQuestionInvalid(questions);
-    }
-
-    private void validateQuestionDuplicate(List<QuestionCreateRequest> questions) {
-        Set<Integer> set = new HashSet<>();
-        questions.forEach(question -> {
-            if (!set.add(question.questionId())) {
-                throw new BangggoodException(ExceptionCode.QUESTION_DUPLICATED);
-            }
-        });
-    }
-
-    private void validateQuestionInvalid(List<QuestionCreateRequest> questions) {
-        for (QuestionCreateRequest questionCreateRequest : questions) {
-            if (!Questionlist.contains(questionCreateRequest.questionId())) {
-                throw new BangggoodException(ExceptionCode.INVALID_QUESTION);
-            }
-        }
-    }
-
-    @Transactional
-    public UserChecklistsPreviewResponse readUserChecklistsPreview() {
-        User user = new User(1L, "방방이");
-        List<Checklist> checklists = checklistRepository.findByUser(user);
-
-        List<UserChecklistPreviewResponse> responses = checklists.stream()
-                .map(checklist -> UserChecklistPreviewResponse.of(
-                        checklist,
-                        createBadges(checklist.getQuestions())))
-                .toList();
-
-        return new UserChecklistsPreviewResponse(responses);
-    }
-
-    private List<BadgeResponse> createBadges(List<ChecklistQuestion> questions) {
-        return Category.getBadges(questions).stream()
-                .map(BadgeResponse::from)
-                .toList();
-    }
-
-    @Transactional
-    public ChecklistQuestionsResponse readChecklistQuestions() {
-        List<CategoryQuestionsResponse> categoryQuestionsResponses = new ArrayList<>();
-        for (Category category : Category.values()) {
-            CategoryQuestionsResponse categoryQuestionsResponse =
-                    new CategoryQuestionsResponse(category.getId(), category.getDescription(),
-                            readChecklistQuestion(category));
-            categoryQuestionsResponses.add(categoryQuestionsResponse);
-        }
-        return new ChecklistQuestionsResponse(categoryQuestionsResponses);
-    }
-
-    private List<QuestionResponse> readChecklistQuestion(Category category) {
-        List<Question> questions = Questionlist.getQuestionsByCategoryId(category.getId());
-        return questions.stream()
-                .map(question -> {
-                    int questionId = Questionlist.getQuestionIdByQuestion(question);
-                    String title = Questionlist.getTitleByQuestionId(questionId);
-                    String subtitle = Questionlist.getSubtitleByQuestionId(questionId);
-                    return new QuestionResponse(questionId, title, subtitle);
-                })
-                .toList();
-    }
+//    @Transactional
+//    public long createChecklist(ChecklistCreateRequest checklistCreateRequest) {
+//        Room room = roomRepository.save(checklistCreateRequest.toRoomEntity());
+//
+//        ChecklistInfo checklistInfo = checklistCreateRequest.toChecklistInfo();
+//        Checklist checklist = new Checklist(new User(1L, "방방이"), room, checklistInfo.deposit(), checklistInfo.rent(),
+//                checklistInfo.contractTerm(), checklistInfo.realEstate());
+//        checklistRepository.save(checklist);
+//
+//        createChecklistOptions(checklistCreateRequest, checklist);
+//        createChecklistQuestions(checklistCreateRequest, checklist);
+//        return checklist.getId();
+//    }
+//
+//    private void createChecklistOptions(ChecklistCreateRequest checklistCreateRequest, Checklist checklist) {
+//        List<Integer> optionIds = checklistCreateRequest.options();
+//        validateOptions(optionIds);
+//        List<ChecklistOption> checklistOptions = optionIds.stream()
+//                .map(option -> new ChecklistOption(checklist, option))
+//                .toList();
+//        checklistOptionRepository.saveAll(checklistOptions);
+//    }
+//
+//    private void validateOptions(List<Integer> optionIds) {
+//        validateOptionDuplicate(optionIds);
+//        validateOptionInvalid(optionIds);
+//    }
+//
+//    private void validateOptionDuplicate(List<Integer> optionIds) {
+//        Set<Integer> set = new HashSet<>();
+//        optionIds.forEach(id -> {
+//            if (!set.add(id)) {
+//                throw new BangggoodException(ExceptionCode.OPTION_DUPLICATED);
+//            }
+//        });
+//    }
+//
+//    private void validateOptionInvalid(List<Integer> optionIds) {
+//        for (Integer optionId : optionIds) {
+//            if (!Option.contains(optionId)) {
+//                throw new BangggoodException(ExceptionCode.INVALID_OPTION);
+//            }
+//        }
+//    }
+//
+//    private void createChecklistQuestions(ChecklistCreateRequest checklistCreateRequest, Checklist checklist) {
+//        List<QuestionCreateRequest> questions = checklistCreateRequest.questions();
+//        validateQuestion(questions);
+//        for (QuestionCreateRequest questionCreateRequest : questions) {
+//            Integer questionId = questionCreateRequest.questionId();
+//            ChecklistQuestion checklistQuestion = new ChecklistQuestion(checklist, questionId,
+//                    questionCreateRequest.answer());
+//            checklistQuestionRepository.save(checklistQuestion);
+//        }
+//    }
+//
+//    private void validateQuestion(List<QuestionCreateRequest> questions) {
+//        validateQuestionDuplicate(questions);
+//        validateQuestionInvalid(questions);
+//    }
+//
+//    private void validateQuestionDuplicate(List<QuestionCreateRequest> questions) {
+//        Set<Integer> set = new HashSet<>();
+//        questions.forEach(question -> {
+//            if (!set.add(question.questionId())) {
+//                throw new BangggoodException(ExceptionCode.QUESTION_DUPLICATED);
+//            }
+//        });
+//    }
+//
+//    private void validateQuestionInvalid(List<QuestionCreateRequest> questions) {
+//        for (QuestionCreateRequest questionCreateRequest : questions) {
+//            if (!Questionlist.contains(questionCreateRequest.questionId())) {
+//                throw new BangggoodException(ExceptionCode.INVALID_QUESTION);
+//            }
+//        }
+//    }
+//
+//    @Transactional
+//    public UserChecklistsPreviewResponse readUserChecklistsPreview() {
+//        User user = new User(1L, "방방이");
+//        List<Checklist> checklists = checklistRepository.findByUser(user);
+//
+//        List<UserChecklistPreviewResponse> responses = checklists.stream()
+//                .map(checklist -> UserChecklistPreviewResponse.of(
+//                        checklist,
+//                        createBadges(checklist.getQuestions())))
+//                .toList();
+//
+//        return new UserChecklistsPreviewResponse(responses);
+//    }
+//
+//    private List<BadgeResponse> createBadges(List<ChecklistQuestion> questions) {
+//        return Category.getBadges(questions).stream()
+//                .map(BadgeResponse::from)
+//                .toList();
+//    }
+//
+//    @Transactional
+//    public ChecklistQuestionsResponse readChecklistQuestions() {
+//        List<CategoryQuestionsResponse> categoryQuestionsResponses = new ArrayList<>();
+//        for (Category category : Category.values()) {
+//            CategoryQuestionsResponse categoryQuestionsResponse =
+//                    new CategoryQuestionsResponse(category.getId(), category.getDescription(),
+//                            readChecklistQuestion(category));
+//            categoryQuestionsResponses.add(categoryQuestionsResponse);
+//        }
+//        return new ChecklistQuestionsResponse(categoryQuestionsResponses);
+//    }
+//
+//    private List<QuestionResponse> readChecklistQuestion(Category category) {
+//        List<Question> questions = Questionlist.getQuestionsByCategoryId(category.getId());
+//        return questions.stream()
+//                .map(question -> {
+//                    int questionId = Questionlist.getQuestionIdByQuestion(question);
+//                    String title = Questionlist.getTitleByQuestionId(questionId);
+//                    String subtitle = Questionlist.getSubtitleByQuestionId(questionId);
+//                    return new QuestionResponse(questionId, title, subtitle);
+//                })
+//                .toList();
+//    }
 
     @Transactional
     public WrittenChecklistResponse readChecklistById(long id) {
@@ -211,65 +189,61 @@ public class ChecklistService {
 
     private WrittenCategoryQuestionsResponse readQuestionsByCategory(Category category,
                                                                      List<ChecklistQuestion> checklistQuestions) {
-        List<WrittenQuestionResponse> writtenQuestionResponses = new ArrayList<>();
-        for (ChecklistQuestion checklistQuestion : checklistQuestions) {
-            int questionId = checklistQuestion.getQuestionId();
-            if (Questionlist.isQuestionsInCategory(questionId, category.getId())) {
-                writtenQuestionResponses.add(
-                        new WrittenQuestionResponse(questionId, Questionlist.getTitleByQuestionId(questionId),
-                                Questionlist.getSubtitleByQuestionId(questionId), checklistQuestion.getAnswer()));
-            }
-        }
-        return new WrittenCategoryQuestionsResponse(category.getId(), category.getDescription(),
-                writtenQuestionResponses);
+
+        List<WrittenQuestionResponse> writtenQuestionResponses =
+                Question.filter(category, checklistQuestions).stream()
+                        .map(WrittenQuestionResponse::of)
+                        .collect(Collectors.toList());
+
+        return WrittenCategoryQuestionsResponse.of(category, writtenQuestionResponses);
     }
 
 
-    @Transactional
-    public ChecklistsComparisonReadResponse readChecklistsComparison(List<Long> checklistIds) {
-        User user = new User(1L, "방끗");
-
-        List<ChecklistComparisonReadResponse> responses = checklistRepository.findByUserAndIdIn(user, checklistIds)
-                .stream()
-                .map(checklist -> {
-                    // 카테고리별 총점
-                    List<CategoryScoreReadResponse> categoryScores = calculateCategoryScores(checklist);
-
-                    // 체크리스트 총점
-                    int checklistScore = calculateChecklistScore(categoryScores);
-
-                    // 옵션 개수
-                    int checklistOptionCount = checklistOptionRepository.countByChecklist(checklist);
-
-                    return ChecklistComparisonReadResponse.of(
-                            checklist, checklistOptionCount, checklistScore, categoryScores);
-                })
-                .sorted(Comparator.comparing(ChecklistComparisonReadResponse::score).reversed())
-                .toList();
-
-        return new ChecklistsComparisonReadResponse(responses);
-    }
-
-    private List<CategoryScoreReadResponse> calculateCategoryScores(Checklist checklist) {
-        List<CategoryScoreReadResponse> categoryScores = new ArrayList<>();
-
-        for (Category category : Category.values()) {
-            int categoryScore = category.calculateTotalScore(checklist.getQuestions());
-            if (categoryScore != 0) {
-                categoryScores.add(new CategoryScoreReadResponse(
-                        category.getId(),
-                        category.getDescription(),
-                        categoryScore
-                ));
-            }
-        }
-
-        return categoryScores;
-    }
-
-    private int calculateChecklistScore(List<CategoryScoreReadResponse> categoryScores) {
-        return categoryScores.stream()
-                .mapToInt(CategoryScoreReadResponse::score)
-                .sum() / categoryScores.size();
-    }
+//    @Transactional
+//    public ChecklistsComparisonReadResponse readChecklistsComparison(List<Long> checklistIds) {
+//        User user = new User(1L, "방끗");
+//
+//        List<ChecklistComparisonReadResponse> responses = checklistRepository.findByUserAndIdIn(user, checklistIds)
+//                .stream()
+//                .map(checklist -> {
+//                    // 카테고리별 총점
+//                    List<CategoryScoreReadResponse> categoryScores = calculateCategoryScores(checklist);
+//
+//                    // 체크리스트 총점
+//                    int checklistScore = calculateChecklistScore(categoryScores);
+//
+//                    // 옵션 개수
+//                    int checklistOptionCount = checklistOptionRepository.countByChecklist(checklist);
+//
+//                    return ChecklistComparisonReadResponse.of(
+//                            checklist, checklistOptionCount, checklistScore, categoryScores);
+//                })
+//                .sorted(Comparator.comparing(ChecklistComparisonReadResponse::score).reversed())
+//                .toList();
+//
+//        return new ChecklistsComparisonReadResponse(responses);
+//    }
+//
+//    private List<CategoryScoreReadResponse> calculateCategoryScores(Checklist checklist) {
+//        List<CategoryScoreReadResponse> categoryScores = new ArrayList<>();
+//
+//        for (Category category : Category.values()) {
+//            int categoryScore = category.calculateTotalScore(checklist.getQuestions());
+//            if (categoryScore != 0) {
+//                categoryScores.add(new CategoryScoreReadResponse(
+//                        category.getId(),
+//                        category.getName(),
+//                        categoryScore
+//                ));
+//            }
+//        }
+//
+//        return categoryScores;
+//    }
+//
+//    private int calculateChecklistScore(List<CategoryScoreReadResponse> categoryScores) {
+//        return categoryScores.stream()
+//                .mapToInt(CategoryScoreReadResponse::score)
+//                .sum() / categoryScores.size();
+//    }
 }

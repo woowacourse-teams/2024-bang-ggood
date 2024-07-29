3a90e567318385950e168f1dc16fba1d4e6d4012
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-29 20:02:53 +0900

    fix: reset css에서 input reset 은 text에만 적용되게 좁힘

--- frontend/src/styles/reset.ts
+++ frontend/src/styles/reset.ts
@@ -171,7 +171,7 @@ export const normalize = css`
 
   /* input */
   /* input 기본 스타일 초기화 */
-  input {
+  input[type='text'] {
     -webkit-appearance: none;
     -moz-appearance: none;
     appearance: none;

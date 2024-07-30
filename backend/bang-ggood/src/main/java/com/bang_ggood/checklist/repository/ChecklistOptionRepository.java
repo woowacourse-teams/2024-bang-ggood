aa130adf2ce84362b085f18e9f7022fe7697856b
jinwoo22 <jenny010925@naver.com>
2024-07-30 11:30:54 +0900

    fix: 머지 실패 시 오류 해결

--- backend/bang-ggood/src/main/java/com/bang_ggood/checklist/repository/ChecklistOptionRepository.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/checklist/repository/ChecklistOptionRepository.java
@@ -2,15 +2,12 @@ package com.bang_ggood.checklist.repository;
 
 import com.bang_ggood.checklist.domain.Checklist;
 import com.bang_ggood.checklist.domain.ChecklistOption;
-import com.bang_ggood.checklist.domain.Option;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ChecklistOptionRepository extends JpaRepository<ChecklistOption, Long> {
 
     List<ChecklistOption> findByChecklistId(long checklistId);
 
     Integer countByChecklist(Checklist checklist);
-
-    boolean existsIdIsIn(List<Integer> ids);
 }

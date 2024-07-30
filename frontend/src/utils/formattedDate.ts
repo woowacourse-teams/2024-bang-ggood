901ac4af1356b700f83e4a266e454c4a93871314
Hyelim Choi <74346290+healim01@users.noreply.github.com>
2024-07-30 14:07:07 +0900

    [FE] 체크리스트 리스트 페이지 내 코드 리팩토링 및 시간 포맷팅을 추가한다. (#175)
    * feat: 시간 포맷팅 유틸로 24시간 이내 작성 시 `~시간 전`으로 표시
    
    * refactor: 구조분해 할당으로 코드 수정
    
    * chore: 잘못 올라간 콘솔로그 삭제

--- frontend/src/utils/formattedDate.ts
+++ frontend/src/utils/formattedDate.ts
@@ -1,5 +1,9 @@
 const formattedDate = (stringDate: string) => {
   const date = new Date(stringDate);
+  const nowDate = new Date();
+  const timeDifference = Math.floor((nowDate.getTime() - date.getTime()) / (1000 * 60 * 60));
+
+  if (timeDifference < 24) return `${timeDifference}시간 전`;
   return date.toISOString().slice(2, 10).replace(/-/g, '/');
 };
 

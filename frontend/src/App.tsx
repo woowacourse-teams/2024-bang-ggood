59a8515e6bc7481301b8675ad7d76df420454d17
ooherin <555ohr@naver.com>
2024-07-30 14:15:25 +0900

    chore: tanstack-query 설치 및 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { Global, ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 
 import { ToastProvider } from '@/components/common/Toast/ToastContext';
@@ -7,13 +9,17 @@ import { baseStyle } from '@/styles/global';
 import theme from '@/styles/theme';
 
 const App = () => {
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider theme={theme}>
-      <ToastProvider>
-        <Global styles={baseStyle} />
-        <RouterProvider router={router} />
-      </ToastProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <ToastProvider>
+          <Global styles={baseStyle} />
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ToastProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 

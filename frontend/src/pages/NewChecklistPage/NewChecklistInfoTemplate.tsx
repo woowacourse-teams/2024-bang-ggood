252e213bc50ef7a05ceea8e558206c823ffa8126
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-30 18:18:36 +0900

    임시커밋

--- frontend/src/pages/NewChecklistPage/NewChecklistInfoTemplate.tsx
+++ frontend/src/pages/NewChecklistPage/NewChecklistInfoTemplate.tsx
@@ -1,4 +1,3 @@
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { useState } from 'react';
 
@@ -30,22 +29,29 @@ const NewChecklistInfoTemplate = ({
   return (
     <S.ContentWrapper>
       <S.Container>
-        {/* 스타일링이 매우 가변적이어서, 불가피하게 유틸함수를 부분적으로 사용 */}
-        {makeCustomForm({
-          label: '방 이름',
-          onChange: onChangeForForm,
-          name: 'name',
-          values: roomInfo,
-          required: true,
-        })}
+        {/* 방이름 */}
+        <CustomFormField label="방 이름" onChange={onChangeForForm} name="name" values={roomInfo} required={true} />
+        {/* 주소 */}
         <FormField>
-          <FormField.Label label="보증금 / 월세" />
           <S.FlexVertical>
-            <S.FlexHorizontal
-              css={css`
-                gap: 0;
-              `}
-            >
+            <FormField.Label label="주소" />
+            <S.FlexHorizontal gap={'6%'}>
+              <S.CustomInput onChange={onChangeForForm} name="address" value={roomInfo.address} />
+              <S.AddressButton> 주소 찾기</S.AddressButton>
+            </S.FlexHorizontal>
+            <FormField.P value="" />
+          </S.FlexVertical>
+        </FormField>
+        {/* 교통편 */}
+        <S.FlexVertical>
+          <FormField.Label label="가까운 교통편" />
+          <FormField.P value="주소를 추가하면 가까운 역을 찾아드려요!" />
+        </S.FlexVertical>
+        {/* 보증금 월세 */}
+        <FormField>
+          <S.FlexVertical>
+            <FormField.Label label="보증금 / 월세" />
+            <S.FlexHorizontal gap={0}>
               <S.CustomInput placeholder="" onChange={onChangeForForm} name="deposit" value={roomInfo.deposit} />
               <S.CustomLabel label=" 만원   " />
               <S.CustomInput placeholder="" onChange={onChangeForForm} name="rent" value={roomInfo.rent} />
@@ -53,36 +59,29 @@ const NewChecklistInfoTemplate = ({
             <FormField.P value="" />
           </S.FlexVertical>
         </FormField>
+        {/* 방 종류 */}
+        <S.FlexVertical>
+          <FormField.Label label="방 종류" />
+          <select>
+            <option></option>
+            <option></option>
+            <option></option>
+            <option></option>
+          </select>
+        </S.FlexVertical>
+        {/* 방 크기 */}
+        {/* 층수 */}
+        {/* 계약 기간 */}
         <S.FlexHorizontal>
-          {makeCustomForm({
-            label: '계약 기간(년)',
-            values: roomInfo,
-            name: 'contractTerm',
-            onChange: onChangeForForm,
-          })}
-          {makeCustomForm({ label: '층수', values: roomInfo, name: 'floor', onChange: onChangeForForm })}
+          <CustomFormField label="계약 기간(개월)" values={roomInfo} name="contractTerm" onChange={onChangeForForm} />
+          <CustomFormField label="층수" values={roomInfo} name="floor" onChange={onChangeForForm} />
         </S.FlexHorizontal>
-        <FormField>
-          <FormField.Label label="가까운 교통편" />
-          <S.FlexHorizontal
-            css={css`
-              gap: 0;
-            `}
-          >
-            <S.CustomInput placeholder="지하철역" onChange={onChangeForForm} name="station" value={roomInfo.station} />
-            <S.CustomLabel label=" 까지   " />
-            <S.CustomInput
-              placeholder="분"
-              onChange={onChangeForForm}
-              name="walkingTime"
-              value={roomInfo.walkingTime}
-            />
-          </S.FlexHorizontal>
-          <FormField.P value="" />
-        </FormField>
-        {makeCustomForm({ label: '부동산 이름', onChange: onChangeForForm, values: roomInfo, name: 'realEstate' })}
-        <S.SubmitButton label="가구 옵션 추가하기" size="full" onClick={onClickOptionModalOpen} />
+        {/* 부동산 이름 */}
+        <CustomFormField label="부동산 이름" onChange={onChangeForForm} values={roomInfo} name="realEstate" />
 
+        {/* 가구옵션 */}
+        {/* 가구옵션버튼 */}
+        <S.AddOptionButton label="가구 옵션 추가하기" size="full" onClick={onClickOptionModalOpen} />
         {/*옵션 선택 모달*/}
         {isOptionModalOpen && (
           <OptionModal
@@ -97,10 +96,10 @@ const NewChecklistInfoTemplate = ({
   );
 };
 
-const makeCustomForm = (res: MakeFormArgs) => (
-  <S.CustomFormField key={res.label}>
-    <FormField.Label label={res.label} required={res.required} />
-    <FormField.Input placeholder="" width="full" onChange={res.onChange} name={res.name} value={res.values[res.name]} />
+const CustomFormField = ({ label, name, values, required, onChange }: MakeFormArgs) => (
+  <S.CustomFormField key={label}>
+    <FormField.Label label={label} required={required} />
+    <FormField.Input placeholder="" width="full" onChange={onChange} name={name} value={values[name]} />
     <FormField.P value={''} />
   </S.CustomFormField>
 );
@@ -122,9 +121,8 @@ const S = {
   Container: styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
+    justify-content: start;
     row-gap: 24px;
-    justify-content: 'center';
     padding: 28px 22px;
   `,
   Center: styled.div`
@@ -136,7 +134,10 @@ const S = {
     font-weight: ${({ theme }) => theme.text.weight.bold};
     font-size: ${({ theme }) => theme.text.size.large};
   `,
-  SubmitButton: styled(Button)`
+  AddressButton: styled.button`
+    background-color: ${({ theme }) => theme.palette.grey300};
+  `,
+  AddOptionButton: styled(Button)`
     width: 100%;
     border-radius: 4px;
 
@@ -148,16 +149,15 @@ const S = {
 
     color: black;
   `,
-  FlexHorizontal: styled.div`
+  FlexHorizontal: styled.div<{ gap?: number | string }>`
     ${flexRow}
     justify-content: space-between;
-    column-gap: 6%;
+    column-gap: ${({ gap }) => gap ?? '6%'};
     width: 100%;
   `,
-  FlexVertical: styled.div`
+  FlexVertical: styled.div<{ gap?: number }>`
     ${flexColumn}
-    row-gap: 2%;
-
+    row-gap: ${({ gap }) => gap ?? '10px'};
     flex: auto;
   `,
   CustomInput: styled(FormField.Input)`
@@ -174,6 +174,9 @@ const S = {
   `,
   CustomFormField: styled(FormField)`
     flex: auto;
+    display: flex;
+    flex-direction: column;
+    row-gap: 10px;
   `,
 };
 

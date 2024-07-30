e1e8ef6fa564817c9a96e72b08ba4cb2005820f7
Hyelim Choi <HyelimChoi01@gmail.com>
2024-07-30 11:11:20 +0900

    feat: 시간 포맷팅 유틸로 24시간 이내 작성 시 `~시간 전`으로 표시

--- frontend/src/utils/formattedDate.ts
+++ frontend/src/utils/formattedDate.ts
@@ -1,5 +1,10 @@
 const formattedDate = (stringDate: string) => {
   const date = new Date(stringDate);
+  const nowDate = new Date();
+  const timeDifference = Math.floor((nowDate.getTime() - date.getTime()) / (1000 * 60 * 60));
+  console.log(timeDifference);
+
+  if (timeDifference < 24) return `${timeDifference}시간 전`;
   return date.toISOString().slice(2, 10).replace(/-/g, '/');
 };
 

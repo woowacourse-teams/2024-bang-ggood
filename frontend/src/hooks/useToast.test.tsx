74719cf349523501e71312d28ed258d855054826
ooherin <555ohr@naver.com>
2024-07-31 13:07:01 +0900

    fix: toast관련 테스트를  훅 테스트로 수정

--- frontend/src/hooks/useToast.test.tsx
+++ frontend/src/hooks/useToast.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 
 import { ThemeProvider } from '@emotion/react';
-import { render, renderHook, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { render, renderHook, waitFor } from '@testing-library/react';
 import { act } from 'react';
 
 import Toast from '@/components/common/Toast/Toast';
@@ -22,27 +22,27 @@ describe('useToast 테스트', () => {
     const TOAST_DURATION = 10;
     const TOAST_DURATION_MINUTE = TOAST_DURATION / 1000;
 
-    it(`토스트에 메세지를 적으면 토스트가 ${TOAST_DURATION_MINUTE}초간 나타난다.`, async () => {
+    it(`토스트에 메세지를 적으면 토스트가 메세지가 설정된다.`, async () => {
       const { result } = renderHook(() => useToast(TOAST_DURATION_MINUTE));
 
       act(() => {
         result.current.showToast(TOAST_MESSAGE);
       });
 
-      expect(screen.getByText(TOAST_MESSAGE)).toBeInTheDocument();
+      expect(result.current.toast).toBe(TOAST_MESSAGE);
     });
 
-    it(`토스트에 메세지를 적은 후 ${TOAST_DURATION_MINUTE}초 뒤에 토스트가 사라진다.`, async () => {
+    it(`토스트를 나오게 한 후 ${TOAST_DURATION_MINUTE}초 뒤에 토스트의 메세지 state가 null이 된다.`, async () => {
       const { result } = renderHook(() => useToast(TOAST_DURATION_MINUTE));
 
       act(() => {
         result.current.showToast(TOAST_MESSAGE);
       });
-      expect(screen.getByText(TOAST_MESSAGE)).toBeInTheDocument();
-      await waitForElementToBeRemoved(() => screen.getByText(TOAST_MESSAGE), { timeout: TOAST_DURATION + 1 });
+      expect(result.current.toast).toBe(TOAST_MESSAGE);
+      await waitFor(() => expect(result.current.toast).toBe(null), { timeout: TOAST_DURATION + 1 });
     });
 
-    it(`토스트 호출을 두번하면 이후 메세지로 토스트가 작성된다`, async () => {
+    it(`토스트 호출을 두번하면 이후 메세지로 토스가 바뀐다.`, async () => {
       const { result } = renderHook(() => useToast(TOAST_DURATION_MINUTE));
       const CHANGED_TOAST_MESSAGE = '토스트가 바뀌었다.';
 
@@ -52,7 +52,6 @@ describe('useToast 테스트', () => {
       });
 
       expect(result.current.toast).toBe(CHANGED_TOAST_MESSAGE);
-      expect(screen.getByText(CHANGED_TOAST_MESSAGE)).toBeInTheDocument();
     });
   });
 });

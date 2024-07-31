28ecd78c400b9515679ac58ba214212c93bad12f
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-07-31 14:45:06 +0900

    feat: 체크리스트 방 정보 작성 DTO 필드 추가

--- backend/bang-ggood/src/main/java/com/bang_ggood/room/dto/request/RoomCreateRequest.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/room/dto/request/RoomCreateRequest.java
@@ -4,11 +4,12 @@ import com.bang_ggood.room.domain.Room;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record RoomCreateRequest(@NotBlank(message = "방 이름이 존재하지 않습니다.") String name,
-                                Integer deposit, Integer rent, Integer contractTerm, Integer floor,
-                                String address, String station, Integer walkingTime, String realEstate) {
+public record RoomCreateRequest(@NotBlank(message = "방 이름이 존재하지 않습니다.") String roomName,
+                                Integer deposit, Integer rent, Integer contractTerm, String address,
+                                String station, Integer walkingTime, String realEstate,
+                                String type, String structure, Integer size, Integer floor, String floorLevel) {
 
     public Room toRoomEntity() {
-        return new Room(name, floor, address, station, walkingTime);
+        return new Room(roomName, floor, address, station, walkingTime);
     }
 }

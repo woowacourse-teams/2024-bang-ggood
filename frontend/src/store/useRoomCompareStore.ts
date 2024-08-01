75049e9988631f0f8c721c1d9fa5c9831a0b2b39
skiende74 <skiende74@gmail.com>
2024-08-01 17:24:24 +0900

    feat: useRoomCompareStore에 토글기능추가

--- frontend/src/store/useRoomCompareStore.ts
+++ frontend/src/store/useRoomCompareStore.ts
@@ -4,9 +4,10 @@ interface RoomCompareState {
   rooms: Set<number>;
   addRoom: (roomId: number) => void;
   deleteRoom: (roomId: number) => void;
+  toggleRoom: (roomId: number) => void;
   clear: () => void;
 }
-const useRoomCompareStore = create<RoomCompareState>(set => ({
+const useRoomCompareStore = create<RoomCompareState>((set, get) => ({
   rooms: new Set(),
   addRoom: (roomId: number) => {
     set(state => ({
@@ -22,6 +23,24 @@ const useRoomCompareStore = create<RoomCompareState>(set => ({
   clear: () => {
     set({
       rooms: new Set(),
+    });
+  },
+  has: (roomId: number) => get().rooms.has(roomId),
+  toggleRoom: (roomId: number) => {
+    // TODO: addRoom, deleteRoom을 여기서도 사용하는 방법 찾기 (되어야 코드짧아짐)
+    const isIncluded = get().rooms.has(roomId);
+    if (!isIncluded) {
+      set(({ rooms }) => ({
+        rooms: rooms.add(roomId),
+      }));
+      return;
+    }
+
+    set(({ rooms }) => {
+      rooms.delete(roomId);
+      return {
+        rooms,
+      };
     });
   },
 }));

8bbd84bffe51360465ed149ccd08a1ed01a96b5a
jinwoo22 <jenny010925@naver.com>
2024-08-02 15:34:51 +0900

    style: RestClient -> OauthClient로 변경

--- backend/bang-ggood/src/main/java/com/bang_ggood/config/OauthClientConfig.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/config/OauthClientConfig.java
@@ -1,15 +1,15 @@
 package com.bang_ggood.config;
 
-import com.bang_ggood.handler.RestClientExceptionHandler;
+import com.bang_ggood.handler.OauthClientExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-public class RestClientConfig {
+public class OauthClientConfig {
 
     @Bean
-    RestClient restClient(RestClientExceptionHandler handler) {
+    RestClient restClient(OauthClientExceptionHandler handler) {
         return RestClient.builder()
                 .defaultStatusHandler(handler)
                 .build();
--- backend/bang-ggood/src/main/java/com/bang_ggood/handler/OauthClientExceptionHandler.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/handler/OauthClientExceptionHandler.java
@@ -12,7 +12,7 @@ import org.springframework.web.client.ResponseErrorHandler;
 import java.io.IOException;
 
 @Component
-public class RestClientExceptionHandler implements ResponseErrorHandler {
+public class OauthClientExceptionHandler implements ResponseErrorHandler {
 
     @Override
     public boolean hasError(ClientHttpResponse response) {

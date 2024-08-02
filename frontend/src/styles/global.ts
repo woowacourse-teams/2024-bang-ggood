fb6b5ffed0411664a13830387f90888534defe75
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-02 15:35:36 +0900

    style: 폰트 스타일 '수트' 추가

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -4,9 +4,24 @@ import { normalize } from '../styles/reset';
 import theme from '../styles/theme';
 
 export const baseStyle = css`
+  @font-face {
+    font-family: 'SUIT';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'SUIT';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Bold.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
+  }
+
   ${normalize}
 
   body {
+    font-family: 'SUIT';
     max-width: 600px;
     min-height: 100vh;
     margin: 0px auto;

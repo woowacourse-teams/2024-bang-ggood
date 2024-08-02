dda130815d894f61ed04be8e990d6885b0f858b4
skiende74 <skiende74@gmail.com>
2024-08-02 17:07:30 +0900

    fix: 변경사항 스토리북에 반영하여 에러해결

--- frontend/src/components/RoomCompare/CompareSelectCard.stories.tsx
+++ frontend/src/components/RoomCompare/CompareSelectCard.stories.tsx
@@ -1,6 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react/*';
 import { useState } from 'react';
 
+import { checklistList } from '@/mocks/fixtures/checklistList';
+
 import CompareSelectCard from './CompareSelectCard';
 
 const meta: Meta<typeof CompareSelectCard> = {
@@ -13,7 +15,20 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   render: () => {
+    const room = checklistList.checklists[0];
+
     const [isSelected, setIsSelected] = useState(false);
-    return <CompareSelectCard isSelected={isSelected} onClick={() => setIsSelected(!isSelected)} />;
+    return (
+      <CompareSelectCard
+        key={room.checklistId}
+        isSelected={isSelected}
+        onClick={() => setIsSelected(!isSelected)}
+        roomName={room.roomName}
+        location={room.address}
+        deposit={room.deposit}
+        rent={room.rent}
+        createDate={new Date(room.createdAt)}
+      />
+    );
   },
 };

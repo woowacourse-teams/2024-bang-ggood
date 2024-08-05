3992b578866a8d0fb9df00346610916f62d670ef
jinwoo22 <jenny010925@naver.com>
2024-08-05 15:17:38 +0900

    style: static 변수 네이밍 변경

--- backend/bang-ggood/src/test/java/com/bang_ggood/user/service/UserServiceTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/user/service/UserServiceTest.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static com.bang_ggood.user.UserFixture.USER1;
-import static com.bang_ggood.user.UserFixture.oauthInfoResponseUSER1;
+import static com.bang_ggood.user.UserFixture.OAUTH_INFO_RESPONSE_USER1;
 import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,7 +36,7 @@ class UserServiceTest extends IntegrationTestSupport {
     void login_signup() {
         // given
         Mockito.when(oauthClient.requestOauthInfo(any(OauthLoginRequest.class)))
-                .thenReturn(UserFixture.oauthInfoResponseUSER2);
+                .thenReturn(UserFixture.OAUTH_INFO_RESPONSE_USER2);
 
         // when
         String token = userService.login(oauthLoginRequest);
@@ -51,7 +51,7 @@ class UserServiceTest extends IntegrationTestSupport {
         // given
         userRepository.save(USER1);
         Mockito.when(oauthClient.requestOauthInfo(any(OauthLoginRequest.class)))
-                .thenReturn(oauthInfoResponseUSER1);
+                .thenReturn(OAUTH_INFO_RESPONSE_USER1);
 
         // when
         String token = userService.login(oauthLoginRequest);

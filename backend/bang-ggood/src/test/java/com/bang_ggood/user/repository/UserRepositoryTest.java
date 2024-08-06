8d650c5e5cc942896c136415bd734219a290c2bb
jinwoo22 <jenny010925@naver.com>
2024-08-06 12:20:21 +0900

    style: findByEmail 테스트 displayName 내용 변경

--- backend/bang-ggood/src/test/java/com/bang_ggood/user/repository/UserRepositoryTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/user/repository/UserRepositoryTest.java
@@ -15,7 +15,7 @@ class UserRepositoryTest extends IntegrationTestSupport {
     @Autowired
     private UserRepository userRepository;
 
-    @DisplayName("유저 조회 성공 : 이메일로 유저 조회할 때")
+    @DisplayName("유저 이메일 조회 성공 : 유저 삭제 후 유저를 조회하면(논리적 삭제) 조회되지 않는다.")
     @Test
     void findByEmail() {
         // given
@@ -29,5 +29,4 @@ class UserRepositoryTest extends IntegrationTestSupport {
         // then
         Assertions.assertThat(findUser).isEmpty();
     }
-
 }

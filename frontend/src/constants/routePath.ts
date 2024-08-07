309ff2054acbbc6eaf57e3481921886e49605db5
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-07 13:20:58 +0900

    chore: 라우트 수정 및 불필요한 주석 삭제

--- frontend/src/constants/routePath.ts
+++ frontend/src/constants/routePath.ts
@@ -1,17 +1,16 @@
 export const ROUTE_PATH = {
   root: '/',
   /*checklist */
-  checklist: '/checklist',
+  checklistList: '/checklist',
   checklistNew: '/checklist/new',
-  checklistList: '/checklist-list',
   checklistCustom: `/checklist/custom`,
   checklistId: '/checklist/:checklistId',
   checklistOne: (id: number) => `/checklist/${id}`,
   /*category*/
-  categoryChoose: '/category/choose',
+  categoryChoose: '/category-choose',
   /*room*/
-  roomCompareSelect: '/room/compare/select',
-  roomCompare: '/room/compare',
+  roomCompareSelect: '/room-compare/select',
+  roomCompare: '/room-compare',
   /* TODO : 추후 추가될 페이지들의 라우트패스 */
   location: '/location',
   myPage: '/my-page',

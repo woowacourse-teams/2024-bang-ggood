f45558ae2ac4f5f0fc61f68ec6a6229f9d96f412
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-07 13:20:36 +0900

    chore: 폰트 모듈 오류 해결

--- frontend/src/styles/font.ts
+++ frontend/src/styles/font.ts
@@ -3,13 +3,15 @@ import { css } from '@emotion/react';
 export const fonts = css`
   @font-face {
     font-family: 'SUIT';
-    src: local('SUIT'), url('../assets/fonts/SUIT-Regular.woff2');
+    src:
+      local('SUIT'),
+      url('@/assets/fonts/SUIT-Regular.woff2') format('woff2');
     font-weight: 400;
   }
 
   @font-face {
     font-family: 'SUIT';
-    src: url('../assets/fonts/SUIT-Bold.woff2');
+    src: url('@/assets/fonts/SUIT-Bold.woff2') format('woff2');
     font-weight: 700;
   }
 `;

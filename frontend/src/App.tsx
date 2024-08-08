1009eb1ec7e9b285d342e5e8870d8fa72f61811a
ooherin <555ohr@naver.com>
2024-08-09 07:33:38 +0900

    fix: 충돌 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RouterProvider } from 'react-router-dom';
 
 import Toast from '@/components/common/Toast/Toast';
-import router from '@/Router';
 import { baseStyle } from '@/styles/global';
 import theme from '@/styles/theme';
 
@@ -15,7 +13,6 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <Global styles={baseStyle} />
-        <RouterProvider router={router} />
         <ReactQueryDevtools initialIsOpen={false} />
         <Toast />
       </ThemeProvider>

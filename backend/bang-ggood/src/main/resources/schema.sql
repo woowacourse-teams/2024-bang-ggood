de42411a6e346bf2fb07e875810808ae283c3e1f
tsulocalize <tsulocalizing@gmail.com>
2024-08-08 18:48:28 +0900

    fix: 유저 name column을 nullable 하도록 수정

--- backend/bang-ggood/src/main/resources/schema.sql
+++ backend/bang-ggood/src/main/resources/schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE room
 CREATE TABLE users
 (
     id          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
+    name        VARCHAR(255),
     email       VARCHAR(255) NOT NULL,
     created_at  TIMESTAMP(6),
     modified_at TIMESTAMP(6),

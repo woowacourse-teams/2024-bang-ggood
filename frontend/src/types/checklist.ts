b1695c8575a43337f1eef529c6caeed7c5afe8c9
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-09 16:08:21 +0900

    chore: 타입 주석 설명 추가

--- frontend/src/types/checklist.ts
+++ frontend/src/types/checklist.ts
@@ -3,32 +3,14 @@ import { Badge } from '@/types/badge';
 import { CategoryScore } from '@/types/category';
 import { RoomInfo } from '@/types/room';
 
+export interface CategoryAndQuestion {
+  categoryId: number;
+  questionId: number;
+}
+
 interface ChecklistCategoryBase {
   categoryId: number;
   categoryName: string;
-}
-
-export interface ChecklistCategoryQuestions extends ChecklistCategoryBase {
-  questions: ChecklistQuestion[];
-}
-
-/* 체크리스트 작성: 모든 질문 인테페이스 */
-export interface ChecklistCategoryQnA extends ChecklistCategoryBase {
-  questions: OneQuestionWithAnswer[];
-}
-
-// 하나의 질문에 대한 인터페이스
-export interface OneQuestionWithAnswer extends ChecklistQuestion {
-  answer: AnswerType;
-}
-
-/* 체크리스트 커스텀 */
-export interface ChecklistCategoryQnIsSelected extends ChecklistCategoryBase {
-  questions: ChecklistQuestionWithIsSelected[];
-}
-
-export interface ChecklistQuestionWithIsSelected extends ChecklistQuestion {
-  isSelected: boolean;
 }
 
 export interface ChecklistQuestion {
@@ -37,10 +19,28 @@ export interface ChecklistQuestion {
   subtitle: string | null;
 }
 
-/*체크리스트를 제공할 때 쓰는 인터페이스 */
-export interface ChecklistAnswer {
-  questionId: number;
+// 기본 체크리스트
+export interface ChecklistCategoryQuestions extends ChecklistCategoryBase {
+  questions: ChecklistQuestion[];
+}
+
+// 체크리스트 작성: 모든 질문 + 답변
+export interface ChecklistCategoryQnA extends ChecklistCategoryBase {
+  questions: OneQuestionWithAnswer[];
+}
+
+// 하나의 질문 + 답변 컴포넌트
+export interface OneQuestionWithAnswer extends ChecklistQuestion {
   answer: AnswerType;
+}
+
+// 체크리스트 커스텀
+export interface ChecklistCategoryQnIsSelected extends ChecklistCategoryBase {
+  questions: ChecklistQuestionWithIsSelected[];
+}
+
+export interface ChecklistQuestionWithIsSelected extends ChecklistQuestion {
+  isSelected: boolean;
 }
 
 export interface ChecklistPreview extends RoomInfo {

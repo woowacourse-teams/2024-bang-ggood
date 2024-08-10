5a54129067967c9fee423fd87bc3916c56bb01c6
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-11 02:25:39 +0900

    chore: 사용되지 않는 이모션 컴포넌트 삭제

--- frontend/src/components/NewChecklist/ChecklistQuestion/QuestionMemo.tsx
+++ frontend/src/components/NewChecklist/ChecklistQuestion/QuestionMemo.tsx
@@ -1,61 +1,61 @@
-// import styled from '@emotion/styled';
-// import { useRef } from 'react';
+import styled from '@emotion/styled';
+import { useRef } from 'react';
 
-// import Textarea from '@/components/_common/Textarea/Textarea';
-// import useInput from '@/hooks/useInput';
+import Textarea from '@/components/_common/Textarea/Textarea';
+import useInput from '@/hooks/useInput';
 
-// interface Props {
-//   questionId: number;
-//   text: string;
-// }
+interface Props {
+  questionId: number;
+  text: string;
+}
 
-// const QuestionMemo = ({ text }: Props) => {
-//   // const { currentTabId } = useTabContext();
-//   const { onChange, value } = useInput(text);
-//   // const { updateMemo } = useChecklistGrade();
-//   const intervalRef = useRef<number | undefined>(undefined);
+const QuestionMemo = ({ text }: Props) => {
+  // const { currentTabId } = useTabContext();
+  const { onChange, value } = useInput(text);
+  // const { updateMemo } = useChecklistGrade();
+  const intervalRef = useRef<number | undefined>(undefined);
 
-//   // const handleUpdateMemo = () => {
-//   //   updateMemo({ categoryId: currentTabId, questionId, newMemo: value });
-//   // };
+  // const handleUpdateMemo = () => {
+  //   updateMemo({ categoryId: currentTabId, questionId, newMemo: value });
+  // };
 
-//   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     onChange(e);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e);
 
-//     if (intervalRef.current !== undefined) {
-//       clearTimeout(intervalRef.current);
-//     }
+    if (intervalRef.current !== undefined) {
+      clearTimeout(intervalRef.current);
+    }
 
-//     intervalRef.current = window.setTimeout(() => {
-//       // handleUpdateMemo();
-//     }, 2000);
-//   };
+    intervalRef.current = window.setTimeout(() => {
+      // handleUpdateMemo();
+    }, 2000);
+  };
 
-//   const handleBlur = () => {
-//     if (intervalRef.current !== undefined) {
-//       clearInterval(intervalRef.current);
-//       intervalRef.current = undefined;
-//     }
-//     // handleUpdateMemo();
-//   };
+  const handleBlur = () => {
+    if (intervalRef.current !== undefined) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = undefined;
+    }
+    // handleUpdateMemo();
+  };
 
-//   return (
-//     <S.Container>
-//       <Textarea height="medium" value={value || ''} onChange={handleInputChange} onBlur={handleBlur} />
-//     </S.Container>
-//   );
-// };
+  return (
+    <S.Container>
+      <Textarea height="medium" value={value || ''} onChange={handleInputChange} onBlur={handleBlur} />
+    </S.Container>
+  );
+};
 
-// export default QuestionMemo;
+export default QuestionMemo;
 
-// const S = {
-//   Container: styled.div`
-//     position: relative;
-//     margin-top: 15px;
-//   `,
-//   ButtonBox: styled.div`
-//     position: absolute;
-//     right: 10px;
-//     bottom: 10px;
-//   `,
-// };
+const S = {
+  Container: styled.div`
+    position: relative;
+    margin-top: 15px;
+  `,
+  ButtonBox: styled.div`
+    position: absolute;
+    right: 10px;
+    bottom: 10px;
+  `,
+};

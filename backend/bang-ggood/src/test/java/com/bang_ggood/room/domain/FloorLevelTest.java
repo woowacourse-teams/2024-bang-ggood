6e039afffa8e73e3c27516e2f0c8eb369e0fa8aa
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-08-11 22:43:09 +0900

    feat(FloorLevel): null일 경우 추가

--- backend/bang-ggood/src/test/java/com/bang_ggood/room/domain/FloorLevelTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/room/domain/FloorLevelTest.java
@@ -20,6 +20,16 @@ class FloorLevelTest {
         assertThat(FloorLevel.from(name)).isEqualTo(FloorLevel.BASEMENT);
     }
 
+    @DisplayName("name으로 FloorLevel 생성 성공: null일 경우")
+    @Test
+    void from_null() {
+        // given
+        String name = null;
+
+        // when & then
+        assertThat(FloorLevel.from(name)).isEqualTo(FloorLevel.NONE);
+    }
+
     @DisplayName("name으로 FloorLevel 생성 실패 : 해당하지 않는 이름일 경우")
     @Test
     void from_invalidFloorLevel_exception() {

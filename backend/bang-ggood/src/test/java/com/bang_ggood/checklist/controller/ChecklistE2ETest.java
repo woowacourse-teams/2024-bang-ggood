0a25e1e5c201e5ca358bc2d34f47287d14b62f48
jinwoo22 <jenny010925@naver.com>
2024-08-12 15:38:58 +0900

    fix: 머지 충돌 해결

--- backend/bang-ggood/src/test/java/com/bang_ggood/checklist/controller/ChecklistE2ETest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/checklist/controller/ChecklistE2ETest.java
@@ -73,40 +73,12 @@ class ChecklistE2ETest extends AcceptanceTest {
                 .statusCode(400);
     }
 
-    @DisplayName("체크리스트 좋아요 추가 성공")
-    @Test
-    void createChecklistLike() {
-        long checklistId = checklistService.createChecklist(USER1, ChecklistFixture.CHECKLIST_CREATE_REQUEST);
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .header(new Header(HttpHeaders.COOKIE, this.responseCookie.toString()))
-                .when().post("/checklists/" + checklistId + "/like")
-                .then().log().all()
-                .statusCode(204);
-    }
-
-    @DisplayName("체크리스트 좋아요 추가 실패 : 이미 좋아요가 추가가 된 체크리스트인 경우")
-    @Test
-    void createChecklistLike_checklistAlreadyLiked_exception() {
-        long checklistId = checklistService.createChecklist(USER1, ChecklistFixture.CHECKLIST_CREATE_REQUEST);
-        checklistService.createChecklistLike(USER1, checklistId);
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .header(new Header(HttpHeaders.COOKIE, this.responseCookie.toString()))
-                .when().post("/checklists/" + checklistId + "/like")
-                .then().log().all()
-                .statusCode(409);
-    }
-
     @DisplayName("체크리스트 질문 조회 성공")
     @Test
     void readChecklistQuestions() {
         // given
         customChecklistQuestionRepository.saveAll(CustomChecklistFixture.CUSTOM_CHECKLIST_QUESTION_DEFAULT);
 
-        // when
         ChecklistQuestionsResponse checklistQuestionsResponse = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .header(new Header(HttpHeaders.COOKIE, this.responseCookie.toString()))
@@ -205,13 +177,10 @@ class ChecklistE2ETest extends AcceptanceTest {
     @DisplayName("커스텀 체크리스트 업데이트 성공")
     @Test
     void updateCustomChecklist() {
-        Map<String, List<Integer>> params = new HashMap<>();
-        params.put("questionIds", List.of(1, 3, 5, 7, 9, 14, 21, 30));
-
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .header(new Header(HttpHeaders.COOKIE, this.responseCookie.toString()))
-                .body(params)
+                .body(CustomChecklistFixture.CUSTOM_CHECKLIST_UPDATE_REQUEST)
                 .when().put("/custom-checklist")
                 .then().log().all()
                 .statusCode(204);

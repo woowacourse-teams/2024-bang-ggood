28a863af20a0f45e50e841b8b83c7969eefafa59
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-12 16:53:30 +0900

    feat: 체크리스트 리스트 페이지 레이아웃 변경

--- frontend/src/components/ChecklistList/CustomBanner.tsx
+++ frontend/src/components/ChecklistList/CustomBanner.tsx
@@ -1,43 +1,43 @@
 import styled from '@emotion/styled';
 
 import { PencilIcon } from '@/assets/assets';
-import { flexCenter, title3 } from '@/styles/common';
+import { boxShadow, flexCenter, title3 } from '@/styles/common';
 
 interface Props {
   onClick?: () => void;
 }
 
-const EditBanner = ({ onClick }: Props) => {
+const CustomBanner = ({ onClick }: Props) => {
   return (
     <S.Banner onClick={onClick}>
-      <PencilIcon />
-      <S.Title>
-        체크리스트
-        <br />
-        커스텀하기
-      </S.Title>
+      <PencilIcon width={50} height={70} />
+      <S.Title>나에게 최적인 체크리스트 만들기</S.Title>
     </S.Banner>
   );
 };
 
-export default EditBanner;
+export default CustomBanner;
 
 const S = {
   Banner: styled.div`
-    display: flex;
+    ${flexCenter}
 
-    width: 50%;
-    height: 75px;
+    width: 100%;
+    height: 80px;
     padding: 16px;
 
-    background-color: ${({ theme }) => theme.palette.yellow200};
+    border-radius: 16px;
+
+    background-color: ${({ theme }) => theme.palette.yellow300};
 
     color: ${({ theme }) => theme.palette.black};
     line-height: 1.3;
     box-sizing: border-box;
     justify-content: space-evenly;
     align-items: center;
-    gap: 10px;
+    gap: 5px;
+
+    ${boxShadow};
 
     &:hover,
     &:active {

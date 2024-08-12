cf6b723bec24ea6572dbbf4b13448fea1b113a45
skiende74 <skiende74@gmail.com>
2024-08-12 23:34:42 +0900

    style: styled-components의 위치를 컴포넌트 아래로 이동

--- frontend/src/components/_common/FormField/FormField.tsx
+++ frontend/src/components/_common/FormField/FormField.tsx
@@ -13,6 +13,26 @@ const FormFieldWrapper = styled.div<{ rowGap?: string }>`
   row-gap: ${({ rowGap }) => (rowGap ? rowGap : '10px')};
 `;
 
+type GetProps<T> = T extends React.FC<infer P> ? P : never;
+
+interface LabelProps extends HTMLAttributes<HTMLLabelElement> {
+  label: string;
+  required?: boolean;
+}
+
+const FormField = Object.assign(FormFieldWrapper, {
+  Label: ({ label, required = false, ...rest }: LabelProps) => (
+    <S.LabelContainer {...rest} style={{ fontSize: theme.text.size.medium, fontWeight: theme.text.weight.bold }}>
+      {label}
+      {required && <S.MovedRequiredDot />}
+    </S.LabelContainer>
+  ),
+  Input: ({ ...rest }: GetProps<typeof Input>) => <Input {...rest} />,
+  ErrorMessage: ({ value, ...rest }: { value: string } & HTMLAttributes<HTMLParagraphElement>) => (
+    <S.P {...rest}>{value}</S.P>
+  ),
+});
+
 const S = {
   MovedRequiredDot: styled(InputRequiredDot)`
     position: relative;
@@ -30,23 +50,4 @@ const S = {
     z-index: 0;
   `,
 };
-
-type GetProps<T> = T extends React.FC<infer P> ? P : never;
-
-interface LabelProps extends HTMLAttributes<HTMLLabelElement> {
-  label: string;
-  required?: boolean;
-}
-
-const FormField = Object.assign(FormFieldWrapper, {
-  Label: ({ label, required = false, ...rest }: LabelProps) => (
-    <S.LabelContainer {...rest} style={{ fontSize: theme.text.size.medium, fontWeight: theme.text.weight.bold }}>
-      {label}
-      {required && <S.MovedRequiredDot />}
-    </S.LabelContainer>
-  ),
-  Input: ({ ...rest }: GetProps<typeof Input>) => <Input {...rest} />,
-  P: ({ value, ...rest }: { value: string } & HTMLAttributes<HTMLParagraphElement>) => <S.P {...rest}>{value}</S.P>,
-});
-
 export default FormField;

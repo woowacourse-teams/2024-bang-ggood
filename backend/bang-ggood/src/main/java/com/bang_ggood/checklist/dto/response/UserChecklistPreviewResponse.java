bfaa5ac9e911cae5adb9eb43e2c74507df4ca71d
jinwoo22 <jenny010925@naver.com>
2024-08-12 16:14:58 +0900

    feat: 체크리스트 리스트 반환시 한줄평, 좋아요 여부 추가

--- backend/bang-ggood/src/main/java/com/bang_ggood/checklist/dto/response/UserChecklistPreviewResponse.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/checklist/dto/response/UserChecklistPreviewResponse.java
@@ -5,15 +5,18 @@ import java.time.LocalDateTime;
 
 public record UserChecklistPreviewResponse(
         Long checklistId, String roomName, String address,
-        Integer deposit, Integer rent, LocalDateTime createdAt) {
+        Integer deposit, Integer rent, LocalDateTime createdAt,
+        String summary, boolean isLiked) {
 
-    public static UserChecklistPreviewResponse of(Checklist checklist) {
+    public static UserChecklistPreviewResponse of(Checklist checklist, boolean isLiked) {
         return new UserChecklistPreviewResponse(
                 checklist.getId(),
                 checklist.getRoomName(),
                 checklist.getRoomAddress(),
                 checklist.getDeposit(),
                 checklist.getRent(),
-                checklist.getCreatedAt());
+                checklist.getCreatedAt(),
+                checklist.getSummary(),
+                isLiked);
     }
 }

9c2519c838a5a94369a6b00058c1f2201b3a1bd7
Hyerin <125418818+ooherin@users.noreply.github.com>
2024-08-13 11:52:06 +0900

    [FE] 메모를 모달로 리팩토링한다. (#336)

--- frontend/src/components/NewChecklist/NewChecklistContent.tsx
+++ frontend/src/components/NewChecklist/NewChecklistContent.tsx
@@ -1,23 +1,47 @@
+import styled from '@emotion/styled';
+
+import { MemoFilled } from '@/assets/assets';
+import FloatingButton from '@/components/_common/Button/FloatingButton';
 import { useTabContext } from '@/components/_common/Tabs/TabContext';
+import MemoModal from '@/components/NewChecklist/MemoModal/MemoModal';
 import NewChecklistInfoTemplate from '@/components/NewChecklist/NewChecklistInfoTemplate';
 import NewChecklistTemplate from '@/components/NewChecklist/NewChecklistTemplate';
 import OptionChecklistTemplate from '@/components/NewChecklist/Option/OptionChecklistTemplate';
-import useChecklistStore from '@/store/useChecklistStore';
+import useModalOpen from '@/hooks/useModalOpen';
 
 const NewChecklistContent = () => {
   const { currentTabId } = useTabContext();
-  const { getCategoryQnA } = useChecklistStore();
+  const { isModalOpen, modalOpen, modalClose } = useModalOpen();
 
-  return currentTabId === -1 ? (
-    /*방 기본정보 템플릿*/
-    <NewChecklistInfoTemplate />
-  ) : currentTabId === 0 ? (
-    /*옵션 선택 템플릿*/
-    <OptionChecklistTemplate />
-  ) : (
-    /* 체크리스트 템플릿 */
-    <NewChecklistTemplate questions={getCategoryQnA(currentTabId)} />
+  return (
+    <S.Container>
+      {currentTabId === -1 ? (
+        /*방 기본정보 템플릿*/
+        <NewChecklistInfoTemplate />
+      ) : currentTabId === 0 ? (
+        /*옵션 선택 템플릿*/
+        <OptionChecklistTemplate />
+      ) : (
+        /* 체크리스트 템플릿 */
+        <NewChecklistTemplate />
+      )}
+
+      {/*메모 모달*/}
+      {isModalOpen && <MemoModal isModalOpen={isModalOpen} modalClose={modalClose} />}
+      {/*메모 작성 버튼*/}
+      <FloatingButton onClick={modalOpen}>
+        <MemoFilled />
+      </FloatingButton>
+    </S.Container>
   );
+};
+
+const S = {
+  Container: styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+  `,
 };
 
 export default NewChecklistContent;

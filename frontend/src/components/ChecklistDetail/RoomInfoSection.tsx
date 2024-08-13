2d2939f5db59150f3d6d16b9b3c671ed5833c16b
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-13 15:27:46 +0900

    feat: 디테일 페이지 수정

--- frontend/src/components/ChecklistDetail/RoomInfoSection.tsx
+++ frontend/src/components/ChecklistDetail/RoomInfoSection.tsx
@@ -5,11 +5,10 @@ import { RoomInfo } from '@/types/room';
 
 interface Props {
   room: RoomInfo;
-  score: number;
   createdAt: string;
 }
 
-const RoomInfoSection = ({ room, score }: Props) => {
+const RoomInfoSection = ({ room }: Props) => {
   // TODO: 로딩 중일 때 스켈레톤표시
   if (!room) return null;
   const { roomName, deposit, rent, address, contractTerm, floor, floorLevel, station, walkingTime, realEstate } = room;
@@ -18,34 +17,33 @@ const RoomInfoSection = ({ room, score }: Props) => {
     <S.Container>
       <S.Row>
         <S.Title>{roomName}</S.Title>
-        <S.Score>{score}점</S.Score>
       </S.Row>
       <S.Row>{address}</S.Row>
       <S.Row>
         <S.Rent>
           <S.Label>월세 :</S.Label>
-          {deposit}/{rent}
+          {deposit ?? '00'}/{rent ?? '00'}
         </S.Rent>
       </S.Row>
       <S.Row>
         <S.Box>
-          <S.Label>계약 기간 : </S.Label>
-          {contractTerm}개월
+          <S.Label>계약 기간 :</S.Label>
+          {contractTerm ?? '00'}개월
         </S.Box>
         {/* TODO: 방 종류, 방 구조 추가 */}
         <S.Box>
           <S.Label>층 :</S.Label>
-          {floorLevel === '지상' ? `${floor}층` : floorLevel}
+          {floorLevel === '지상' ? `${floor ?? '00'}층` : floorLevel}
         </S.Box>
       </S.Row>
       <S.Row>
         <S.Label>교통편 :</S.Label>
-        {station}까지 도보
-        {walkingTime}분
+        {station ?? '역'}까지 도보
+        {walkingTime ?? '00'}분
       </S.Row>
       <S.Row>
         <S.Label>부동산 :</S.Label>
-        {realEstate}
+        {realEstate ?? '00부동산'}
       </S.Row>
     </S.Container>
   );

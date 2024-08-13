ccd9ca220dcc58050a3dd89f65c1c6a9c9f3aaef
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-13 11:37:35 +0900

    feat: 1시간 이내일시 분 단위로 나타나게 변경

--- frontend/src/utils/formattedDate.ts
+++ frontend/src/utils/formattedDate.ts
@@ -2,8 +2,10 @@ const formattedDate = (stringDate: string, splitter: string = '/') => {
   const date = new Date(stringDate);
   const nowDate = new Date();
   const timeDifference = Math.floor((nowDate.getTime() - date.getTime()) / (1000 * 60 * 60));
+  const minuteDifference = Math.floor((nowDate.getTime() - date.getTime()) / (1000 * 60));
 
-  if (timeDifference < 24) return `${timeDifference}시간 전`;
+  if (timeDifference < 1) return `${minuteDifference}분 전`;
+  else if (timeDifference < 24) return `${timeDifference}시간 전`;
   return date.toISOString().slice(2, 10).replace(/-/g, splitter);
 };
 

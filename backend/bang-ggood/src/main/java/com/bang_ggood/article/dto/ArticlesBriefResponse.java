833a642d956fa0cedc3784894249d8427b3d93c3
tsulocalize <tsulocalizing@gmail.com>
2024-08-14 15:27:24 +0900

    feat: Article 목록 조회 기능 구현

---
+++ backend/bang-ggood/src/main/java/com/bang_ggood/article/dto/ArticlesBriefResponse.java
@@ -0,0 +1,6 @@
+package com.bang_ggood.article.dto;
+
+import java.util.List;
+
+public record ArticlesBriefResponse(List<ArticleBriefResponse> responses) {
+}
--- backend/bang-ggood/src/main/java/com/bang_ggood/article/service/ArticleService.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/article/service/ArticleService.java
@@ -1,9 +1,12 @@
 package com.bang_ggood.article.service;
 
 import com.bang_ggood.article.domain.Article;
+import com.bang_ggood.article.dto.ArticleBriefResponse;
 import com.bang_ggood.article.dto.ArticleResponse;
+import com.bang_ggood.article.dto.ArticlesBriefResponse;
 import com.bang_ggood.article.repository.ArticleRepository;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class ArticleService {
@@ -17,5 +20,12 @@ public class ArticleService {
     public ArticleResponse readArticle(Long id) {
         Article article = articleRepository.getById(id);
         return ArticleResponse.from(article);
+    }
+
+    public ArticlesBriefResponse readArticles() {
+        List<ArticleBriefResponse> articles = articleRepository.findAll().stream()
+                .map(ArticleBriefResponse::from)
+                .toList();
+        return new ArticlesBriefResponse(articles);
     }
 }

59c08c542ff82c7eb3704f8d731a73dc8c762817
ooherin <555ohr@naver.com>
2024-08-14 17:56:42 +0900

    feat: useHandleTipBox 생성

--- frontend/src/hooks/useHandleTipBox.ts
+++ frontend/src/hooks/useHandleTipBox.ts
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 
-const useHandleTipBox = () => {
-  return (
-    <div>useHandleTipBox</div>
-  )
-}
+const STORAGE_KEYS = {
+  TIP: 'tipState',
+};
 
-export default useHandleTipBox
+const useHandleTipBox = (tipId: number) => {
+  const [openTipIds, setOpenTipIds] = useState<Record<string, boolean>>(() => {
+    const savedTipState = localStorage.getItem(STORAGE_KEYS.TIP);
+    return savedTipState !== null ? JSON.parse(savedTipState) : {};
+  });
 
+  const toggleTipOpen = () => {
+    setOpenTipIds(prev => {
+      const newOpenTipIds = { ...prev, [tipId]: !prev[tipId] };
+      localStorage.setItem(STORAGE_KEYS.TIP, JSON.stringify(newOpenTipIds));
+      return newOpenTipIds;
+    });
+  };
+
+  const getCurrentTipIsOpen = () => {
+    return openTipIds[tipId];
+  };
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEYS.TIP, JSON.stringify(openTipIds));
+  }, [openTipIds]);
+
+  return { isTipOpen: getCurrentTipIsOpen(), toggleTipOpen };
+};
+
+export default useHandleTipBox;

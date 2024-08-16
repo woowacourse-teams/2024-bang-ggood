78ef06d89cbaefaba6c879f18c261f25e3a1dd2b
ooherin <555ohr@naver.com>
2024-08-16 14:59:35 +0900

    refactor: 옵션 변경한 내용으로 수정

--- frontend/src/constants/options.ts
+++ frontend/src/constants/options.ts
@@ -1,11 +1,15 @@
 import AirConditionerFilled from '@/assets/icons/option/airconditioner-filled.svg';
 import AirConditionerUnfilled from '@/assets/icons/option/airconditioner-unfilled.svg';
+import BedFilled from '@/assets/icons/option/bed-filled.svg';
+import BedUnfilled from '@/assets/icons/option/bed-unfilled.svg';
 import ClosetFilled from '@/assets/icons/option/closet-filled.svg';
 import ClosetUnfilled from '@/assets/icons/option/closet-unfilled.svg';
 import DeskFilled from '@/assets/icons/option/desk-filled.svg';
 import DeskUnfilled from '@/assets/icons/option/desk-unfilled.svg';
 import ElectronicStoveFilled from '@/assets/icons/option/electronic-stove-filled.svg';
 import ElectronicStoveUnfilled from '@/assets/icons/option/electronic-stove-unfilled.svg';
+import ElevatorFilled from '@/assets/icons/option/elevator-filled.svg';
+import ElevatorUnfilled from '@/assets/icons/option/elevator-unfilled.svg';
 import MicrowaveFilled from '@/assets/icons/option/microwave-filled.svg';
 import MicrowaveUnfilled from '@/assets/icons/option/microwave-unfilled.svg';
 import RefrigeratorFilled from '@/assets/icons/option/refrigerator-filled.svg';
@@ -21,24 +25,24 @@ import { OptionWithIcon } from '@/types/option';
 const OPTIONS: OptionWithIcon[] = [
   {
     id: 1,
-    name: 'airConditioner',
-    UnFilledIcon: AirConditionerUnfilled,
-    FilledIcon: AirConditionerFilled,
-    displayName: '에어컨',
+    name: 'refrigerator',
+    UnFilledIcon: RefrigeratorUnfilled,
+    FilledIcon: RefrigeratorFilled,
+    displayName: '냉장고',
   },
   {
     id: 2,
-    name: 'closet',
-    UnFilledIcon: ClosetUnfilled,
-    FilledIcon: ClosetFilled,
-    displayName: '옷장',
+    name: 'sink',
+    UnFilledIcon: SinkUnfilled,
+    FilledIcon: SinkFilled,
+    displayName: '싱크대',
   },
   {
     id: 3,
-    name: 'desk',
-    UnFilledIcon: DeskUnfilled,
-    FilledIcon: DeskFilled,
-    displayName: '책상',
+    name: 'electronicStove',
+    UnFilledIcon: ElectronicStoveUnfilled,
+    FilledIcon: ElectronicStoveFilled,
+    displayName: '인덕션',
   },
   {
     id: 4,
@@ -49,10 +53,10 @@ const OPTIONS: OptionWithIcon[] = [
   },
   {
     id: 5,
-    name: 'shoeRack',
-    UnFilledIcon: ShoesRackUnfilled,
-    FilledIcon: ShoesRackFilled,
-    displayName: '신발장',
+    name: 'airConditioner',
+    UnFilledIcon: AirConditionerUnfilled,
+    FilledIcon: AirConditionerFilled,
+    displayName: '에어컨',
   },
   {
     id: 6,
@@ -63,24 +67,39 @@ const OPTIONS: OptionWithIcon[] = [
   },
   {
     id: 7,
-    name: 'electronicStove',
-    UnFilledIcon: ElectronicStoveUnfilled,
-    FilledIcon: ElectronicStoveFilled,
-    displayName: '인덕션',
+    name: 'closet',
+    UnFilledIcon: ClosetUnfilled,
+    FilledIcon: ClosetFilled,
+    displayName: '옷장',
   },
   {
     id: 8,
-    name: 'sink',
-    UnFilledIcon: SinkUnfilled,
-    FilledIcon: SinkFilled,
-    displayName: '싱크대',
+    name: 'desk',
+    UnFilledIcon: DeskUnfilled,
+    FilledIcon: DeskFilled,
+    displayName: '책상',
   },
+
   {
     id: 9,
-    name: 'refrigerator',
-    UnFilledIcon: RefrigeratorUnfilled,
-    FilledIcon: RefrigeratorFilled,
-    displayName: '냉장고',
+    name: 'bed',
+    UnFilledIcon: BedUnfilled,
+    FilledIcon: BedFilled,
+    displayName: '침대',
+  },
+  {
+    id: 10,
+    name: 'shoeRack',
+    UnFilledIcon: ShoesRackUnfilled,
+    FilledIcon: ShoesRackFilled,
+    displayName: '신발장',
+  },
+  {
+    id: 11,
+    name: 'elevator',
+    UnFilledIcon: ElevatorUnfilled,
+    FilledIcon: ElevatorFilled,
+    displayName: '엘리베이터',
   },
 ];
 

26232df11a221cfb115284283caf0251384be95e
skiende74 <skiende74@gmail.com>
2024-08-16 19:33:26 +0900

    feat: 임시커밋. 둘을 합쳐서 에러상태

--- frontend/src/pages/EditChecklistPage.tsx
+++ frontend/src/pages/EditChecklistPage.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useStore } from 'zustand';
 
-import { getChecklistQuestions } from '@/apis/checklist';
+import { getChecklistDetail } from '@/apis/checklist';
 import Button from '@/components/_common/Button/Button';
 import Header from '@/components/_common/Header/Header';
 import { TabProvider } from '@/components/_common/Tabs/TabContext';
@@ -12,47 +12,40 @@ import SummaryModal from '@/components/NewChecklist/SummaryModal/SummaryModal';
 import { ROUTE_PATH } from '@/constants/routePath';
 import { DEFAULT_TOAST_DURATION } from '@/constants/system';
 import useAddChecklistQuery from '@/hooks/query/useAddChecklistQuery';
-import useHandleTipBox from '@/hooks/useHandleTipBox';
 import useModalOpen from '@/hooks/useModalOpen';
 import useNewChecklistTabs from '@/hooks/useNewChecklistTabs';
 import useToast from '@/hooks/useToast';
 import checklistRoomInfoStore from '@/store/checklistRoomInfoStore';
-import useChecklistStore from '@/store/useChecklistStore';
-import useOptionStore from '@/store/useOptionStore';
 import { ChecklistCategoryQnA } from '@/types/checklist';
 
+type RouteParams = {
+  checklistId: string;
+};
 const EditChecklistPage = () => {
-  const { showToast } = useToast(DEFAULT_TOAST_DURATION);
-  const { tabs } = useNewChecklistTabs();
-  const { mutate: addChecklist } = useAddChecklistQuery();
-
-  /*방 기본 정보 */
-  const { value: roomInfoAnswer, actions } = useStore(checklistRoomInfoStore);
-  const navigate = useNavigate();
-  /* 선택된 옵션 */
-  const { selectedOptions, resetToDefaultOptions } = useOptionStore();
-  /* 체크리스트 답변 */
-  const { checklistCategoryQnA, setAnswerInQuestion, setValidCategory } = useChecklistStore();
-  /* 한줄평 모달 */
   const { isModalOpen, modalOpen, modalClose } = useModalOpen();
-  const { resetShowTipBox } = useHandleTipBox('OPTION');
+
+  const { mutate: addChecklist } = useAddChecklistQuery();
+  const { showToast } = useToast(DEFAULT_TOAST_DURATION);
+  const { checklistId } = useParams() as RouteParams;
+  const actions = useStore(checklistRoomInfoStore, state => state.actions);
+  const roomName = useStore(checklistRoomInfoStore, state => state.value.roomName);
+
+  const navigate = useNavigate();
+
+  const { tabs } = useNewChecklistTabs();
 
   useEffect(() => {
     const fetchChecklist = async () => {
-      const checklist = await getChecklistQuestions();
-
-      // 체크리스트 질문에 대한 답안지 객체 생성
-      setAnswerInQuestion(checklist);
-      // 현재 질문이 있는 유효한 카테고리 생성
-      setValidCategory();
-      // 옵션 선택지 리셋
-      resetToDefaultOptions();
-      //로컬 스토리지 팁 보이는 여부 리셋
-      resetShowTipBox();
+      const checklist = await getChecklistDetail(Number(checklistId));
+      actions.setAll({ value: checklist.room });
     };
-
     fetchChecklist();
-  }, []);
+  }, [checklistId, actions]);
+
+  // TODO: fetch 시 로딩 상태일 때 스켈레톤처리. 성공할 떄만 return 문 보여주는 로직이 필요
+  if (!roomName) {
+    return <div>체크리스트가 없어요</div>;
+  }
 
   /* 현재 상태를 백엔드에 보내는 답안 포맷으로 바꾸는 함수 */
   const transformQuestions = (checklist: ChecklistCategoryQnA[]) => {

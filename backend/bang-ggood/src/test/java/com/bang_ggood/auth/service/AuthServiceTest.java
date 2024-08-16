09790bfbb04c785e2c841ee486757016c60984dd
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2024-08-16 11:29:07 +0900

    [BE] 회원가입시 디폴트 체크리스트를 추가한다. (#379)

--- backend/bang-ggood/src/test/java/com/bang_ggood/auth/service/AuthServiceTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/auth/service/AuthServiceTest.java
@@ -2,7 +2,13 @@ package com.bang_ggood.auth.service;
 
 import com.bang_ggood.IntegrationTestSupport;
 import com.bang_ggood.auth.dto.request.OauthLoginRequest;
+import com.bang_ggood.category.dto.response.CategoryQuestionsResponse;
+import com.bang_ggood.checklist.domain.Question;
+import com.bang_ggood.checklist.dto.response.ChecklistQuestionsResponse;
+import com.bang_ggood.checklist.dto.response.UserChecklistsPreviewResponse;
+import com.bang_ggood.checklist.service.ChecklistService;
 import com.bang_ggood.user.UserFixture;
+import com.bang_ggood.user.domain.User;
 import com.bang_ggood.user.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -24,6 +30,8 @@ class AuthServiceTest extends IntegrationTestSupport {
     private OauthClient oauthClient;
     @Autowired
     private AuthService authService;
+    @Autowired
+    private ChecklistService checklistService;
     @Autowired
     private UserRepository userRepository;
 
@@ -54,5 +62,43 @@ class AuthServiceTest extends IntegrationTestSupport {
 
         // then
         Assertions.assertThat(token).isNotBlank();
+    }
+
+    @DisplayName("로그인 성공 : 회원 가입시 디폴트 체크리스트 질문을 추가한다.")
+    @Test
+    void login_default_checklist_question() {
+        // given
+        Mockito.when(oauthClient.requestOauthInfo(any(OauthLoginRequest.class)))
+                .thenReturn(UserFixture.OAUTH_INFO_RESPONSE_USER2);
+
+        // when
+        String token = authService.login(oauthLoginRequest);
+
+        // then
+        User user = authService.extractUser(token);
+        ChecklistQuestionsResponse checklistQuestions = checklistService.readChecklistQuestions(user);
+
+        int sum = 0;
+        for (CategoryQuestionsResponse response: checklistQuestions.categories()) {
+            sum += response.questions().size();
+        }
+
+        Assertions.assertThat(sum).isEqualTo(Question.findDefaultQuestions().size());
+    }
+
+    @DisplayName("로그인 성공 : 회원 가입시 디폴트 체크리스트를 추가한다.")
+    @Test
+    void login_default_checklist() {
+        // given
+        Mockito.when(oauthClient.requestOauthInfo(any(OauthLoginRequest.class)))
+                .thenReturn(UserFixture.OAUTH_INFO_RESPONSE_USER2);
+
+        // when
+        String token = authService.login(oauthLoginRequest);
+
+        // then
+        User user = authService.extractUser(token);
+        UserChecklistsPreviewResponse response = checklistService.readUserChecklistsPreview(user);
+        Assertions.assertThat(response.checklists()).hasSize(1);
     }
 }

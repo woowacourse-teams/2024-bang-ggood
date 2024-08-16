fe04a60fd8673cd1b9169120eadaf06c7007df7c
jinwoo22 <jenny010925@naver.com>
2024-08-16 20:28:20 +0900

    test

--- backend/bang-ggood/src/main/java/com/bang_ggood/Application.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/Application.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(Application.class, args);
     }
 

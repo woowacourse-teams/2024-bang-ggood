d1038d0bf152e3cf94ca1a3316ddfdf74a0460e2
skiende74 <skiende74@gmail.com>
2024-08-16 17:35:00 +0900

    feat: 관리비포함항목 상수 순서변경

--- frontend/src/constants/roomInfo.ts
+++ frontend/src/constants/roomInfo.ts
@@ -1,3 +1,3 @@
 export const roomStructures = ['오픈형 원룸', '분리형 원룸', '투룸', '쓰리룸 이상'] as const;
 export const roomFloorLevels = ['지상', '반지하/지하', '옥탑'] as const;
-export const roomIncludedUtilites = ['난방', '수도', '인터넷', '전기'] as const;
+export const roomIncludedUtilites = ['수도', '인터넷', '전기', '가스'] as const;

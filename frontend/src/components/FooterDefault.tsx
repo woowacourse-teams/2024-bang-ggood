e0f69b782064f4fc0adce1c1171ee342476fb5e4
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-16 14:36:53 +0900

    feat: 푸터 리팩토링 및 단순화

--- frontend/src/components/FooterDefault.tsx
+++ frontend/src/components/FooterDefault.tsx
@@ -1,15 +1,12 @@
 import Footer from '@/components/_common/Footer/Footer';
-import { ROUTE_PATH } from '@/constants/routePath';
 
 const FooterDefault = () => {
   return (
     <Footer>
-      {[
-        { node: <Footer.HomeLogo />, nodeActive: <Footer.HomeLogoActive />, path: ROUTE_PATH.root },
-        { node: <Footer.LocationLogo />, nodeActive: <Footer.LocationLogoActive />, path: ROUTE_PATH.location },
-        { node: <Footer.ChecklistLogo />, nodeActive: <Footer.ChecklistLogoActive />, path: ROUTE_PATH.checklistList },
-        { node: <Footer.MyPageLogo />, nodeActive: <Footer.MyPageLogoActive />, path: ROUTE_PATH.myPage },
-      ]}
+      <Footer.HomeLogo />
+      <Footer.LocationLogo />
+      <Footer.ChecklistLogo />
+      <Footer.MyPageLogo />
     </Footer>
   );
 };

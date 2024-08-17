4801db7ae427d9868d3fbbdb6035bfec20765421
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-18 03:48:19 +0900

    chore: 목 데이터 추가 및 api 타입 단언

--- frontend/src/apis/article.ts
+++ frontend/src/apis/article.ts
@@ -1,8 +1,9 @@
 import fetcher from '@/apis/fetcher';
 import { BASE_URL, ENDPOINT } from '@/apis/url';
+import { Article } from '@/types/article';
 
 export const getArticleList = async () => {
   const response = await fetcher.get({ url: BASE_URL + ENDPOINT.ARTICLES });
   const data = await response.json();
-  return data.articles;
+  return data.articles as Article[];
 };

29790c897cc6fdb6ae4e61e17d10c240c6df1cb7
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-18 03:48:40 +0900

    feat: 레이아웃 컴포넌트 사용성 업데이트

--- frontend/src/components/_common/layout/Layout.tsx
+++ frontend/src/components/_common/layout/Layout.tsx
@@ -1,15 +1,21 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { CSSProperties } from 'react';
+
+import { FOOTER_SIZE, HEADER_SIZE } from '@/constants/style';
+import theme from '@/styles/theme';
 
 interface Props {
   bgColor?: string;
   children: React.ReactNode;
   style?: CSSProperties;
+  withHeader?: boolean;
+  withFooter?: boolean;
 }
 
-const Layout = ({ bgColor = 'white', children, style }: Props) => {
+const Layout = ({ bgColor = theme.palette.white, children, withHeader = false, withFooter = false, style }: Props) => {
   return (
-    <S.Wrapper bgColor={bgColor} style={style}>
+    <S.Wrapper bgColor={bgColor} style={style} withHeader={withHeader} withFooter={withFooter}>
       {children}
     </S.Wrapper>
   );
@@ -18,13 +24,33 @@ const Layout = ({ bgColor = 'white', children, style }: Props) => {
 export default Layout;
 
 const S = {
-  Wrapper: styled.div<{ bgColor: string }>`
+  Wrapper: styled.div<{ bgColor: string; withHeader: boolean; withFooter: boolean }>`
+    box-sizing: border-box;
     overflow: hidden auto;
-
+    ${({ withHeader, withFooter }) => getHeightStyle(withHeader, withFooter)}
     padding: 16px;
 
     background-color: ${({ bgColor }) => bgColor};
-    min-height: calc(100vh - 64px);
-    box-sizing: border-box;
   `,
+};
+
+const getHeightStyle = (withHeader: boolean, withFooter: boolean) => {
+  if (withHeader && withFooter) {
+    return css`
+      height: calc(100vh - ${HEADER_SIZE}px - ${FOOTER_SIZE}px);
+    `;
+  }
+  if (withHeader) {
+    return css`
+      height: calc(100vh - ${HEADER_SIZE});
+    `;
+  }
+  if (withFooter) {
+    return css`
+      height: calc(100vh - ${FOOTER_SIZE});
+    `;
+  }
+  return css`
+    height: 100vh;
+  `;
 };

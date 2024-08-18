054d84ef4e26e524b9e8523c9d4dfc91f95cf890
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-18 21:22:19 +0900

    design: 아티클 리스트 키워드 카드 배경색 추가

--- frontend/src/components/Article/ArticleCard.tsx
+++ frontend/src/components/Article/ArticleCard.tsx
@@ -5,14 +5,18 @@ import { ROUTE_PATH } from '@/constants/routePath';
 import { boxShadow, flexColumn, title2 } from '@/styles/common';
 import { Article } from '@/types/article';
 import formattedDate from '@/utils/formattedDate';
+import getSeqColor from '@/utils/getSeqColor';
 
 interface Props {
+  index: number;
   article: Article;
 }
 
-const ArticleCard = ({ article }: Props) => {
+const ArticleCard = ({ index, article }: Props) => {
   const navigate = useNavigate();
   const { articleId, keyword, title, summary, createdAt } = article;
+
+  const { color500 } = getSeqColor(index);
 
   const handleClick = () => {
     navigate(ROUTE_PATH.articleOne(articleId));
@@ -20,7 +24,7 @@ const ArticleCard = ({ article }: Props) => {
 
   return (
     <S.Container onClick={handleClick}>
-      <S.Keyword> {keyword}</S.Keyword>
+      <S.Keyword bgColor={color500}> {keyword}</S.Keyword>
       <S.Title>{title}</S.Title>
       <S.Label>{summary}</S.Label>
       <S.Label>{formattedDate(createdAt)}</S.Label>
@@ -43,14 +47,14 @@ const S = {
     background-color: ${({ theme }) => theme.palette.white};
     ${boxShadow};
   `,
-  Keyword: styled.span`
-    align-self: flex-start;
+  Keyword: styled.span<{ bgColor: string }>`
     padding: 4px 10px;
 
-    background-color: ${({ theme }) => theme.palette.grey100};
+    background-color: ${({ bgColor }) => bgColor};
 
-    color: ${({ theme }) => theme.palette.grey600};
-    font-size: ${({ theme }) => theme.text.size.xxSmall};
+    color: ${({ theme }) => theme.palette.white};
+    font-size: ${({ theme }) => theme.text.size.xSmall};
+    align-self: flex-start;
 
     box-sizing: content-box;
     border-radius: 6px;

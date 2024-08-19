c036c61bc958301938a518341848564c58e94e9f
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-19 16:00:48 +0900

    feat: 쿼리 내부 타입 단언

--- frontend/src/hooks/query/useGetArticleQuery.ts
+++ frontend/src/hooks/query/useGetArticleQuery.ts
@@ -2,9 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getArticle } from '@/apis/article';
 import { QUERY_KEYS } from '@/constants/queryKeys';
+import { Article } from '@/types/article';
 
 const useGetArticleQuery = (articleId: string) => {
-  return useQuery({ queryKey: [QUERY_KEYS.ARTICLE, articleId], queryFn: () => getArticle(Number(articleId)) });
+  return useQuery<Article>({ queryKey: [QUERY_KEYS.ARTICLE, articleId], queryFn: () => getArticle(Number(articleId)) });
 };
 
 export default useGetArticleQuery;

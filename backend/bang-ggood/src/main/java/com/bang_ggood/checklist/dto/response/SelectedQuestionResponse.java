e857ba394a32ec1248c9ed3d0b173f2db49d667c
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2024-08-19 18:19:23 +0900

    [BE] 체크리스트 조회 API를 수정한다. (#423)

--- backend/bang-ggood/src/main/java/com/bang_ggood/checklist/dto/response/SelectedQuestionResponse.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/checklist/dto/response/SelectedQuestionResponse.java
@@ -2,7 +2,7 @@ package com.bang_ggood.checklist.dto.response;
 
 import com.bang_ggood.checklist.domain.ChecklistQuestion;
 
-public record SelectedQuestionResponse(Integer questionId, String title, String subtitle, String grade) {
+public record SelectedQuestionResponse(Integer questionId, String title, String subtitle, String answer) {
 
     public static SelectedQuestionResponse of(ChecklistQuestion checklistQuestion) {
         return new SelectedQuestionResponse(

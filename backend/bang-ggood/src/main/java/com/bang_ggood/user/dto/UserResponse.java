4aa5883c6ebe01ff442691c9f205a27ac248d082
tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>
2024-08-19 17:15:33 +0900

    [BE] 유저 정보를 조회한다. (#413)

---
+++ backend/bang-ggood/src/main/java/com/bang_ggood/user/dto/UserResponse.java
@@ -0,0 +1,13 @@
+package com.bang_ggood.user.dto;
+
+import com.bang_ggood.user.domain.User;
+import java.time.LocalDateTime;
+
+public record UserResponse(Long id, String name, String email, LocalDateTime createdAt) {
+
+    public static UserResponse from(User user) {
+        return new UserResponse(
+                user.getId(), user.getName(), user.getEmail(), user.getCreatedAt()
+        );
+    }
+}

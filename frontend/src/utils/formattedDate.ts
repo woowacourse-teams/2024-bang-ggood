7d13278004f2a3d96daf9a64c35b2595abe35549
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-19 18:20:35 +0900

    feat: 30일 이전은 `~일 전` 으로 날짜 변경

--- frontend/src/utils/formattedDate.ts
+++ frontend/src/utils/formattedDate.ts
@@ -1,11 +1,15 @@
 const formattedDate = (stringDate: string, splitter: string = '/') => {
   const date = new Date(stringDate);
   const nowDate = new Date();
+
+  const dayDifference = Math.floor((nowDate.getTime() - date.getTime()) / (1000 * 60 * 60 * 24));
   const hourDifference = Math.floor((nowDate.getTime() - date.getTime()) / (1000 * 60 * 60));
   const minuteDifference = Math.floor((nowDate.getTime() - date.getTime()) / (1000 * 60));
 
   if (hourDifference < 1) return `${minuteDifference}분 전`;
   else if (hourDifference < 24) return `${hourDifference}시간 전`;
+  else if (dayDifference < 30) return `${dayDifference}일 전`;
+
   return date.toISOString().slice(2, 10).replace(/-/g, splitter);
 };
 

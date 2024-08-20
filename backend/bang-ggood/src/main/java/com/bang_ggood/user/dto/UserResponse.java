baf90eeff8019f0fbafb9f907aafb98ae12a94d6
tkdgur0906 <tkdgur0906@naver.com>
2024-08-20 15:29:27 +0900

    refactor: response 필드 명 수정

--- backend/bang-ggood/src/main/java/com/bang_ggood/user/dto/UserResponse.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/user/dto/UserResponse.java
@@ -3,7 +3,7 @@ package com.bang_ggood.user.dto;
 import com.bang_ggood.user.domain.User;
 import java.time.LocalDateTime;
 
-public record UserResponse(Long userId, String nickname, String userEmail, LocalDateTime createdAt) {
+public record UserResponse(Long userId, String username, String userEmail, LocalDateTime createdAt) {
     public static UserResponse from(User user) {
         return new UserResponse(
                 user.getId(), user.getName(), user.getEmail(), user.getCreatedAt()

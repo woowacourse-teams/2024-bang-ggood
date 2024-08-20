c527be575801c37ad7bed395ab2c197a1a37eb4a
skiende74 <skiende74@gmail.com>
2024-08-20 14:28:35 +0900

    fix: 무한리렌더 해결

--- frontend/src/hooks/useInitialChecklist.ts
+++ frontend/src/hooks/useInitialChecklist.ts
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import useGetChecklistQuestionQuery from '@/hooks/query/useGetChecklistQuestionQuery';
 import useHandleTipBox from '@/hooks/useHandleTipBox';
 import useChecklistStore from '@/store/useChecklistStore';
@@ -8,12 +10,14 @@ const useInitialChecklist = () => {
   const resetToDefaultOptions = useOptionStore(state => state.resetToDefaultOptions);
   const { resetShowTipBox } = useHandleTipBox('OPTION'); // TODO: 상수화 처리
 
-  const { data, isLoading, isError, error } = useGetChecklistQuestionQuery();
-  if (isLoading) return { data, isLoading, isError, error };
-  setAnswerInQuestion(data); // 체크리스트 질문에 대한 답안지 객체 생성
-  resetToDefaultOptions(); // 옵션 선택지 리셋
-  resetShowTipBox(); // 로컬 스토리지 팁 보이는 여부 리셋
-  return { data, isLoading, isError, error };
+  const result = useGetChecklistQuestionQuery();
+  useEffect(() => {
+    setAnswerInQuestion(result.data ?? []); // 체크리스트 질문에 대한 답안지 객체 생성
+    resetToDefaultOptions(); // 옵션 선택지 리셋
+    resetShowTipBox(); // 로컬 스토리지 팁 보이는 여부 리셋
+  }, [result.data]);
+
+  return result;
 };
 
 export default useInitialChecklist;

fe18cd5de0b784e07680fad652c398273d223966
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-08-21 10:21:51 +0900

    feat(FoomFixture): 바뀐 필드에 따라 일괄 수정

--- backend/bang-ggood/src/test/java/com/bang_ggood/room/RoomFixture.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/room/RoomFixture.java
@@ -6,42 +6,49 @@ import com.bang_ggood.room.domain.FloorLevel;
 import com.bang_ggood.room.domain.Room;
 import com.bang_ggood.room.domain.Structure;
 import com.bang_ggood.room.dto.request.RoomRequest;
+import java.util.List;
 
 public class RoomFixture {
 
     public static final Room ROOM_1 = new Room(
-            "살기 좋은 방", "부개역", 10, "인천광역시 부평구",
-            Type.APARTMENT, 3.5, 3, FloorLevel.GROUND, Structure.TWO_ROOM
+            "살기 좋은 방", "인천광역시 부평구", null,
+            "부개", 10, FloorLevel.GROUND,3,
+            Structure.TWO_ROOM, 3.5
     );
 
     public static final Room ROOM_2 = new Room(
-            "살기 싫은 방", "대구역", 10, "대구광역시 중구",
-            Type.OFFICETEL, 4.0, null, FloorLevel.BASEMENT, Structure.DIVIDED_ONE_ROOM
+            "살기 싫은 방", "대구광역시 중구","롯데타워",
+            "대구", 10, FloorLevel.BASEMENT, null,
+             Structure.DIVIDED_ONE_ROOM, 4.0
     );
 
     public static final Room ROOM_3 = new Room(
-            "살기 애매한 방", "잠실역", 5, "서울특별시 송파구",
-            Type.VILLA, 5.5, null, FloorLevel.ROOFTOP, Structure.DUPLEX
+            "살기 애매한 방", "서울특별시 송파구","루터회관",
+            "잠실", 5, FloorLevel.ROOFTOP, null,
+            Structure.DUPLEX, 5.5
     );
 
     public static final RoomRequest ROOM_CREATE_REQUEST = new RoomRequest(
-            "방이름", 1000, 50, 12, "부산광역시 루터회관",
-            "잠실역", 10, "방끗공인중개사", Type.VILLA.getName(),
-            Structure.TWO_ROOM.getName(), 3.3, 3, FloorLevel.GROUND.getName(),
-            OccupancyMonth.APRIL.getMonth(), OccupancyPeriod.EARLY.getPeriod(), "메모", "한줄평"
+            "방이름", "부산광역시 북구", "루터회관", "잠실",
+            10, 1000, 50, 5,
+            List.of(1, 3), FloorLevel.GROUND.getName(), 10,
+            Structure.TWO_ROOM.getName(), 3.3, 12, OccupancyMonth.APRIL.getMonth(), OccupancyPeriod.EARLY.getPeriod(),
+            "방끗공인중개사", "메모", "한줄평"
     );
 
     public static final RoomRequest ROOM_UPDATE_REQUEST = new RoomRequest(
-            "방이름", 1000, 50, 12, "부산광역시 루터회관",
-            "잠실역", 10, "방끗공인중개사", Type.VILLA.getName(),
-            Structure.OPEN_ONE_ROOM.getName(), 3.3, 3, FloorLevel.GROUND.getName(),
-            OccupancyMonth.APRIL.getMonth(), OccupancyPeriod.EARLY.getPeriod(), "메모", "한줄평"
+            "방이름", "부산광역시 루터회관", "잠실역", "루터회관",
+            10, 1000, 50, 5,
+            List.of(1, 3), FloorLevel.GROUND.getName(), 10,
+            Structure.OPEN_ONE_ROOM.getName(), 3.3, 12, OccupancyMonth.APRIL.getMonth(), OccupancyPeriod.EARLY.getPeriod(),
+            "방끗공인중개사", "메모", "한줄평"
     );
 
     public static final RoomRequest ROOM_CREATE_REQUEST_NO_ROOM_NAME = new RoomRequest(
-            null, 1000, 50, 12, "부산광역시 루터회관",
-            "잠실역", 10, "방끗공인중개사", Type.VILLA.getName(),
-            Structure.TWO_ROOM.getName(), 3.3, 3, FloorLevel.GROUND.getName(),
-            OccupancyMonth.APRIL.getMonth(), OccupancyPeriod.EARLY.getPeriod(), "메모", "한줄평"
+            null, "부산광역시 루터회관", "루터회관", "잠실역",
+            10, 1000, 50, 5,
+            List.of(1, 3), FloorLevel.GROUND.getName(), 10,
+            Structure.TWO_ROOM.getName(), 3.3, 12, OccupancyMonth.APRIL.getMonth(), OccupancyPeriod.EARLY.getPeriod(),
+            "방끗공인중개사", "메모", "한줄평"
     );
 }

ad631069e94ebc57f66bb44571bdcdca74839569
skiende74 <skiende74@gmail.com>
2024-08-21 10:56:06 +0900

    feat: N번째 방 구현

--- frontend/src/components/NewChecklist/NewRoomInfoForm/RoomName.tsx
+++ frontend/src/components/NewChecklist/NewRoomInfoForm/RoomName.tsx
@@ -1,12 +1,26 @@
+import { useEffect } from 'react';
 import { useStore } from 'zustand';
 
 import FormField from '@/components/_common/FormField/FormField';
-import checklistRoomInfoStore from '@/store/checklistRoomInfoStore';
+import useGetChecklistListQuery from '@/hooks/query/useGetChecklistListQuery';
+import checklistRoomInfoStore, { initialRoomInfo } from '@/store/checklistRoomInfoStore';
 
 const RoomName = () => {
   const actions = useStore(checklistRoomInfoStore, state => state.actions);
   const roomName = useStore(checklistRoomInfoStore, state => state.rawValue.roomName);
   const errorMessage = useStore(checklistRoomInfoStore, state => state.errorMessage.roomName);
+
+  const { data: checklistList } = useGetChecklistListQuery();
+
+  useEffect(() => {
+    if (!checklistList) return;
+    const count = checklistList.filter(
+      checklist => new Date(checklist.createdAt).getUTCDay() === new Date().getUTCDay(),
+    ).length;
+    if (roomName !== initialRoomInfo.roomName) return;
+    const date = new Date();
+    actions.set('roomName', `${date.getMonth()}월 ${date.getDay()}일 ${count + 1}번째 방`);
+  }, [checklistList]);
 
   return (
     <FormField>

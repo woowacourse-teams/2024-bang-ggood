88843cac6954c31a597057d35baf784d73c76d22
skiende74 <skiende74@gmail.com>
2024-08-21 11:55:55 +0900

    refactor: 커스텀훅으로 분리

--- frontend/src/components/NewChecklist/NewRoomInfoForm/RoomName.tsx
+++ frontend/src/components/NewChecklist/NewRoomInfoForm/RoomName.tsx
@@ -1,26 +1,15 @@
-import { useEffect } from 'react';
 import { useStore } from 'zustand';
 
 import FormField from '@/components/_common/FormField/FormField';
-import useGetChecklistListQuery from '@/hooks/query/useGetChecklistListQuery';
-import checklistRoomInfoStore, { initialRoomInfo } from '@/store/checklistRoomInfoStore';
+import useDefaultRoomName from '@/components/NewChecklist/NewRoomInfoForm/useDefaultRoomName';
+import checklistRoomInfoStore from '@/store/checklistRoomInfoStore';
 
 const RoomName = () => {
   const actions = useStore(checklistRoomInfoStore, state => state.actions);
   const roomName = useStore(checklistRoomInfoStore, state => state.rawValue.roomName);
   const errorMessage = useStore(checklistRoomInfoStore, state => state.errorMessage.roomName);
 
-  const { data: checklistList } = useGetChecklistListQuery();
-
-  useEffect(() => {
-    if (!checklistList) return;
-    const count = checklistList.filter(
-      checklist => new Date(checklist.createdAt).getUTCDay() === new Date().getUTCDay(),
-    ).length;
-    if (roomName !== initialRoomInfo.roomName) return;
-    const date = new Date();
-    actions.set('roomName', `${date.getMonth()}월 ${date.getDay()}일 ${count + 1}번째 방`);
-  }, [checklistList]);
+  useDefaultRoomName();
 
   return (
     <FormField>

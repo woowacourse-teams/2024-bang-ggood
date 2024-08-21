7a50b02ee3fe39fe2f18682e2cd311e5312a420f
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-08-21 11:48:37 +0900

    feat(ChecklistQuestionRepository): 체크리스트 ID로 조회

--- backend/bang-ggood/src/main/java/com/bang_ggood/checklist/repository/ChecklistQuestionRepository.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/checklist/repository/ChecklistQuestionRepository.java
@@ -2,8 +2,10 @@ package com.bang_ggood.checklist.repository;
 
 import com.bang_ggood.checklist.domain.ChecklistQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface ChecklistQuestionRepository extends JpaRepository<ChecklistQuestion, Long> {
@@ -12,4 +14,11 @@ public interface ChecklistQuestionRepository extends JpaRepository<ChecklistQues
             + "WHERE cq.checklist.id = :checklistId "
             + "AND cq.deleted = false")
     List<ChecklistQuestion> findByChecklistId(@Param("checklistId") long checklistId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE ChecklistQuestion cq "
+            + "SET cq.deleted = true "
+            + "WHERE cq.checklist.id = :checklistId")
+    void deleteAllByChecklistId(@Param("checklistId") Long checklistId);
 }

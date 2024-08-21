6f353c92330aa27efdfae32bfac77ce3fe624494
Hyelim Choi <74346290+healim01@users.noreply.github.com>
2024-08-21 20:05:36 +0900

    [FE] 디폴트 방 이름 날짜 버그를 수정한다.  (#522)

--- frontend/src/components/NewChecklist/NewRoomInfoForm/useDefaultRoomName.ts
+++ frontend/src/components/NewChecklist/NewRoomInfoForm/useDefaultRoomName.ts
@@ -9,14 +9,18 @@ const useDefaultRoomName = () => {
   const roomName = useStore(checklistRoomInfoStore, state => state.rawValue.roomName);
 
   const { data: checklistList } = useGetChecklistListQuery();
+
   useEffect(() => {
     if (!checklistList) return;
+
     const count = checklistList.filter(
       checklist => new Date(checklist.createdAt).getUTCDay() === new Date().getUTCDay(),
     ).length;
+
     if (roomName !== initialRoomInfo.roomName) return;
+
     const date = new Date();
-    actions.set('roomName', `${date.getMonth()}월 ${date.getDay()}일 ${count}번째 방`);
+    actions.set('roomName', `${date.getMonth() + 1}월 ${date.getDate()}일 ${count}번째 방`);
   }, [checklistList]);
 };
 

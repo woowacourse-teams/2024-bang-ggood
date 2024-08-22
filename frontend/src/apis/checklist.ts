c73726f1bf9b14bc91f6f3c0f6ce72b9df66bac1
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-22 15:14:00 +0900

    feat: 체크리스트 수정 api 추가

--- frontend/src/apis/checklist.ts
+++ frontend/src/apis/checklist.ts
@@ -33,6 +33,9 @@ export const postChecklist = async (checklist: ChecklistPostForm) => {
   return response;
 };
 
+export const putChecklist = async (id: number, checklist: ChecklistPostForm) => {
+  checklist.room = mapObjUndefinedToNull(checklist.room);
+  const response = await fetcher.put({ url: BASE_URL + ENDPOINT.CHECKLIST_ID(id), body: checklist });
   return response;
 };
 

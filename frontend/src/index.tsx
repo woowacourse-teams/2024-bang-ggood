e1f4d9d163e49b7b47aeefdd1ef63a941e491c02
skiende74 <skiende74@gmail.com>
2024-08-22 21:24:07 +0900

    feat: edit page에서는 putChecklist 한다.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,15 +7,15 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 
 async function enableMocking() {
-  // if (process.env.DEV_MODE !== 'on') {
-  //   return;
-  // }
-  // const { worker } = await import('./mocks/browser');
-  // await worker.start({
-  //   serviceWorker: {
-  //     url: '/mockServiceWorker.js',
-  //   },
-  // });
+  if (process.env.DEV_MODE !== 'on') {
+    return;
+  }
+  const { worker } = await import('./mocks/browser');
+  await worker.start({
+    serviceWorker: {
+      url: '/mockServiceWorker.js',
+    },
+  });
 }
 
 enableMocking().then(() => {

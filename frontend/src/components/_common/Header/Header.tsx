e0b439c09e947b90d7d4eefa943f9a05f5ccbaac
Hyelim Choi <HyelimChoi01@gmail.com>
2024-08-22 14:17:33 +0900

    design: 헤더 사이즈 및 레이아웃 변경

--- frontend/src/components/_common/Header/Header.tsx
+++ frontend/src/components/_common/Header/Header.tsx
@@ -38,7 +38,7 @@ const S = {
     z-index: ${({ theme }) => theme.zIndex.HEADER};
     width: 100%;
     height: ${HEADER_SIZE}rem;
-    padding: 1.6rem;
+    padding: 2rem 1.6rem 1.2rem;
 
     background-color: ${({ theme }) => theme.palette.white};
     max-width: 60rem;

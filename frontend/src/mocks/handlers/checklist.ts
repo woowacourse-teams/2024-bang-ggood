d5a2386476bc8a74ea6a880ddb17025613bd84c1
skiende74 <skiende74@gmail.com>
2024-09-09 18:03:47 +0900

    feat(mock): 체크리스트 좋아요 모킹추가

--- frontend/src/mocks/handlers/checklist.ts
+++ frontend/src/mocks/handlers/checklist.ts
@@ -38,4 +38,20 @@ export const checklistHandlers = [
   http.get(BASE_URL + ENDPOINT.CHECKLIST_ALL_QUESTION, () => {
     return HttpResponse.json(checklistAllQuestions, { status: 200 });
   }),
+  http.post(BASE_URL + ENDPOINT.LIKE(1), () => {
+    const id = 1;
+    addLike(id);
+    return HttpResponse.json(null, { status: 200 });
+  }),
+  http.delete(BASE_URL + ENDPOINT.LIKE(1), () => {
+    removeLike(1);
+    return HttpResponse.json(null, { status: 200 });
+  }),
+  http.get(BASE_URL + ENDPOINT.LIKE(1), () => {
+    return HttpResponse.json(checklist.get(1), { status: 200 });
+  }),
 ];
+
+const checklist = new Map();
+const addLike = (id: number) => checklist.set(id, true);
+const removeLike = (id: number) => checklist.set(id, false);

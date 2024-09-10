34513091facc045ffcda355a1cc83b3f3f228ef3
skiende74 <skiende74@gmail.com>
2024-09-10 16:17:58 +0900

    feat: 체크옵션도 persist (로컬스토리지)적용

--- frontend/src/store/useOptionStore.ts
+++ frontend/src/store/useOptionStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 import { DEFAULT_OPTIONS, OPTION_COUNT, OPTIONS } from '@/constants/options';
 
@@ -15,50 +16,61 @@ interface OptionState {
   getSelectedOptionsName: () => (string | undefined)[];
 }
 
-const useOptionStore = create<OptionState>((set, get) => ({
-  selectedOptions: DEFAULT_OPTIONS,
+const useOptionStore = create<OptionState>()(
+  persist(
+    (set, get) => ({
+      selectedOptions: DEFAULT_OPTIONS,
 
-  getSelectedOptionsName: () => {
-    const state = get();
-    const optionsNames = state.selectedOptions.map(optionId => {
-      const target = OPTIONS.find(option => option.id === optionId);
-      return target?.displayName;
-    });
-    return optionsNames;
-  },
+      getSelectedOptionsName: () => {
+        const state = get();
+        const optionsNames = state.selectedOptions.map(optionId => {
+          const target = OPTIONS.find(option => option.id === optionId);
+          return target?.displayName;
+        });
+        return optionsNames;
+      },
 
-  resetToDefaultOptions: () => {
-    set({ selectedOptions: DEFAULT_OPTIONS });
-  },
+      resetToDefaultOptions: () => {
+        set({ selectedOptions: DEFAULT_OPTIONS });
+      },
 
-  setSelectedOptions: (options: number[]) => set({ selectedOptions: options }),
+      setSelectedOptions: (options: number[]) => set({ selectedOptions: options }),
 
-  isSelectedOption: optionId => get().selectedOptions.includes(optionId),
+      isSelectedOption: optionId => get().selectedOptions.includes(optionId),
 
-  addOption: option =>
-    set(state => ({
-      selectedOptions: [...state.selectedOptions, option],
-    })),
+      addOption: option =>
+        set(state => ({
+          selectedOptions: [...state.selectedOptions, option],
+        })),
 
-  removeOption: option =>
-    set(state => ({
-      selectedOptions: state.selectedOptions.filter(o => o !== option),
-    })),
+      removeOption: option =>
+        set(state => ({
+          selectedOptions: state.selectedOptions.filter(o => o !== option),
+        })),
 
-  addAllOptions: () => {
-    set(() => ({
-      selectedOptions: Array.from({ length: OPTION_COUNT }, (_, i) => i + 1),
-    }));
-  },
+      addAllOptions: () => {
+        set(() => ({
+          selectedOptions: Array.from({ length: OPTION_COUNT }, (_, i) => i + 1),
+        }));
+      },
 
-  removeAllOptions: () => {
-    set({ selectedOptions: [] });
-  },
+      removeAllOptions: () => {
+        set({ selectedOptions: [] });
+      },
 
-  isAllSelected: () => {
-    const state = get();
-    return state.selectedOptions.length === OPTION_COUNT;
-  },
-}));
+      isAllSelected: () => {
+        const state = get();
+        return state.selectedOptions.length === OPTION_COUNT;
+      },
+    }),
+    {
+      name: 'checklist-answer-option',
+      partialize: state => ({
+        selectedOptions: state.selectedOptions,
+        // actions는 저장하지 않음
+      }),
+    },
+  ),
+);
 
 export default useOptionStore;

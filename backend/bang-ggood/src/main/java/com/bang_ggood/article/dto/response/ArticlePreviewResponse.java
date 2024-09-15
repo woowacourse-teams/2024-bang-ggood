60bb59f1868b5a50163afeeaa6a95eb4db80fcce
tsulocalize <tsulocalizing@gmail.com>
2024-09-15 18:42:18 +0900

    feat: 아티클 응답에 Thumbnail 추가

--- backend/bang-ggood/src/main/java/com/bang_ggood/article/dto/response/ArticlePreviewResponse.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/article/dto/response/ArticlePreviewResponse.java
@@ -2,13 +2,14 @@ package com.bang_ggood.article.dto.response;
 
 import com.bang_ggood.article.domain.Article;
 
-public record ArticlePreviewResponse(Long articleId, String title, String keyword) {
+public record ArticlePreviewResponse(Long articleId, String title, String keyword, String thumbnail) {
 
     public static ArticlePreviewResponse from(Article article) {
         return new ArticlePreviewResponse(
                 article.getId(),
                 article.getTitle(),
-                article.getKeyword()
+                article.getKeyword(),
+                article.getThumbnail()
         );
     }
 }

966571e9cca9a2446d24c1e6f6615db39a3e4621
Hyelim Choi <HyelimChoi01@gmail.com>
2024-09-18 00:27:49 +0900

    test: 관리비 항목 선택 스토어 테스트 추가

--- frontend/src/store/checklistRoomInfoStore.test.ts
+++ frontend/src/store/checklistRoomInfoStore.test.ts
@@ -13,7 +13,7 @@ describe('useChecklistBasicInfoStore 테스트', () => {
     expect(store.getState()).toEqual(store.getInitialState());
   });
 
-  describe('roomName', () => {
+  describe('방 이름', () => {
     it('20자 이하 입력 시 정상 입력된다.', () => {
       store.getState().actions.onChange(makeEvent('roomName', '3'.repeat(20)));
 
@@ -30,6 +30,7 @@ describe('useChecklistBasicInfoStore 테스트', () => {
       expect(store.getState().errorMessage.roomName).not.toBe('');
     });
   });
+
   describe('보증금', () => {
     it('숫자 입력 후 빈 문자열을 설정할 경우(모두 지울 경우), 빈 문자열로 설정된다.', () => {
       store.getState().actions.onChange(makeEvent('deposit', '112'));
@@ -39,6 +40,27 @@ describe('useChecklistBasicInfoStore 테스트', () => {
       expect(store.getState().rawValue.deposit).toBe('');
     });
   });
+
+  describe('관리비', () => {
+    it.only('관리비 입력 필드에는 숫자만 입력 가능하다.', () => {
+      store.getState().actions.onChange(makeEvent('maintenanceFee', '관리비'));
+      expect(store.getState().rawValue.maintenanceFee).toBe('');
+
+      store.getState().actions.onChange(makeEvent('maintenanceFee', '123'));
+      expect(store.getState().rawValue.maintenanceFee).toBe('123');
+    });
+  });
+
+  describe('관리비 포함 항목', () => {
+    it.only('선택한 관리비 항목 종류만을 가지고 있는다.', () => {
+      store.getState().actions.set('includedMaintenances', [1]);
+      expect(store.getState().value.includedMaintenances).toStrictEqual([1]);
+
+      store.getState().actions.set('includedMaintenances', [1, 2]);
+      expect(store.getState().value.includedMaintenances).toStrictEqual([1, 2]);
+    });
+  });
+
   describe('층수', () => {
     it('4. 을 입력해도 입력상태가 지워지지 않는다.', () => {
       store.getState().actions.onChange(makeEvent('floor', '4.'));
@@ -46,6 +68,7 @@ describe('useChecklistBasicInfoStore 테스트', () => {
       expect(store.getState().rawValue.floor).toBe('4.');
     });
   });
+
   describe('평수', () => {
     it('4를 입력할 수 있다.', () => {
       store.getState().actions.onChange(makeEvent('size', '4'));

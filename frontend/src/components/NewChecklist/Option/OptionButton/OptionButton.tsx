23ba2809a2020bc9363526b6488c68e65f8f7228
Hyerin <125418818+ooherin@users.noreply.github.com>
2024-09-24 16:47:22 +0900

    [FE] 폰트를 최적화하는 코드를 추가한다.  (#639)

--- frontend/src/components/NewChecklist/Option/OptionButton/OptionButton.tsx
+++ frontend/src/components/NewChecklist/Option/OptionButton/OptionButton.tsx
@@ -40,13 +40,7 @@ const OptionButton = ({ option, isSelected }: { option: OptionWithIcon; isSelect
   );
 };
 
-/*사용자가 누른 옵션 버튼만 리렌더링*/
-const MemoizedOptionButton = React.memo((props: { option: OptionWithIcon; isSelected: boolean }) => {
-  const isSelected = useSelectedOptionStore(state => state.actions.isSelectedOption(props.option.id));
-  return <OptionButton option={props.option} isSelected={isSelected} />;
-});
-
-export default MemoizedOptionButton;
+export default React.memo(OptionButton);
 
 const S = {
   Box: styled.div<{ color: string; borderColor: string }>`

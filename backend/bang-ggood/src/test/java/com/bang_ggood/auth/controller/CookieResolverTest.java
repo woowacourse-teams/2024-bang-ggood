cd380a653d01ee48f1c2895c12dc4dcad2cf767e
jinwoo22 <jenny010925@naver.com>
2024-09-25 00:55:56 +0900

    test: CookieResolver 테스트 코드 수정

--- backend/bang-ggood/src/test/java/com/bang_ggood/auth/controller/CookieResolverTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/auth/controller/CookieResolverTest.java
@@ -1,7 +1,5 @@
-package com.bang_ggood.auth.config;
+package com.bang_ggood.auth.controller;
 
-import com.bang_ggood.auth.controller.CookieProvider;
-import com.bang_ggood.auth.controller.CookieResolver;
 import com.bang_ggood.global.exception.BangggoodException;
 import com.bang_ggood.global.exception.ExceptionCode;
 import jakarta.servlet.http.Cookie;
@@ -11,9 +9,9 @@ import org.junit.jupiter.api.Test;
 
 class CookieResolverTest {
 
-    @DisplayName("쿠키에서 토큰 값 조회 성공")
+    @DisplayName("쿠키에서 토큰 값 조회 성공 : 값이 액세스 토큰일 때")
     @Test
-    void extractToken() {
+    void extractAccessToken() {
         // given
         String expectedToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2IiwiaWF0Ijox";
         CookieResolver cookieResolver = new CookieResolver();
@@ -21,6 +19,21 @@ class CookieResolverTest {
 
         // when
         String token = cookieResolver.extractAccessToken(cookies);
+
+        // then
+        Assertions.assertThat(token).isEqualTo(expectedToken);
+    }
+
+    @DisplayName("쿠키에서 토큰 값 조회 성공 : 값이 리프레시 토큰일 때")
+    @Test
+    void extractRefreshToken() {
+        // given
+        String expectedToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2IiwiaWF0Ijox";
+        CookieResolver cookieResolver = new CookieResolver();
+        Cookie[] cookies = { new Cookie(CookieProvider.REFRESH_TOKEN_COOKIE_NAME, expectedToken) };
+
+        // when
+        String token = cookieResolver.extractRefreshToken(cookies);
 
         // then
         Assertions.assertThat(token).isEqualTo(expectedToken);

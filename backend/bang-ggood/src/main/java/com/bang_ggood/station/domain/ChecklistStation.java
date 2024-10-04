929c5e8f38450af0e398955246ece34746d344eb
tsulocalize <tsulocalizing@gmail.com>
2024-10-04 11:00:36 +0900

    feat: checklistStation 도메인 생성

---
+++ backend/bang-ggood/src/main/java/com/bang_ggood/station/domain/ChecklistStation.java
@@ -0,0 +1,51 @@
+package com.bang_ggood.station.domain;
+
+import com.bang_ggood.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class ChecklistStation extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long checklistId;
+
+    private String stationName;
+
+    private String stationLine;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getChecklistId() {
+        return checklistId;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public String getStationLine() {
+        return stationLine;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChecklistStation that = (ChecklistStation) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}

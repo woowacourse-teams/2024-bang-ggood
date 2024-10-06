281917245ed68763cb15e29a3fa7003b65f5064e
ooherin <555ohr@naver.com>
2024-10-06 16:38:31 +0900

    fix: 방 비교 관련 코드 모두 삭제

--- frontend/src/mocks/handlers/checklist.ts
+++ frontend/src/mocks/handlers/checklist.ts
@@ -5,7 +5,6 @@ import { checklistAllQuestions } from '@/mocks/fixtures/checklistAllQuestions';
 import { checklistDetail } from '@/mocks/fixtures/checklistDetail';
 import { checklistList } from '@/mocks/fixtures/checklistList';
 import { checklistQuestions } from '@/mocks/fixtures/checklistQuestions';
-import { threeRoomsForCompare, twoRoomsForCompare } from '@/mocks/fixtures/roomsForCompare';
 
 export const checklistHandlers = [
   http.get(BASE_URL + ENDPOINT.CHECKLIST_QUESTION, () => {
@@ -26,14 +25,6 @@ export const checklistHandlers = [
 
   http.post(BASE_URL + ENDPOINT.CHECKLISTS, () => {
     return HttpResponse.json({}, { status: 201 });
-  }),
-
-  http.get(BASE_URL + ENDPOINT.CHECKLIST_COMPARE({ id1: 1, id2: 2, id3: 3 }), ({ request }) => {
-    const url = new URL(request.url);
-    const roomIds = url.searchParams.getAll('id');
-
-    if (!roomIds[2]) return HttpResponse.json(twoRoomsForCompare, { status: 200 });
-    return HttpResponse.json(threeRoomsForCompare, { status: 200 });
   }),
 
   http.get(BASE_URL + ENDPOINT.CHECKLIST_ALL_QUESTION, () => {

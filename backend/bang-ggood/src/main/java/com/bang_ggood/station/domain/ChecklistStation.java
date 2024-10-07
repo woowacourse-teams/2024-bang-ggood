8b6171c78796cf7bfe3134bbbf40f340260fceed
tsulocalize <tsulocalizing@gmail.com>
2024-10-07 12:34:23 +0900

    refactor: lombok 적용

--- backend/bang-ggood/src/main/java/com/bang_ggood/station/domain/ChecklistStation.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/station/domain/ChecklistStation.java
@@ -5,8 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import java.util.Objects;
 
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
 @Entity
 public class ChecklistStation extends BaseEntity {
 
@@ -27,29 +33,6 @@ public class ChecklistStation extends BaseEntity {
         this.stationName = stationName;
         this.stationLine = stationLine;
         this.walkingTime = walkingTime;
-    }
-
-    protected ChecklistStation() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getChecklistId() {
-        return checklistId;
-    }
-
-    public String getStationName() {
-        return stationName;
-    }
-
-    public String getStationLine() {
-        return stationLine;
-    }
-
-    public int getWalkingTime() {
-        return walkingTime;
     }
 
     @Override

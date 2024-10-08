05fb9c8c8d1473308af0630fa36d0159ab5ab764
jinwoo22 <jenny010925@naver.com>
2024-10-08 10:54:30 +0900

    style: 공백 제거

--- backend/bang-ggood/src/main/java/com/bang_ggood/auth/dto/response/RefreshTokenCheckResponse.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/auth/dto/response/RefreshTokenCheckResponse.java
@@ -2,7 +2,7 @@ package com.bang_ggood.auth.dto.response;
 
 public record RefreshTokenCheckResponse(boolean isRefreshTokenExist) {
 
-    public static RefreshTokenCheckResponse from (boolean isRefreshTokenExist) {
+    public static RefreshTokenCheckResponse from(boolean isRefreshTokenExist) {
         return new RefreshTokenCheckResponse(isRefreshTokenExist);
     }
 }

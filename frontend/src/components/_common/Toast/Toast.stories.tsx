6a489ae420fcdd46a831a75a11ef3c1097169e81
Hyelim Choi <HyelimChoi01@gmail.com>
2024-10-09 14:14:47 +0900

    test: 토스트 스토리북 추가

--- frontend/src/components/_common/Toast/Toast.stories.tsx
+++ frontend/src/components/_common/Toast/Toast.stories.tsx
@@ -2,7 +2,9 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import Button from '@/components/_common/Button/Button';
 import Toast from '@/components/_common/Toast/Toast';
+import ToastContainer from '@/components/_common/Toast/ToastContainer';
 import useToast from '@/hooks/useToast';
+
 /**
  * Toast는 사용자에게 일정시간 동안 알림을 주는 컴포넌트입니다.
  */
@@ -12,24 +14,43 @@ const meta: Meta<typeof Toast> = {
   decorators: [
     Story => (
       <>
-        <Toast />
+        <ToastContainer />
         <Story />
       </>
     ),
   ],
 };
+
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Confirm: Story = {
   render: () => {
     const { showToast } = useToast();
 
     return (
-      <>
-        <Button label="토스트 생성" onClick={() => showToast('토스트가 생성되었습니다.')} />
-      </>
+      <div style={{ height: '100px' }}>
+        <Button
+          label="토스트 생성"
+          onClick={() => showToast({ message: '토스트가 생성되었습니다.', type: 'confirm' })}
+        />
+      </div>
+    );
+  },
+};
+
+export const Error: Story = {
+  render: () => {
+    const { showToast } = useToast();
+
+    return (
+      <div style={{ height: '100px' }}>
+        <Button
+          label="토스트 생성"
+          onClick={() => showToast({ message: '에러 토스트가 생성되었습니다.', type: 'error' })}
+        />
+      </div>
     );
   },
 };

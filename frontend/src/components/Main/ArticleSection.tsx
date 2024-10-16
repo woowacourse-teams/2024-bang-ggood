540f2dc2e8b306b53f9e276d8d15acf02b4c08c6
Hyelim Choi <HyelimChoi01@gmail.com>
2024-10-16 14:59:36 +0900

    feat: 박스 에러 바운더리 생성

--- frontend/src/components/Main/ArticleSection.tsx
+++ frontend/src/components/Main/ArticleSection.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import { ErrorBoundary } from 'react-error-boundary';
 import { useNavigate } from 'react-router-dom';
 
-import ListErrorFallback from '@/components/_common/errorBoundary/ListErrorFallback';
+import BoxErrorFallback from '@/components/_common/errorBoundary/BoxErrorFallback';
 import ArticleCardContainer from '@/components/Main/ArticleCardContainer';
 import { ROUTE_PATH } from '@/constants/routePath';
 import { flexRow, flexSpaceBetween, title4 } from '@/styles/common';
@@ -20,7 +20,7 @@ const ArticleSection = () => {
         <S.Title>방 구하기 전 꼭 필요한 이야기</S.Title>
         <S.ShowMore onClick={handleClickShowMore}>더보기</S.ShowMore>
       </S.Row>
-      <ErrorBoundary FallbackComponent={ListErrorFallback}>
+      <ErrorBoundary FallbackComponent={BoxErrorFallback}>
         <ArticleCardContainer />
       </ErrorBoundary>
     </>

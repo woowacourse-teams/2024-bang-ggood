010c615f8d76b25f788b49e7d2b62f23da156fe4
ooherin <555ohr@naver.com>
2024-10-17 14:56:42 +0900

    feat: useValidateInput 생성 및 이메일 유효성 검사 생성

---
+++ frontend/src/hooks/useValidateInput.ts
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+
+// import { ErrorStatus } from '@/utils/validation';
+
+export enum ErrorStatus {
+  INVALID_FORMAT = 'invalidFormat',
+  // EXPIRED_CARD_DATE = 'expiredCardDate',
+  // INVALID_MONTH = 'invalidMonth',
+  // NAME_SHOULD_BE_CAPITAL = 'nameCapital',
+  // DOUBLE_SPACE = 'doubleSpace',
+  // ENTER_REQUIRED = 'enter',
+  // IS_NOT_NUMBER = 'is_not_number',
+}
+
+export type ValidateFuncWithPropsType = (
+  value: string,
+  currentName: string,
+) => { isValid: boolean; errorType?: string };
+
+export type InputChangeCallbackType = (value: string) => string;
+interface Props {
+  initialValue: string;
+  validates: ValidateFuncWithPropsType[];
+  inputChangeCallbacks?: InputChangeCallbackType[];
+}
+
+const useValidateInput = ({ initialValue, validates }: Props) => {
+  const [value, setValue] = useState(initialValue);
+  const [errors, setErrors] = useState<Set<string>>(new Set());
+  const [isValidated, setIsValidated] = useState(false);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    const newValue = value;
+
+    setValue(newValue);
+
+    const updateErrors = (errorType: string | undefined, isValid: boolean) => {
+      setErrors(prevErrors => {
+        const updatedErrors = new Set(prevErrors);
+        if (isValid) {
+          updatedErrors.delete(errorType!);
+        } else {
+          updatedErrors.add(errorType!);
+        }
+        return updatedErrors;
+      });
+    };
+
+    validates.forEach(validate => {
+      const errorResult = validate(newValue, name);
+      updateErrors(errorResult.errorType, errorResult.isValid);
+    });
+  };
+
+  useEffect(() => {
+    if (errors.size !== 0) {
+      setIsValidated(true);
+    } else {
+      setIsValidated(false);
+    }
+  }, [errors, value]);
+
+  return {
+    value,
+    setValue,
+    errors,
+    onChange,
+    isValidated,
+    setErrors,
+  };
+};
+
+export default useValidateInput;
--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,10 +1,29 @@
 import styled from '@emotion/styled';
 
 import { BangBangIcon, BangGgoodTextIcon } from '@/assets/assets';
+import Button from '@/components/_common/Button/Button';
 import FormField from '@/components/_common/FormField/FormField';
+import useValidateInput from '@/hooks/useValidateInput';
 import { flexCenter, title3 } from '@/styles/common';
 
 const AuthPage = () => {
+  const validateEmail = (value: string) => {
+    const regex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!regex.test(value)) {
+      return { errorType: '이메일 형식이 맞지 않습니다.', isValid: false };
+    }
+    return { errorType: '이메일 형식이 맞지 않습니다.', isValid: true };
+  };
+
+  const {
+    value: email,
+    errors: emailErrors,
+    onChange: onChangeEmail,
+  } = useValidateInput({
+    initialValue: '',
+    validates: [validateEmail],
+  });
+
   return (
     <>
       <S.Wrapper>
@@ -16,20 +35,29 @@ const AuthPage = () => {
           <S.Label>회원가입</S.Label>
           <FormField>
             <FormField.Label label="이메일" />
-            <FormField.Input />
+            <FormField.Input
+              value={email}
+              name="email"
+              onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => onChangeEmail(e)}
+            />
+            <FormField.ErrorMessage value={Array.from(emailErrors)[0]} />
           </FormField>
           <FormField>
             <FormField.Label label="닉네임" />
             <FormField.Input />
+            <FormField.ErrorMessage />
           </FormField>
           <FormField>
             <FormField.Label label="비밀번호" />
             <FormField.Input />
+            <FormField.ErrorMessage />
           </FormField>
           <FormField>
             <FormField.Label label="비밀번호 확인" />
             <FormField.Input />
+            <FormField.ErrorMessage />
           </FormField>
+          <Button label="회원가입" size="full" isSquare={true} color={'dark'} />
         </S.Box>
       </S.Wrapper>
     </>
@@ -65,7 +93,6 @@ const S = {
     display: flex;
     position: relative;
     width: 300px;
-    height: 400px;
     flex-direction: column;
     padding: 16px;
     border-radius: 10px;

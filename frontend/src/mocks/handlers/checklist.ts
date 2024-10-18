9240de6a5cd18c299f124973b98f21598a39804c
Hyelim Choi <HyelimChoi01@gmail.com>
2024-10-18 17:37:49 +0900

    chore: 라이크 핸들러 분리로 인해 코드 삭제

--- frontend/src/mocks/handlers/checklist.ts
+++ frontend/src/mocks/handlers/checklist.ts
@@ -22,26 +22,13 @@ export const checklistHandlers = [
   http.get(BASE_URL + ENDPOINT.CHECKLISTS, () => {
     return HttpResponse.json(checklistList, { status: 200 });
   }),
+
   http.get(BASE_URL + ENDPOINT.CHECKLISTS_LIKE, () => {
     const newChecklistList = {
       ...checklistList,
       checklists: checklistList.checklists.filter(({ isLiked }) => isLiked),
     };
     return HttpResponse.json(newChecklistList, { status: 200 });
-  }),
-
-  http.post(BASE_URL + ENDPOINT.CHECKLISTS, () => {
-    return HttpResponse.json({}, { status: 201 });
-  }),
-
-  http.post(BASE_URL + ENDPOINT.CHECKLISTS_V1, () => {
-    return HttpResponse.json(
-      JSON.stringify({
-        bangggoodCode: 'LIKE_ALREADY_EXISTS',
-        message: '체크리스트가 이미 좋아요 상태입니다.', // 추가적인 메시지 제공
-      }),
-      { status: 500 },
-    );
   }),
 
   http.get(BASE_URL + ENDPOINT.CHECKLIST_ALL_QUESTION, () => {

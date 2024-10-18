222dd5d63915b3dbecc1364711a6272c68f0b268
Hyelim Choi <HyelimChoi01@gmail.com>
2024-10-18 17:34:29 +0900

    chore: 메시지 상수 폴더 변경

--- frontend/src/hooks/query/useToggleLikeQuery.ts
+++ frontend/src/hooks/query/useToggleLikeQuery.ts
@@ -1,13 +1,11 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { deleteLike, postLike } from '@/apis/like';
-import { TOAST_MESSAGE } from '@/constants/message';
 import { QUERY_KEYS } from '@/constants/queryKeys';
 import useRefetchGetChecklistList from '@/hooks/query/useRefetchGetChecklistList';
-import useToast from '@/hooks/useToast';
 
 const useToggleLikeQuery = () => {
-  const { showToast } = useToast();
+  // const { showToast } = useToast();
   const queryClient = useQueryClient();
   const { invalidateChecklistListQuery } = useRefetchGetChecklistList();
 
@@ -17,11 +15,6 @@ const useToggleLikeQuery = () => {
     },
     retry: 3,
     onSuccess: () => {
-      invalidateChecklistListQuery();
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.CHECKLIST] });
-    },
-    onError: () => {
-      showToast({ message: TOAST_MESSAGE.LIKE_ERROR, type: 'error' });
       invalidateChecklistListQuery();
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.CHECKLIST] });
     },

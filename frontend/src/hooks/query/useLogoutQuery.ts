6491b4cdf606c7fe20c8056c99fe36f4ba6afe1a
Hyelim Choi <HyelimChoi01@gmail.com>
2024-10-24 22:54:20 +0900

    feat: 로그아웃/탈퇴 이후 모든 캐시 무효화

--- frontend/src/hooks/query/useLogoutQuery.ts
+++ frontend/src/hooks/query/useLogoutQuery.ts
@@ -1,15 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 import { postLogout } from '@/apis/user';
 import { ROUTE_PATH } from '@/constants/routePath';
 
 const useLogoutQuery = () => {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   return useMutation({
     mutationFn: postLogout,
     onSuccess: () => {
+      queryClient.clear();
       navigate(ROUTE_PATH.root);
     },
   });

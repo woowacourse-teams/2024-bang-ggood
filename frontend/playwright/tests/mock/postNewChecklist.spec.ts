7eecd3cf393508c2af115954caf3f182263e2270
Hyelim Choi <HyelimChoi01@gmail.com>
2024-10-24 19:56:47 +0900

    chore: e2e route 상수처리

--- frontend/playwright/tests/mock/postNewChecklist.spec.ts
+++ frontend/playwright/tests/mock/postNewChecklist.spec.ts
@@ -1,8 +1,10 @@
 import { test } from '@playwright/test';
 
+import { ROUTE_PATH } from '@/constants/routePath';
+
 test('빈 체크리스트를 제출할 수 있다.', async ({ page }) => {
-  await page.goto('/checklist/new');
+  await page.goto(ROUTE_PATH.checklistNew);
   await page.getByRole('button', { name: '저장' }).click();
   await page.getByRole('button', { name: '체크리스트 저장하기' }).click();
-  await page.waitForURL('/checklist');
+  await page.waitForURL(ROUTE_PATH.checklistList);
 });

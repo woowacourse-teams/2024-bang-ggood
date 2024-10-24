27f06bf8c5e95142f85946dc352cc64f66373e62
Hyelim Choi <HyelimChoi01@gmail.com>
2024-10-25 03:11:35 +0900

    fix: 탭이 랜더링 되지 않고 에러를 던지는 경우 수정

--- frontend/src/hooks/useInitialChecklist.ts
+++ frontend/src/hooks/useInitialChecklist.ts
@@ -1,11 +1,17 @@
+import { useEffect } from 'react';
+
 import useGetChecklistQuestionQuery from '@/hooks/query/useGetChecklistQuestionQuery';
 import useChecklistStore from '@/store/useChecklistStore';
 
 const useInitialChecklist = () => {
   const result = useGetChecklistQuestionQuery();
-
   const initAnswerSheetIfEmpty = useChecklistStore(state => state.actions.initAnswerSheetIfEmpty);
-  initAnswerSheetIfEmpty(result.data ?? []); // 체크리스트 질문에 대한 답안지 객체 생성
+
+  useEffect(() => {
+    if (result.isSuccess && result.data) {
+      initAnswerSheetIfEmpty(result.data); // 체크리스트 질문에 대한 답안지 객체 생성
+    }
+  }, [result.isSuccess, result.data, initAnswerSheetIfEmpty]);
 
   return result;
 };

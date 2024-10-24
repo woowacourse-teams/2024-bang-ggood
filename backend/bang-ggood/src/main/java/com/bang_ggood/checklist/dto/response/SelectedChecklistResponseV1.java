b26e7358db5428b8ca30ad884581d99caed9279c
sunghyun-0 <94783621+tsulocalize@users.noreply.github.com>
2024-10-24 16:11:01 +0900

    [BE] 체크리스트 조회 시 지하철 역 json 형태를 변경한다. (#880)

--- backend/bang-ggood/src/main/java/com/bang_ggood/checklist/dto/response/SelectedChecklistResponseV1.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/checklist/dto/response/SelectedChecklistResponseV1.java
@@ -4,6 +4,7 @@ import com.bang_ggood.option.dto.response.SelectedOptionResponse;
 import com.bang_ggood.question.dto.response.SelectedCategoryQuestionsResponse;
 import com.bang_ggood.room.dto.response.SelectedRoomResponse;
 import com.bang_ggood.room.dto.response.SelectedRoomResponseV1;
+import com.bang_ggood.station.dto.response.SubwayStationResponse;
 import com.bang_ggood.station.dto.response.SubwayStationResponses;
 import java.util.List;
 
@@ -11,12 +12,12 @@ public record SelectedChecklistResponseV1(SelectedRoomResponseV1 room,
                                           List<SelectedOptionResponse> options,
                                           List<SelectedCategoryQuestionsResponse> categories,
                                           boolean isLiked,
-                                          SubwayStationResponses stations) {
+                                          List<SubwayStationResponse> stations) {
 
     public static SelectedChecklistResponseV1 of(SelectedRoomResponse room, List<SelectedOptionResponse> options,
                                                  List<SelectedCategoryQuestionsResponse> categories, boolean isLiked,
                                                  SubwayStationResponses stations) {
         return new SelectedChecklistResponseV1(SelectedRoomResponseV1.from(room), options, categories, isLiked,
-                stations);
+                stations.getStations());
     }
 }

291524f2faa584ec30fa23b4988f529c257e007b
ooherin <555ohr@naver.com>
2024-10-24 13:41:29 +0900

    fix: 텍스트 수정

--- frontend/src/components/Main/ChecklistCardContainer.tsx
+++ frontend/src/components/Main/ChecklistCardContainer.tsx
@@ -26,7 +26,7 @@ const ChecklistCardContainer = () => {
         .map((checklist: ChecklistPreview, index: number) => (
           <ChecklistPreviewCard key={checklist.checklistId} index={index} checklist={checklist} />
         ))}
-      <S.NewButton label="+ 새로운 체크리스트 생성하기" isSquare size="full" onClick={handleNewChecklist} />
+      <S.NewButton label="+ 새로운 방 체크하기" isSquare size="full" onClick={handleNewChecklist} />
     </>
   );
 };

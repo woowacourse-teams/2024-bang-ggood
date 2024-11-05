0cc59db364a3c425511e0e096cfa0ccd304b9b5d
ooherin <555ohr@naver.com>
2024-11-05 17:45:05 +0900

    fix: 일부 코드 수정

--- frontend/playwright/tests/api/postNewChecklist.spec.ts
+++ frontend/playwright/tests/api/postNewChecklist.spec.ts
@@ -4,7 +4,7 @@ import { ROUTE_PATH } from '@/constants/routePath';
 
 import { DefaultChecklistTabsNames, FirstCategoryQuestion } from '../constants/constants';
 
-test.skip('체크리스트가 인풋을 채우고 제출할 수 있다.', async ({ page }) => {
+test('체크리스트가 인풋을 채우고 제출할 수 있다.', async ({ page }) => {
   await page.goto(ROUTE_PATH.checklistNew);
   const tabs = page.locator('.tab');
   const roomInfoTab = tabs.nth(0);

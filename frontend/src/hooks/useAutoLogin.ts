9ac5b810df12c5eb77f5e7b00953a08ce508e200
Hyelim Choi <HyelimChoi01@gmail.com>
2024-11-05 19:28:56 +0900

    chore: 유저 데이터 트래킹 추가

--- frontend/src/hooks/useAutoLogin.ts
+++ frontend/src/hooks/useAutoLogin.ts
@@ -4,9 +4,11 @@ import { useNavigate } from 'react-router-dom';
 import { deleteToken, getIsUserValid, getUserInfo, postReissueAccessToken } from '@/apis/user';
 import { ROUTE_PATH } from '@/constants/routePath';
 import useToast from '@/hooks/useToast';
+import amplitudeInitializer from '@/service/amplitude/amplitudeInitializer';
 
 const useAutoLogin = () => {
   const navigate = useNavigate();
+  const { init } = amplitudeInitializer();
   const { showToast } = useToast();
 
   const fetchIsUserValid = async () => {
@@ -16,6 +18,7 @@ const useAutoLogin = () => {
         try {
           await postReissueAccessToken();
           const result = await getUserInfo();
+          init(result.userEmail);
           showToast({ message: `${result?.userName}님, 환영합니다.`, type: 'confirm' });
           return navigate(ROUTE_PATH.home);
         } catch (err) {
--- frontend/src/service/amplitude/AmplitudeInitializer.tsx
+++ frontend/src/service/amplitude/AmplitudeInitializer.tsx
@@ -1,19 +1,18 @@
 import * as amplitude from '@amplitude/analytics-browser';
-import { useEffect } from 'react';
 
 const amplitudeApiKey = process.env.AMPLITUDE_API_KEY ?? '';
 
-const AmplitudeInitializer = ({ children }: React.PropsWithChildren) => {
-  useEffect(() => {
-    amplitude.init(amplitudeApiKey, undefined, {
+const amplitudeInitializer = () => {
+  const init = (userName?: string) => {
+    amplitude.init(amplitudeApiKey, userName, {
       defaultTracking: {
         pageViews: false,
       },
       minIdLength: 1,
     });
-  }, []);
+  };
 
-  return children;
+  return { init };
 };
 
-export default AmplitudeInitializer;
+export default amplitudeInitializer;

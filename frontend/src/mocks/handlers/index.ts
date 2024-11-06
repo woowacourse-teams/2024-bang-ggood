083813d812c1688fbcd17e0cc74241658a5a320e
Hyelim Choi <HyelimChoi01@gmail.com>
2024-11-06 12:36:46 +0900

    chore: 체크리스트 10개만 보이는 버그 수정

--- frontend/src/mocks/handlers/index.ts
+++ frontend/src/mocks/handlers/index.ts
@@ -1,15 +1,7 @@
 import { ArticleHandlers } from '@/mocks/handlers/article';
-import { categoryHandlers } from '@/mocks/handlers/category';
 import { checklistHandlers } from '@/mocks/handlers/checklist';
 import { likeHandlers } from '@/mocks/handlers/like';
 import { SubwayHandlers } from '@/mocks/handlers/subway';
 import { userHandlers } from '@/mocks/handlers/user';
 
-export const handlers = [
-  ...checklistHandlers,
-  ...categoryHandlers,
-  ...ArticleHandlers,
-  ...userHandlers,
-  ...SubwayHandlers,
-  ...likeHandlers,
-];
+export const handlers = [...checklistHandlers, ...ArticleHandlers, ...userHandlers, ...SubwayHandlers, ...likeHandlers];

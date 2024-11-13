60b521d04cbddfdb5321d94ae0745688f3ae8e59
tkdgur0906 <tkdgur0906@naver.com>
2024-11-13 11:11:55 +0900

    feat: 비밀번호 재설정 dto의 인증코드, 새로운 비밀번호 NotBlank 추가

--- backend/bang-ggood/src/main/java/com/bang_ggood/auth/dto/request/ConfirmPasswordResetCodeRequest.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/auth/dto/request/ConfirmPasswordResetCodeRequest.java
@@ -1,7 +1,8 @@
 package com.bang_ggood.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record ConfirmPasswordResetCodeRequest(@Email(message = "유효하지 않은 이메일 형식입니다.") String email,
-                                              String code) {
+                                              @NotBlank(message = "인증 코드가 비어있습니다.") String code) {
 }
--- backend/bang-ggood/src/main/java/com/bang_ggood/auth/dto/request/ResetPasswordRequest.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/auth/dto/request/ResetPasswordRequest.java
@@ -1,7 +1,9 @@
 package com.bang_ggood.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record ResetPasswordRequest(@Email(message = "유효하지 않은 이메일 형식입니다.") String email,
-                                   String code, String newPassword) {
+                                   @NotBlank(message = "인증 코드가 비어있습니다.") String code,
+                                   @NotBlank(message = "새로운 비밀번호가 비어있습니다.") String newPassword) {
 }

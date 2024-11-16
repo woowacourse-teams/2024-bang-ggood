10910e51b97cb0c0c4e35b2b0d9715431e776ecb
jinwoo22 <jenny010925@naver.com>
2024-11-16 13:42:44 +0900

    feat: 체크리스트와 카테고리로 조회 기능 구현

--- backend/bang-ggood/src/test/java/com/bang_ggood/question/repository/ChecklistQuestionRepositoryTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/question/repository/ChecklistQuestionRepositoryTest.java
@@ -7,12 +7,14 @@ import com.bang_ggood.checklist.repository.ChecklistRepository;
 import com.bang_ggood.question.ChecklistQuestionFixture;
 import com.bang_ggood.question.QuestionFixture;
 import com.bang_ggood.question.domain.ChecklistQuestion;
+import com.bang_ggood.question.domain.Question;
 import com.bang_ggood.room.RoomFixture;
 import com.bang_ggood.room.domain.Room;
 import com.bang_ggood.room.repository.RoomRepository;
 import com.bang_ggood.user.UserFixture;
 import com.bang_ggood.user.domain.User;
 import com.bang_ggood.user.repository.UserRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,6 +56,30 @@ class ChecklistQuestionRepositoryTest extends IntegrationTestSupport {
         assertAll(
                 () -> assertThat(checklistQuestions.get(0).isDeleted()).isFalse()
         );
+    }
+
+    @DisplayName("체크리스트와 카테고리로 조회 성공")
+    @Test
+    void findAllByChecklistIdAndCategoryId() {
+        // given
+        Question questionCategory1 = QuestionFixture.QUESTION1_CATEGORY1;
+        Question questionCategory2 = QuestionFixture.QUESTION3_CATEGORY2;
+        Room room = roomRepository.save(RoomFixture.ROOM_1());
+        User user = userRepository.save(UserFixture.USER1());
+
+        Checklist checklist = checklistRepository.save(ChecklistFixture.CHECKLIST1_USER1(room, user));
+        ChecklistQuestion checklistQuestion1 = checklistQuestionRepository.save(
+                ChecklistQuestionFixture.CHECKLIST1_QUESTION1(checklist, questionCategory1));
+        ChecklistQuestion checklistQuestion2 = checklistQuestionRepository.save(
+                ChecklistQuestionFixture.CHECKLIST1_QUESTION2(checklist, questionCategory2));
+
+        // when
+        List<ChecklistQuestion> checklistQuestions = checklistQuestionRepository.findAllByChecklistIdAndCategoryId(
+                checklist.getId(),
+                questionCategory1.getCategory().getId());
+
+        // then
+        Assertions.assertThat(checklistQuestions).containsOnly(checklistQuestion1);
     }
 
     @DisplayName("질문 답변을 체크리스트 ID로 논리적 삭제 성공")

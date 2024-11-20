b41f13d2f432bb8a4949dcf9ae20566e101e58c9
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-11-20 15:39:15 +0900

    feat: ChecklistQuestionRepository 구현구현

--- backend/bang-ggood/src/main/java/com/bang_ggood/question/repository/ChecklistQuestionRepository.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/question/repository/ChecklistQuestionRepository.java
@@ -1,6 +1,9 @@
 package com.bang_ggood.question.repository;
 
+import com.bang_ggood.question.domain.Answer;
+import com.bang_ggood.question.domain.Category;
 import com.bang_ggood.question.domain.ChecklistQuestion;
+import jakarta.persistence.criteria.CriteriaBuilder.In;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +17,30 @@ public interface ChecklistQuestionRepository extends JpaRepository<ChecklistQues
             + "WHERE cq.checklist.id = :checklistId "
             + "AND cq.deleted = false")
     List<ChecklistQuestion> findAllByChecklistId(@Param("checklistId") Long checklistId);
+
+    @Query("SELECT DISTINCT cq.question.category FROM ChecklistQuestion cq "
+            + "JOIN cq.checklist cl "
+            + "WHERE cl.id = :checklistId "
+            + "AND cl.user.id = :userId "
+            + "AND cq.deleted = false")
+    List<Category> findAllQuestionCategoriesByUserIdAndChecklistId(@Param("userId") Long userId, @Param("checklistId") Long checklistId);
+
+    @Query("SELECT COUNT(cq) FROM ChecklistQuestion cq "
+            + "WHERE cq.checklist.id = :checklistId "
+            + "AND cq.deleted = false "
+            + "AND cq.answer <> 'NONE' "
+            + "AND cq.question.category.id = :categoryId")
+    Integer countAnsweredQuestionsByChecklistIdAndCategoryId(@Param("checklistId") Long checklistId,
+                                                             @Param("categoryId") Integer categoryId);
+
+    @Query(" SELECT COUNT(cq) FROM ChecklistQuestion cq "
+            + "WHERE cq.checklist.id = :checklistId "
+            + "AND cq.deleted = false "
+            + "AND cq.answer = :answer "
+            + "AND cq.question.category.id = :categoryId ")
+    Integer countAnsweredQuestionsByChecklistIdAndCategoryIdAndAnswer(@Param("checklistId") Long checklistId,
+                                                                      @Param("categoryId") Integer categoryId,
+                                                                      @Param("answer") Answer answer);
 
     @Modifying(flushAutomatically = true, clearAutomatically = true)
     @Transactional
--- backend/bang-ggood/src/test/java/com/bang_ggood/question/repository/ChecklistQuestionRepositoryTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/question/repository/ChecklistQuestionRepositoryTest.java
@@ -6,6 +6,8 @@ import com.bang_ggood.checklist.domain.Checklist;
 import com.bang_ggood.checklist.repository.ChecklistRepository;
 import com.bang_ggood.question.ChecklistQuestionFixture;
 import com.bang_ggood.question.QuestionFixture;
+import com.bang_ggood.question.domain.Answer;
+import com.bang_ggood.question.domain.Category;
 import com.bang_ggood.question.domain.ChecklistQuestion;
 import com.bang_ggood.room.RoomFixture;
 import com.bang_ggood.room.domain.Room;
@@ -13,6 +15,7 @@ import com.bang_ggood.room.repository.RoomRepository;
 import com.bang_ggood.user.UserFixture;
 import com.bang_ggood.user.domain.User;
 import com.bang_ggood.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,14 +38,21 @@ class ChecklistQuestionRepositoryTest extends IntegrationTestSupport {
     @Autowired
     private ChecklistQuestionRepository checklistQuestionRepository;
 
+    private Room room;
+    private User user;
+    private Checklist checklist;
+
+    @BeforeEach
+    void beforeEach() {
+        room = roomRepository.save(RoomFixture.ROOM_1());
+        user = userRepository.save(UserFixture.USER1());
+        checklist = checklistRepository.save(ChecklistFixture.CHECKLIST1_USER1(room, user));
+    }
+
     @DisplayName("질문 답변을 체크리스트 ID로 조회 성공")
     @Test
     void findAllByChecklistId() {
         //given
-        Room room = roomRepository.save(RoomFixture.ROOM_1());
-        User user = userRepository.save(UserFixture.USER1());
-
-        Checklist checklist = checklistRepository.save(ChecklistFixture.CHECKLIST1_USER1(room, user));
         checklistQuestionRepository.save(ChecklistQuestionFixture.CHECKLIST1_QUESTION1(checklist, QuestionFixture.QUESTION1_CATEGORY1));
         checklistQuestionRepository.save(ChecklistQuestionFixture.CHECKLIST1_QUESTION2(checklist, QuestionFixture.QUESTION2_CATEGORY1));
 
@@ -56,14 +66,64 @@ class ChecklistQuestionRepositoryTest extends IntegrationTestSupport {
         );
     }
 
+    @DisplayName("주어진 체크리스트 ID와 사용자 ID로 카테고리들 조회 성공")
+    @Test
+    void findAllQuestionCategoriesByUserIdAndChecklistId() {
+        // given
+        checklistQuestionRepository.save(ChecklistQuestionFixture.CHECKLIST1_QUESTION1(checklist, QuestionFixture.QUESTION1_CATEGORY1));
+        checklistQuestionRepository.save(ChecklistQuestionFixture.CHECKLIST1_QUESTION2(checklist, QuestionFixture.QUESTION3_CATEGORY2));
+
+        // when
+        List<Category> categories = checklistQuestionRepository.findAllQuestionCategoriesByUserIdAndChecklistId(user.getId(), checklist.getId());
+
+        // then
+        assertThat(categories).hasSize(2);
+    }
+
+    @DisplayName("주어진 체크리스트 ID와 카테고리 ID로 답변된 질문 수 카운트 성공")
+    @Test
+    void countAnsweredQuestionsByChecklistIdAndCategoryId() {
+        // given
+        ChecklistQuestion cq1 = checklistQuestionRepository.save(ChecklistQuestionFixture.CHECKLIST1_QUESTION1(checklist, QuestionFixture.QUESTION1_CATEGORY1));
+        ChecklistQuestion cq2 = checklistQuestionRepository.save(ChecklistQuestionFixture.CHECKLIST1_QUESTION2(checklist, QuestionFixture.QUESTION1_CATEGORY1));
+        checklistQuestionRepository.save(cq1);
+        checklistQuestionRepository.save(cq2);
+
+        // when
+        Integer categoryId = cq1.getQuestion().getCategory().getId();
+        Integer count = checklistQuestionRepository.countAnsweredQuestionsByChecklistIdAndCategoryId(checklist.getId(), categoryId);
+
+        // then
+        assertThat(count).isEqualTo(2);
+    }
+
+    @DisplayName("주어진 체크리스트 ID, 카테고리 ID, 특정 답변으로 질문 수 카운트 성공")
+    @Test
+    void countAnsweredQuestionsByChecklistIdAndCategoryIdAndAnswer() {
+        // given
+        ChecklistQuestion cq1 = checklistQuestionRepository.save(ChecklistQuestionFixture.CHECKLIST1_QUESTION1(checklist, QuestionFixture.QUESTION1_CATEGORY1));
+        ChecklistQuestion cq2 = checklistQuestionRepository.save(ChecklistQuestionFixture.CHECKLIST1_QUESTION2(checklist, QuestionFixture.QUESTION1_CATEGORY1));
+        checklistQuestionRepository.save(cq1);
+        checklistQuestionRepository.save(cq2);
+
+        // when
+        Integer categoryId = cq1.getQuestion().getCategory().getId();
+        Integer countYes = checklistQuestionRepository.countAnsweredQuestionsByChecklistIdAndCategoryIdAndAnswer(checklist.getId(), categoryId, Answer.GOOD);
+        Integer countNo = checklistQuestionRepository.countAnsweredQuestionsByChecklistIdAndCategoryIdAndAnswer(checklist.getId(), categoryId, Answer.BAD);
+
+        // then
+        assertAll(
+                () -> assertThat(countYes).isEqualTo(1),
+                () -> assertThat(countNo).isEqualTo(1)
+        );
+    }
+
+
+
     @DisplayName("질문 답변을 체크리스트 ID로 논리적 삭제 성공")
     @Test
     void deleteAllByChecklistId() {
         //given
-        Room room = roomRepository.save(RoomFixture.ROOM_1());
-        User user = userRepository.save(UserFixture.USER1());
-
-        Checklist checklist = checklistRepository.save(ChecklistFixture.CHECKLIST1_USER1(room, user));
         ChecklistQuestion checklistQuestion1 = checklistQuestionRepository.save(
                 ChecklistQuestionFixture.CHECKLIST1_QUESTION1(checklist, QuestionFixture.QUESTION1_CATEGORY1));
         ChecklistQuestion checklistQuestion2 = checklistQuestionRepository.save(

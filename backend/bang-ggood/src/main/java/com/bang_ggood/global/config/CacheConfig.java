d64543713ec6c0279c373845febd824cb26f2c21
jinwoo22 <jenny010925@naver.com>
2024-11-21 14:12:41 +0900

    feat: 캐시 설정정보 추가

---
+++ backend/bang-ggood/src/main/java/com/bang_ggood/global/config/CacheConfig.java
@@ -0,0 +1,29 @@
+package com.bang_ggood.global.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.concurrent.TimeUnit;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .maximumSize(200)
+                .expireAfterWrite(1, TimeUnit.DAYS)
+                .recordStats();
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeine);
+        return cacheManager;
+    }
+}

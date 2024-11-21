e7959ffec9da25f268d138853b42cd905b2692a7
jinwoo22 <jenny010925@naver.com>
2024-11-21 21:00:42 +0900

    fix: ActiveProfile 어노테이션 추가

--- backend/bang-ggood/src/test/java/com/bang_ggood/global/cache/CacheTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/global/cache/CacheTest.java
@@ -11,12 +11,14 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest
-public class CacheTest {
+class CacheTest {
 
     @MockBean
     private ArticleRepository articleRepository;

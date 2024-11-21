81693b0d2545a894c9ffaffbb54d585ad2e943ea
gmuz1c <songjin1127@naver.com>
2024-11-21 16:32:55 +0900

    [BE] 로컬 캐시를 적용한다. (#982)
    Co-authored-by: jinwoo22 <jenny010925@naver.com>
    Co-authored-by: jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>

---
+++ backend/bang-ggood/src/main/java/com/bang_ggood/global/config/cache/CacheName.java
@@ -0,0 +1,8 @@
+package com.bang_ggood.global.config.cache;
+
+public class CacheName {
+    public static final String CATEGORY = "category";
+    public static final String QUESTION = "question";
+    public static final String HIGHLIGHT = "highlight";
+    public static final String ARTICLE = "article";
+}

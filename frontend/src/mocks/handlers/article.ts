b3d184ae38090c09643343cba5d36670391848be
Hyelim Choi <HyelimChoi01@gmail.com>
2024-11-25 14:03:14 +0900

    chore: 불필요한 모킹 삭제

--- frontend/src/mocks/handlers/article.ts
+++ frontend/src/mocks/handlers/article.ts
@@ -1,7 +1,6 @@
 import { http, HttpResponse } from 'msw';
 
 import { BASE_URL, ENDPOINT } from '@/apis/url';
-import { article2 } from '@/mocks/fixtures/a';
 import { article } from '@/mocks/fixtures/article';
 import { articleList } from '@/mocks/fixtures/articleList';
 
@@ -12,10 +11,6 @@ export const ArticleHandlers = [
 
   http.get(BASE_URL + ENDPOINT.ARTICLE_ID(1), () => {
     return HttpResponse.json(article, { status: 200 });
-  }),
-
-  http.get(BASE_URL + ENDPOINT.ARTICLE_ID(2), () => {
-    return HttpResponse.json(article2, { status: 200 });
   }),
 
   http.get(BASE_URL + ENDPOINT.ARTICLE_ID(3), () => {

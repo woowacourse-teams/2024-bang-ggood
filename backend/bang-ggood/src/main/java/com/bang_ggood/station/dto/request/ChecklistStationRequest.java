d2cf519a82a7c76f060beaf51236c3639fc4f276
jinwoo22 <98975580+JINU-CHANG@users.noreply.github.com>
2024-12-06 12:45:41 +0900

    [BE] 체크리스트 작성, 수정 API 요청시 중복된 위도, 경도정보를 삭제한다. (#1022)

--- backend/bang-ggood/src/main/java/com/bang_ggood/station/dto/request/ChecklistStationRequest.java
+++
@@ -1,8 +0,0 @@
-package com.bang_ggood.station.dto.request;
-
-public record ChecklistStationRequest(double latitude, double longitude) {
-
-    public static ChecklistStationRequest of(double latitude, double longitude) {
-        return new ChecklistStationRequest(latitude, longitude);
-    }
-}

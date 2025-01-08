d80acfee58c2f101c28393d17c3822fc36347161
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-01-08 16:06:28 +0900

    feat(ArticleViewService): 조회수 DB 동기화 구현

--- backend/bang-ggood/src/main/java/com/bang_ggood/article/service/ArticleViewService.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/article/service/ArticleViewService.java
@@ -1,11 +1,15 @@
 package com.bang_ggood.article.service;
 
 import com.bang_ggood.article.domain.Article;
-import com.bang_ggood.article.dto.response.ArticleResponse;
+import com.bang_ggood.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 import static com.bang_ggood.global.config.cache.CacheName.ARTICLE;
 
@@ -14,6 +18,8 @@ import static com.bang_ggood.global.config.cache.CacheName.ARTICLE;
 public class ArticleViewService {
 
     private final CacheManager cacheManager;
+    private final ArticleRepository articleRepository;
+    private final Set<Long> cacheArticleIds = new HashSet<>();
 
     public void increaseViewCount(long id) {
         Cache cache = cacheManager.getCache(ARTICLE);
@@ -27,6 +33,22 @@ public class ArticleViewService {
         if (article != null) {
             article.increaseViewCount();
             cache.put(id, article);
+            cacheArticleIds.add(id);
         }
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void syncViewCounts() {
+        Cache cache = cacheManager.getCache(ARTICLE);
+        if (cache != null) {
+            syncAllArticleViewCounts(cache);
+        }
+    }
+
+    private void syncAllArticleViewCounts(Cache cache) {
+        cacheArticleIds.stream()
+                .map(id -> cache.get(id, Article.class))
+                .filter(Objects::nonNull)
+                .forEach(article -> articleRepository.updateViewCount(article.getId(), article.getViewCount()));
     }
 }
--- backend/bang-ggood/src/test/java/com/bang_ggood/article/service/ArticleViewServiceTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/article/service/ArticleViewServiceTest.java
@@ -2,21 +2,36 @@ package com.bang_ggood.article.service;
 
 import com.bang_ggood.IntegrationTestSupport;
 import com.bang_ggood.article.ArticleFixture;
+import com.bang_ggood.article.domain.Article;
 import com.bang_ggood.article.dto.response.ArticleResponse;
 import com.bang_ggood.article.repository.ArticleRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import java.time.Instant;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@EnableScheduling
 class ArticleViewServiceTest extends IntegrationTestSupport {
 
     @Autowired
-    ArticleManageService articleManageService;
+    private ArticleManageService articleManageService;
 
     @Autowired
-    ArticleRepository articleRepository;
+    private ArticleViewService articleViewService;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private TaskScheduler taskScheduler;
 
     @DisplayName("아티클 조회 시 조회수 증가 성공 : DB에서 조회하는 경우")
     @Test
@@ -45,5 +60,22 @@ class ArticleViewServiceTest extends IntegrationTestSupport {
         assertThat(article.viewCount()).isEqualTo(2);
     }
 
+    @DisplayName("캐시된 조회수를 DB에 동기화 성공")
+    @Test
+    void syncViewCounts() throws ExecutionException, InterruptedException {
+        // given
+        Long articleId = articleRepository.save(ArticleFixture.ARTICLE()).getId();
+        articleManageService.readArticle(articleId);
+        articleManageService.readArticle(articleId);
 
+        // when
+        taskScheduler.schedule(
+                () -> articleViewService.syncViewCounts(),
+                Instant.parse("2025-01-01T00:00:00Z")
+        ).get();
+
+        // then
+        Article article = articleRepository.getById(articleId);
+        assertThat(article.getViewCount()).isEqualTo(2);
+    }
 }

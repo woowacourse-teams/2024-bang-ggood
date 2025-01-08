0d704470158dd5086512ec57122465061f04bf67
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-01-08 10:53:44 +0900

    test(CahceTest): Disabled 어노테이션 삭제

--- backend/bang-ggood/src/test/java/com/bang_ggood/global/cache/CacheTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/global/cache/CacheTest.java
@@ -27,18 +27,14 @@ class CacheTest {
     private ArticleService articleService;
 
     private final Long cacheableArticleId = 1L;
-    private Article article;
 
     @BeforeEach
     void setUp() {
-        article = new Article("이름", "내용", "키워드", "요약", "썸네일");
+        Article article = new Article("이름", "내용", "키워드", "요약", "썸네일");
         Mockito.when(articleRepository.getById(cacheableArticleId))
                 .thenReturn(article);
     }
 
-    @Disabled("캐싱을 이용할 경우 증가된 조회수도 불러올 수 없는 문제가 있습니다" +
-            "[아티클 내용]과 [아티클 조회수]를 도메인 분리하면 캐싱 가능한데" +
-            "어느 정도 효과가 있을 지는 의문이 있어 우선 캐시 제거했습니다")
     @DisplayName("캐시가 정상적으로 작동한다.")
     @Test
     void cacheTest() {

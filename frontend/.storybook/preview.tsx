aa822d8d580ad51b8a72fdc7e4c37e60fa8f53eb
Hyelim Choi <HyelimChoi01@gmail.com>
2025-04-01 18:14:09 +0900

    chore: 스토리북 QueryClientProvider 추가

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,12 +1,17 @@
 import { css, Global, ThemeProvider } from '@emotion/react';
 import type { Preview } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { withRouter } from 'storybook-addon-remix-react-router';
 import { normalize } from '../src/styles/reset';
 import theme from '../src/styles/theme';
 
+const queryClient = new QueryClient({
+  defaultOptions: {},
+});
+
 const decorator = Story => (
-  <>
+  <QueryClientProvider client={queryClient}>
     <Global
       styles={css`
         ${normalize}
@@ -18,7 +23,7 @@ const decorator = Story => (
     <ThemeProvider theme={theme}>
       <Story />
     </ThemeProvider>
-  </>
+  </QueryClientProvider>
 );
 
 const preview: Preview = {

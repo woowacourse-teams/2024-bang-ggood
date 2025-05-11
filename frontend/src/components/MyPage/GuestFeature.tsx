947a1ed9d18312d27c37372837e2885d267f9c16
tsulocalize <tsulocalizing@gmail.com>
2025-05-11 20:29:27 +0900

    chore: 마이페이지 GuestFeature 디자인 조정

--- frontend/src/components/MyPage/GuestFeature.tsx
+++ frontend/src/components/MyPage/GuestFeature.tsx
@@ -1,10 +1,11 @@
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 
-import { ArrowRightCircleIcon } from '@/assets/assets';
+import { ArrowRightIcon } from '@/assets/assets';
 import { ROUTE_PATH } from '@/constants/routePath';
 import { VOC_URL } from '@/constants/VoC';
-import { boxShadowSpread, flexColumn, flexRow, flexSpaceBetween, title4 } from '@/styles/common';
+import { flexColumn, flexRow, flexSpaceBetween } from '@/styles/common';
+import { fontStyle } from '@/utils/fontStyle';
 
 const GuestFeature = () => {
   const navigate = useNavigate();
@@ -14,24 +15,23 @@ const GuestFeature = () => {
 
   return (
     <>
+      <S.TopLabelContainer>
+        <span>방끗을 더 잘 사용하기 위해 로그인해보세요!</span>
+      </S.TopLabelContainer>
       <S.Container>
-        <S.LabelContainer>
-          <span>방끗을 더 잘 사용하기 위해 로그인해보세요!</span>
-        </S.LabelContainer>
-
         <S.Section>
           <S.LabelContainer>방끗이 도움되었나요? 한마디 남겨주세요!</S.LabelContainer>
-          <S.Button tabIndex={1} onClick={handleMoveVoc}>
+          <S.Button tabIndex={1} isCenter onClick={handleMoveVoc}>
             방끗이 기다려요, 의견 남기기!
-            <ArrowRightCircleIcon aria-hidden="true" />
+            <ArrowRightIcon aria-hidden="true" />
           </S.Button>
         </S.Section>
 
         <S.Section>
           <S.LabelContainer>방끗 시작하기!</S.LabelContainer>
-          <S.Button onClick={() => navigate(ROUTE_PATH.root)} tabIndex={1}>
+          <S.Button onClick={() => navigate(ROUTE_PATH.root)} isCenter isBlack tabIndex={1}>
             로그인/회원가입 바로가기
-            <ArrowRightCircleIcon aria-hidden="true" />
+            <ArrowRightIcon aria-hidden="true" style={{ filter: 'invert(1)' }} />
           </S.Button>
         </S.Section>
       </S.Container>
@@ -45,42 +45,50 @@ const S = {
   Container: styled.div`
     ${flexColumn}
     ${flexSpaceBetween}
-    height: calc(40dvh);
-    padding: 1.6rem;
+    gap: 2.5rem;
+    padding: 2.8rem 1.6rem 3.2rem 1.6rem;
   `,
   Section: styled.section`
     ${flexColumn}
-    gap: 1rem;
+    gap: 0.8rem;
   `,
   LabelContainer: styled.div`
     ${flexColumn}
     gap: .5rem;
-    ${title4}
+    ${({ theme }) => fontStyle(theme.font.body[1].R)}
   `,
-  Button: styled.button<{ isCenter?: boolean }>`
+  TopLabelContainer: styled.div`
+    ${flexColumn}
+    gap: .5rem;
+    padding: 1.2rem 1.6rem;
+    ${({ theme }) => fontStyle(theme.font.body[1].B)}
+    color: ${({ theme }) => theme.color.gray[400]};
+  `,
+  Button: styled.button<{ isCenter?: boolean, isBlack?:boolean }>`
     width: 100%;
     ${flexRow}
-    padding: 1.6rem;
+    padding: 1.2rem 1.6rem;
+    box-sizing: border-box;
+    gap: 0.8rem;
     align-items: center;
-
-    background-color: ${({ theme }) => theme.palette.white};
-
+    background-color: ${({ theme, isBlack }) => (isBlack ? theme.color.mono.black : theme.color.mono.white)};
     justify-content: ${({ isCenter }) => (isCenter ? 'center' : 'space-between')};
-
-    border-radius: 1.6rem;
-    ${boxShadowSpread}
+    border-radius: 0.8rem;
+    border: 1px solid ${({ theme }) => theme.color.gray[200]};
+    ${({ theme }) => fontStyle(theme.font.body[1].B)}
+    color: ${({ theme, isBlack }) => (isBlack ? theme.color.mono.white : theme.color.mono.black)};
 
     &:hover, &:active {
-      background-color: ${({ theme }) => theme.palette.grey100};
-
-      font-weight: ${({ theme }) => theme.text.weight.bold};
+        background-color: ${({ theme }) => theme.color.gray[100]};
+        font-weight: ${({ theme }) => theme.text.weight.bold};
     }
   `,
   TextButton: styled.button`
     width: fit-content;
+    margin-top: 1rem;
 
-    color: ${({ theme }) => theme.palette.grey500};
+    color: ${({ theme }) => theme.color.gray[500]};
     font-size: ${({ theme }) => theme.text.size.xxSmall};
-    border-bottom: 1px solid ${({ theme }) => theme.palette.grey500};
+    border-bottom: 1px solid ${({ theme }) => theme.color.gray[500]};
   `,
 };

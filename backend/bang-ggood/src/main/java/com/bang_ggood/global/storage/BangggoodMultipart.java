603bee20e703260fa7908629485b1512ed0853fa
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-05-18 01:22:10 +0900

    feat: BangggoodMultipart 구현

---
+++ backend/bang-ggood/src/main/java/com/bang_ggood/global/storage/BangggoodMultipart.java
@@ -0,0 +1,58 @@
+package com.bang_ggood.global.storage;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class BangggoodMultipart implements MultipartFile {
+
+    private final String name;
+    private final String originalFilename;
+    private final String contentType;
+    private final byte[] content;
+
+    @Override
+    public boolean isEmpty() {
+        return content == null || content.length == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return content.length;
+    }
+
+    @Override
+    public byte[] getBytes() {
+        return content;
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return new ByteArrayInputStream(content);
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(dest)) {
+            fos.write(content);
+        }
+    }
+
+    public static BangggoodMultipart from(MultipartFile file, String contentType, byte[] bytes) {
+        return new BangggoodMultipart(
+                file.getName(),
+                file.getOriginalFilename(),
+                contentType,
+                bytes
+        );
+    }
+}

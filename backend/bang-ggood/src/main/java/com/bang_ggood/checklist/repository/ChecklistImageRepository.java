23d031169dc0fbf90f6bcb9fcb3f1c7382488fc2
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-05-18 01:30:44 +0900

    feat: ChecklistImageRepository 구현

---
+++ backend/bang-ggood/src/main/java/com/bang_ggood/checklist/repository/ChecklistImageRepository.java
@@ -0,0 +1,7 @@
+package com.bang_ggood.checklist.repository;
+
+import com.bang_ggood.checklist.domain.ChecklistImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChecklistImageRepository extends JpaRepository<ChecklistImage, Long> {
+}

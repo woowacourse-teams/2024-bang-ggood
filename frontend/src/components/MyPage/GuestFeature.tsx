f6be18bc2e13757c052ae0df0bee2b14b9654f4b
tsulocalize <tsulocalizing@gmail.com>
2025-05-23 16:07:48 +0900

    chore: 게스트 마이페이지 스타일 조정

--- frontend/src/components/MyPage/GuestFeature.tsx
+++ frontend/src/components/MyPage/GuestFeature.tsx
@@ -21,18 +21,18 @@ const GuestFeature = () => {
       <S.Container>
         <S.Section>
           <S.LabelContainer>방끗이 도움되었나요? 한마디 남겨주세요!</S.LabelContainer>
-          <S.Button tabIndex={1} isCenter onClick={handleMoveVoc}>
+          <S.WhiteButton tabIndex={1} isCenter onClick={handleMoveVoc}>
             방끗이 기다려요, 의견 남기기!
             <ArrowRightIcon aria-hidden="true" />
-          </S.Button>
+          </S.WhiteButton>
         </S.Section>
 
         <S.Section>
           <S.LabelContainer>방끗 시작하기!</S.LabelContainer>
-          <S.Button onClick={() => navigate(ROUTE_PATH.root)} isCenter isBlack tabIndex={1}>
+          <S.BlackButton onClick={() => navigate(ROUTE_PATH.root)} isCenter isBlack tabIndex={1}>
             로그인/회원가입 바로가기
             <ArrowRightIcon aria-hidden="true" style={{ filter: 'invert(1)' }} />
-          </S.Button>
+          </S.BlackButton>
         </S.Section>
       </S.Container>
     </>
@@ -64,24 +64,41 @@ const S = {
     ${({ theme }) => fontStyle(theme.font.body[1].B)}
     color: ${({ theme }) => theme.color.gray[400]};
   `,
-  Button: styled.button<{ isCenter?: boolean, isBlack?:boolean }>`
-    width: 100%;
-    ${flexRow}
-    padding: 1.2rem 1.6rem;
-    box-sizing: border-box;
-    gap: 0.8rem;
-    align-items: center;
-    background-color: ${({ theme, isBlack }) => (isBlack ? theme.color.mono.black : theme.color.mono.white)};
-    justify-content: ${({ isCenter }) => (isCenter ? 'center' : 'space-between')};
-    border-radius: 0.8rem;
-    border: 1px solid ${({ theme }) => theme.color.gray[200]};
-    ${({ theme }) => fontStyle(theme.font.body[1].B)}
-    color: ${({ theme, isBlack }) => (isBlack ? theme.color.mono.white : theme.color.mono.black)};
+  BlackButton: styled.button<{ isCenter?: boolean, isNoBorder?:boolean }>`
+      width: 100%;
+      ${flexRow}
+      padding: 1.2rem 1.6rem;
+      box-sizing: border-box;
+      gap: 0.8rem;
+      align-items: center;
+      background-color: ${({ theme }) => theme.color.mono.black};
+      justify-content: ${({ isCenter }) => (isCenter ? 'center' : 'space-between')};
+      border-radius: 0.8rem;
+      border: ${({ isNoBorder, theme }) => (isNoBorder ? '' : '1px solid ' + theme.color.gray[200])};
+      ${({ theme }) => fontStyle(theme.font.body[1].B)}
+      color: ${({ theme }) => theme.color.mono.white};
 
-    &:hover, &:active {
-        background-color: ${({ theme }) => theme.color.gray[100]};
-        font-weight: ${({ theme }) => theme.text.weight.bold};
-    }
+      &:hover, &:active {
+          color: ${({ theme }) => theme.color.gray[500]};
+      }
+  `,
+  WhiteButton: styled.button<{ isCenter?: boolean, isNoBorder?:boolean }>`
+      width: 100%;
+      ${flexRow}
+      padding: 1.2rem 1.6rem;
+      box-sizing: border-box;
+      gap: 0.8rem;
+      align-items: center;
+      background-color: ${({ theme }) => theme.color.mono.white};
+      justify-content: ${({ isCenter }) => (isCenter ? 'center' : 'space-between')};
+      border-radius: 0.8rem;
+      border: ${({ isNoBorder, theme }) => (isNoBorder ? '' : '1px solid ' + theme.color.gray[200])};
+      ${({ theme }) => fontStyle(theme.font.body[1].B)}
+      color: ${({ theme }) => theme.color.mono.black};
+
+      &:hover, &:active {
+          background-color: ${({ theme }) => theme.color.gray[100]};
+      }
   `,
   TextButton: styled.button`
     width: fit-content;

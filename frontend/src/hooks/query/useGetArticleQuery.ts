b78bd737192220059415cf3e001fa0ede0e42eb6
Hyelim Choi <HyelimChoi01@gmail.com>
2025-06-01 15:10:34 +0900

    feat: 아티클 디테일 페이지 작업

--- frontend/src/hooks/query/useGetArticleQuery.ts
+++ frontend/src/hooks/query/useGetArticleQuery.ts
@@ -1,11 +1,16 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { getArticle } from '@/apis/article';
 import { QUERY_KEYS } from '@/constants/queryKeys';
 import { Article } from '@/types/article';
 
 const useGetArticleQuery = (articleId: string) => {
-  return useQuery<Article>({ queryKey: [QUERY_KEYS.ARTICLE, articleId], queryFn: () => getArticle(Number(articleId)) });
+  const { data } = useSuspenseQuery<Article>({
+    queryKey: [QUERY_KEYS.ARTICLE, articleId],
+    queryFn: () => getArticle(Number(articleId)),
+  });
+
+  return { article: data };
 };
 
 export default useGetArticleQuery;

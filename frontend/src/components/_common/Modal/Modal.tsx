5a8c3e739fa7c33efb1490fcb192890eb05faa38
skiende74 <skiende74@gmail.com>
2025-06-08 23:23:27 +0900

    fix: 모달이 왼쪽으로 쏠리는 현상을 해결한다.

--- frontend/src/components/_common/Modal/Modal.tsx
+++ frontend/src/components/_common/Modal/Modal.tsx
@@ -6,6 +6,7 @@ import { createPortal } from 'react-dom';
 import { CloseIcon } from '@/assets/assets';
 import { fadeIn, fadeOut } from '@/styles/animation';
 import { flexColumn } from '@/styles/common';
+
 import FocusTrap from './FocusTrap/FocusTrap';
 import ModalBody from './ModalBody';
 import ModalFooter from './ModalFooter';
@@ -97,6 +98,7 @@ const S = {
   ModalInner: styled.div<{ isOpen: boolean; backgroundColor: string }>`
     overflow: scroll;
     position: relative;
+    width: 100%;
     padding: 1.6rem;
 
     background-color: ${({ backgroundColor }) => backgroundColor};

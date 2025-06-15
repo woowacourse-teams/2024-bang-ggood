e232ad8ac62a7494853ab2898e02da33ecc245ae
tsulocalize <tsulocalizing@gmail.com>
2025-06-15 22:26:41 +0900

    fix: Modal content 바디 쪽으로 이동

--- frontend/src/components/MyPage/DeleteAccountModal.tsx
+++ frontend/src/components/MyPage/DeleteAccountModal.tsx
@@ -20,18 +20,20 @@ const DeleteAccountModal = ({ isOpen, onClose }: Props) => {
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <Modal.header>
+      <Modal.header style={{ marginBottom: `0.8rem`, padding: 0, minHeight: 0 }}>
         회원탈퇴
+      </Modal.header>
+      <Modal.body>
         <S.Contents>
           <span style={{whiteSpace: 'pre-wrap', textAlign: 'center'}}>
             {'잠깐만요! 정말 방끗을 떠나실건가요?\n회원탈퇴를 하면 방끗에서 작성한\n체크리스트를 다시는 확인할 수 없어요'}
           </span>
         </S.Contents>
-      </Modal.header>
+      </Modal.body>
       <Modal.footer>
         <S.ButtonContainer>
-          <Button label="취소하기" size="full" color="light" onClick={onClose} isSquare />
-          <Button label="탈퇴하기" size="full" color="dark" onClick={handleDeleteAccount} isSquare />
+          <Button label="취소하기" size="full" color="light" onClick={onClose} />
+          <Button label="탈퇴하기" size="full" color="dark" onClick={handleDeleteAccount} />
         </S.ButtonContainer>
       </Modal.footer>
     </Modal>

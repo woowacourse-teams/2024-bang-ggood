d6ff3a63b77955d53f55624a0c844d4d7e3914d0
Hyelim Choi <HyelimChoi01@gmail.com>
2025-06-15 15:00:14 +0900

    feat: 다섯번째 섹션 작업

--- frontend/src/components/Landing/FifthSection.tsx
+++ frontend/src/components/Landing/FifthSection.tsx
@@ -1,26 +1,17 @@
-import { LampIcon } from '@/assets/assets';
 import TopButton from '@/components/_common/TopButton/TopButton';
 import CS from '@/components/Landing/style';
 
 const FifthSection = () => {
   return (
     <CS.Container>
-      <CS.TextBox>
-        <CS.RelativeBox>
-          <CS.Text>
-            <CS.Bold>방끗</CS.Bold>과 함께 당신에게 <br />
-            <CS.Highlight>딱 맞는 집을 찾는 여정</CS.Highlight>을 <br />
-            시작해 볼까요?
-          </CS.Text>
-          <CS.IconBox>
-            <LampIcon width={80} />
-          </CS.IconBox>
-          <CS.EmptyBox height="2rem" />
-          <CS.CenterBox>
-            <TopButton text={'시작하기!'} />
-          </CS.CenterBox>
-        </CS.RelativeBox>
-      </CS.TextBox>
+      <CS.Title>
+        방끗과 함께
+        <br />딱 맞는 집을 찾으러 가볼까요?
+      </CS.Title>
+
+      <div style={{ width: '80%' }}>
+        <TopButton text={'시작하기!'} />
+      </div>
     </CS.Container>
   );
 };

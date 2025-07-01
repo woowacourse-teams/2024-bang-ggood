0c7515a4ebb7ded3d52b700ad83616d3adead63f
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2025-07-01 18:45:27 +0900

    [FE] 자잘한 버그를 수정한다 (#1177)

--- frontend/src/components/ChecklistList/NoChecklistTemplate.tsx
+++ frontend/src/components/ChecklistList/NoChecklistTemplate.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 
 import Button from '@/components/_common/Button/Button';
+import Text from '@/components/_common/Text/Text';
 import { ROUTE_PATH } from '@/constants/routePath';
 import { flexCenter } from '@/styles/common';
 
@@ -13,11 +14,22 @@ const NoChecklistTemplate = () => {
 
   return (
     <S.Container>
-      <S.TextBox>
-        <div>체크리스트가 아직 없어요!</div>
-        <div>첫 체크리스트를 작성하러 가볼까요?</div>
-      </S.TextBox>
-      <Button color="dark" label="새 체크리스트 만들기" onClick={handleMoveNewChecklist} size="small" />
+      <Text typography={font => font.body[2].M} color={color => color.gray[500]}>
+        <S.TextBox>
+          <div>체크리스트가 아직 없어요!</div>
+          <div>첫 체크리스트를 작성하러 가볼까요?</div>
+        </S.TextBox>
+      </Text>
+      <Button
+        color="dark"
+        label={
+          <S.Text typography={font => font.body[2].B} color={color => color.mono.white}>
+            새 체크리스트 만들기
+          </S.Text>
+        }
+        onClick={handleMoveNewChecklist}
+        size="small"
+      />
     </S.Container>
   );
 };
@@ -33,6 +45,9 @@ const S = {
   TextBox: styled.div`
     ${flexCenter}
     flex-direction: column;
+  `,
+  Text: styled(Text)`
+    padding: 0 0.8rem;
   `,
 };
 

c95120ef6ff26ad583fdc208508dfd86ffbf6207
jinwoo22 <jenny010925@naver.com>
2025-07-11 17:49:56 +0900

    fix: 체크리스트 공유가 존재할때만 삭제하도록 수정

--- backend/bang-ggood/src/test/java/com/bang_ggood/checklist/service/ChecklistShareServiceTest.java
+++ backend/bang-ggood/src/test/java/com/bang_ggood/checklist/service/ChecklistShareServiceTest.java
@@ -4,6 +4,8 @@ import com.bang_ggood.IntegrationTestSupport;
 import com.bang_ggood.checklist.ChecklistFixture;
 import com.bang_ggood.checklist.domain.Checklist;
 import com.bang_ggood.checklist.domain.ChecklistShare;
+import com.bang_ggood.global.exception.BangggoodException;
+import com.bang_ggood.global.exception.ExceptionCode;
 import com.bang_ggood.room.RoomFixture;
 import com.bang_ggood.room.domain.Room;
 import com.bang_ggood.room.repository.RoomRepository;
@@ -15,6 +17,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ChecklistShareServiceTest extends IntegrationTestSupport {
@@ -65,5 +69,37 @@ class ChecklistShareServiceTest extends IntegrationTestSupport {
 
         //then
         assertThat(firstShare).isEqualTo(secondShare);
+    }
+
+    @DisplayName("체크리스트 공유 삭제 성공")
+    @Test
+    void deleteChecklistShare() {
+        // given
+        Room room = roomRepository.save(RoomFixture.ROOM_1());
+        User user = userRepository.save(UserFixture.USER1());
+        Checklist checklist = ChecklistFixture.CHECKLIST1_USER1(room, user);
+        Checklist savedChecklist = checklistService.createChecklist(checklist);
+        ChecklistShare checklistShare = checklistShareService.createChecklistShare(savedChecklist);
+
+        // when
+        checklistShareService.deleteChecklistShare(savedChecklist);
+
+        // then
+        assertThatThrownBy(() -> checklistShareService.readChecklistShare(checklistShare.getToken()))
+                .isInstanceOf(BangggoodException.class)
+                .hasMessage(ExceptionCode.CHECKLIST_SHARE_NOT_FOUND.getMessage());
+    }
+
+    @DisplayName("체크리스트 공유가 존재하지 않으면 삭제하지 않음")
+    @Test
+    void deleteChecklistShare_notExist() {
+        Room room = roomRepository.save(RoomFixture.ROOM_1());
+        User user = userRepository.save(UserFixture.USER1());
+        Checklist checklist = ChecklistFixture.CHECKLIST1_USER1(room, user);
+        Checklist savedChecklist = checklistService.createChecklist(checklist);
+
+        // then
+        assertThatCode(() -> checklistShareService.deleteChecklistShare(savedChecklist))
+                .doesNotThrowAnyException();
     }
 }

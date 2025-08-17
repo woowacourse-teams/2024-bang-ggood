20c16f02b95e6ffd709a124cc273bf541e60d3ef
skiende74 <skiende74@gmail.com>
2025-08-18 00:18:29 +0900

    feat(필터링모달): 필터링 선택 표 구현

--- frontend/src/components/Building/BuildingFilterModal/BuildingFilterModal.tsx
+++ frontend/src/components/Building/BuildingFilterModal/BuildingFilterModal.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
+
 import Button from '@/components/_common/Button/Button';
 import Modal from '@/components/_common/Modal/Modal';
 import ModalBody from '@/components/_common/Modal/ModalBody';
 import ModalFooter from '@/components/_common/Modal/ModalFooter';
 import ModalHeader from '@/components/_common/Modal/ModalHeader';
 import Text from '@/components/_common/Text/Text';
+import SubwayFilterTable from '@/components/Building/BuildingFilterModal/SubwayFilterTable';
 import BuildingListSearchBar from '@/components/Building/BuildingListSearchBar';
 
 // 건물을 "인근 지하철역"을 기준으로 필터링하는 모달
@@ -13,6 +16,8 @@ interface BuildingFilterModalProps {
   buildingCount: number;
 }
 function BuildingFilterModal({ isOpen, onFilter, buildingCount }: BuildingFilterModalProps) {
+  const [selectedStations, setSelectedStations] = useState<string[]>([]);
+  console.log(selectedStations);
   return (
     <Modal isOpen={isOpen} onClose={() => {}}>
       <ModalHeader>
@@ -20,6 +25,7 @@ function BuildingFilterModal({ isOpen, onFilter, buildingCount }: BuildingFilter
       </ModalHeader>
       <ModalBody>
         <BuildingListSearchBar onSearch={searchTerm => onFilter({ search: searchTerm })} />
+        <SubwayFilterTable onSelectSubwayStation={setSelectedStations} />
       </ModalBody>
       <ModalFooter>
         <Button label="초기화" />
@@ -28,11 +34,5 @@ function BuildingFilterModal({ isOpen, onFilter, buildingCount }: BuildingFilter
     </Modal>
   );
 }
-
-const subwayMap = {
-  서울: { '1호선': ['서울역', '시청', '종각', '명동'] },
-  경기: { '1호선': ['수원역', '화성행궁', '오산역'] },
-  인천: { '1호선': ['인천역', '신포역', '차이나타운'] },
-};
 
 export default BuildingFilterModal;
---
+++ frontend/src/components/Building/BuildingFilterModal/SubwayFilterTable.tsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+
+import FlexBox from '@/components/_common/FlexBox/FlexBox';
+import Text from '@/components/_common/Text/Text';
+
+interface SubwayTableProps {
+  onSelectSubwayStation: (stationNames: string[]) => void;
+}
+
+function SubwayFilterTable({ onSelectSubwayStation }: SubwayTableProps) {
+  const [selectedRegion, setSelectedRegion] = useState<keyof typeof subwayMap | null>(null);
+  const [selectedLine, setSelectedLine] = useState<string | null>(null);
+  const [selectedStations, setSelectedStation] = useState<string[]>([]);
+
+  return (
+    <div>
+      <FlexBox.Horizontal>
+        <div>지역</div>
+        <div>호선</div>
+        <div>역명</div>
+      </FlexBox.Horizontal>
+      <FlexBox.Horizontal>
+        <div style={{ flexDirection: 'column', display: 'flex' }}>
+          {Object.keys(subwayMap).map(region => (
+            <button key={region} onClick={() => setSelectedRegion(region as keyof typeof subwayMap)}>
+              <Text
+                typography={font => font.body[2].B}
+                color={color => (selectedRegion === region ? color.primary[600] : color.mono.black)}
+              >
+                {region}
+              </Text>
+            </button>
+          ))}
+        </div>
+        {selectedRegion && (
+          <FlexBox.Vertical>
+            {Object.entries(subwayMap[selectedRegion])?.map(([line, stations]) => (
+              <div key={line} onClick={() => setSelectedLine(line)}>
+                <Text
+                  typography={font => font.body[2].B}
+                  color={color => (selectedLine === line ? color.primary[600] : color.mono.black)}
+                >
+                  {line}
+                </Text>
+              </div>
+            ))}
+          </FlexBox.Vertical>
+        )}
+        {selectedRegion && selectedLine && (
+          <FlexBox.Vertical>
+            {subwayMap[selectedRegion][selectedLine].map(station => (
+              <button
+                key={station}
+                onClick={() => {
+                  const newStations = selectedStations?.includes(station)
+                    ? selectedStations.filter(s => s !== station)
+                    : [...selectedStations, station];
+                  setSelectedStation(newStations);
+                  onSelectSubwayStation(newStations);
+                }}
+              >
+                <Text
+                  typography={font => font.body[2].B}
+                  color={color => (selectedStations.includes(station) ? color.primary[600] : color.mono.black)}
+                >
+                  {station}
+                </Text>
+              </button>
+            ))}
+          </FlexBox.Vertical>
+        )}
+      </FlexBox.Horizontal>
+    </div>
+  );
+}
+
+const subwayMap: Record<string, Record<string, string[]>> = {
+  서울: {
+    '1호선': ['서울역', '시청', '종각', '동대문'],
+    '2호선': ['시청', '을지로입구', '강남', '잠실'],
+    '3호선': ['경복궁', '안국', '압구정', '고속터미널'],
+    '4호선': ['서울역', '명동', '혜화', '이촌'],
+  },
+  경기: {
+    '1호선': ['수원역', '성균관대', '병점', '오산역'],
+    '3호선': ['대화', '주엽', '정발산', '마두'],
+    '4호선': ['산본', '금정', '중앙', '오이도'],
+    '5호선': ['하남풍산', '하남시청', '하남검단산'],
+    '8호선': ['모란', '수진', '산성'],
+  },
+  인천: {
+    '1호선': ['동인천', '주안', '부평', '인천'],
+    '7호선': ['석남', '산곡', '부평구청'],
+  },
+};
+export default SubwayFilterTable;

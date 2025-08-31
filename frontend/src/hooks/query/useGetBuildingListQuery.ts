f4b9410505668899bc50e599b52139c57b7648ff
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2025-08-31 19:51:59 +0900

    [FE] 빌딩 목록페이지 필터링, 검색 기능 구현한다. (#1193)

--- frontend/src/hooks/query/useGetBuildingListQuery.ts
+++ frontend/src/hooks/query/useGetBuildingListQuery.ts
@@ -2,6 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getBuildingList } from '@/apis/building';
 import { QUERY_KEYS } from '@/constants/queryKeys';
+import { PaginationParams } from '@/types/api';
 
-export const useGetBuildingListQuery = () =>
-  useQuery({ queryKey: [QUERY_KEYS.BUILDING_LIST], queryFn: getBuildingList });
+export const useGetBuildingListQuery = (
+  searchParams: { search?: string; subways?: string[] } = {},
+  pageParams: PaginationParams = { page: 1, size: 20 },
+) => {
+  const subways = searchParams.subways?.join(',');
+  return useQuery({
+    queryKey: [QUERY_KEYS.BUILDING_LIST, searchParams, pageParams],
+    queryFn: () => getBuildingList({ ...searchParams, subways }, pageParams),
+  });
+};

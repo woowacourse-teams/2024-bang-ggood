69807d3c4645ee303d84d7d0de29111c054b92dc
tsulocalize <tsulocalizing@gmail.com>
2025-09-28 16:37:35 +0900

    feat: 빌딩 예외 추가

--- backend/bang-ggood/src/main/java/com/bang_ggood/global/exception/ExceptionCode.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/global/exception/ExceptionCode.java
@@ -50,6 +50,8 @@ public enum ExceptionCode {
     // Building
     BUILDING_ALL_NULL_OR_NOT_NULL(HttpStatus.BAD_REQUEST, ClientExceptionCode.CHECKLIST_ERROR,
             "address, walkingTime, latitude, longitude는 전부 null이거나 전부 값이 있어야 합니다."),
+    BUILDING_NOT_FOUND(HttpStatus.BAD_REQUEST, ClientExceptionCode.BUILDING_NOT_FOUND,
+            "해당 빌딩을 찾을 수 없습니다."),
 
     // Checklist
     CHECKLIST_NOT_FOUND(HttpStatus.BAD_REQUEST, ClientExceptionCode.CHECKLIST_NOT_FOUND, "체크리스트가 존재하지 않습니다."),

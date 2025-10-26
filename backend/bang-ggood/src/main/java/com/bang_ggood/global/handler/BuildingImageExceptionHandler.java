4b8b3ab08acbb5640f8399fa2d41f7523ee96292
jinwoo22 <jenny010925@naver.com>
2025-10-26 16:17:47 +0900

    refactor: 빌딩 이미지 API 요청 예외발생시 로그 남기도록 추가

--- backend/bang-ggood/src/main/java/com/bang_ggood/global/handler/BuildingImageExceptionHandler.java
+++ backend/bang-ggood/src/main/java/com/bang_ggood/global/handler/BuildingImageExceptionHandler.java
@@ -1,14 +1,16 @@
 package com.bang_ggood.global.handler;
 
 import com.bang_ggood.global.exception.BangggoodException;
-import com.bang_ggood.global.exception.ExternalAPIException;
 import com.bang_ggood.global.exception.ExceptionCode;
+import com.bang_ggood.global.exception.ExternalAPIException;
 import com.bang_ggood.global.exception.dto.BuildingImageExceptionResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResponseErrorHandler;
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class BuildingImageExceptionHandler implements ResponseErrorHandler {
 
@@ -25,6 +27,7 @@ public class BuildingImageExceptionHandler implements ResponseErrorHandler {
     public void handleError(ClientHttpResponse response) {
         BuildingImageExceptionResponse responseBody = ExternalAPIExceptionConvertor.convert(response,
                 BuildingImageExceptionResponse.class);
+        log.warn(responseBody.toString());
         throw new ExternalAPIException(responseBody);
     }
 }
